6e2fa1662b9ebf0fe8b03ff5c7816a97ede3c0ee
hazem osama ibrahim <hazem3500@gmail.com>
2020-05-28 18:42:14 +0200

    setup test environment

---
+++ src/components/dls/Cards/Card.test.tsx
@@ -0,0 +1,9 @@
+import Card from "./Card";
+import { render } from "../../../../tests/utils";
+
+test("should render", () => {
+  const { container } = render(
+    <Card title="title" subtitle="subtitle" image="#" />
+  );
+  expect(container).toMatchSnapshot();
+});
