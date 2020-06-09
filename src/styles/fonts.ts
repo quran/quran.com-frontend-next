54d183c6def97897c10807c0cb93ecfdb584fbaf
Mohamed El Mahallawy <mmahalwy@gmail.com>
2020-06-09 02:51:10 -0400

    change cdn url

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,6 +1,6 @@
 import range from 'lodash/range';
 
-export const baseUrl = 'https://qurancdn.nyc3.cdn.digitaloceanspaces.com';
+export const baseUrl = 'https://quranstatic.nyc3.cdn.digitaloceanspaces.com';
 
 export const makeFont = (url: string, pageNumber: string | number) => `
 @font-face {
