a1c951c245d730e6259848e58b81cf4dd252eac1
nabeel <nabeel.elhusayni@kensho.com>
2020-06-29 12:25:39 -0400

    Upgrade eslint related deps (#49)
    * Upgrade eslint related deps
    
    * more strict about member names

--- src/constants/words.ts
+++ src/constants/words.ts
@@ -1,4 +1,5 @@
-// eslint-disable-next-line import/prefer-default-export
+/* eslint-disable import/prefer-default-export */
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export enum WORD_TYPES {
   CHAR_TYPE_WORD = 'word',
   CHAR_TYPE_END = 'end',
