f5efce5c2360241763df489917739b38e549852b
Abdellatif Abdelfattah <tifa3333@gmail.com>
2020-07-02 23:28:38 +0200

    add rtk and react-redux

--- src/pages_/_app.tsx
+++ src/pages_/_app.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { DefaultSeo } from 'next-seo';
 import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
 import { theme } from '../utils/styles';
 import { createSEOConfig } from '../utils/seo';
 import Footer from '../components/Footer';
+import store from '../redux/store';
 
 function MyApp({ Component, pageProps }): JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <DefaultSeo {...createSEOConfig({})} />
-
-      <Component {...pageProps} />
-      <Footer />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <DefaultSeo {...createSEOConfig({})} />
+        <Component {...pageProps} />
+        <Footer />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
