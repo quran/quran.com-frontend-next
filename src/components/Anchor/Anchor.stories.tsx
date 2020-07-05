a0030a12919f12103c44f7c1acd1c502fa206e6c
Pidaus <firdaus.kamal251@gmail.com>
2020-07-05 15:11:05 +0800

    New Anchor component and stories

---
+++ src/components/Anchor/Anchor.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Anchor from '.';
+
+export default {
+  title: 'dls|Anchor',
+};
+
+export const defaultAnchor = () => <Anchor href="http://example.com">Example.com</Anchor>;
---
+++ src/components/Anchor/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AnchorStyle = styled.a`
+  cursor: pointer;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.grey['2']};
+  & :hover {
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+const Anchor = ({ href, children }: { href: string; children: any }) => (
+  <AnchorStyle href={href} target="_blank" rel="noopener noreferrer">
+    {children}
+  </AnchorStyle>
+);
+
+export default Anchor;
