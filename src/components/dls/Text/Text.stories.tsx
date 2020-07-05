a62e91168df0b30958e70309a6c5087d895b6c37
Pidaus <firdaus.kamal251@gmail.com>
2020-07-05 15:10:44 +0800

    Update Text component stories

--- src/components/dls/Text/Text.stories.tsx
+++ src/components/dls/Text/Text.stories.tsx
@@ -9,6 +9,8 @@ export const normal = () => <Text>Hello there</Text>;
 
 export const primaryColor = () => <Text primary>Hello there</Text>;
 
+export const largeSize = () => <Text large>Hello there</Text>;
+
 export const smallSize = () => <Text small>Hello there</Text>;
 
 export const miniSize = () => <Text mini>Hello there</Text>;
