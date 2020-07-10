566ed065256a76649b1ebddd4e95085ef186eecc
Abdellatif <tifa3333@gmail.com>
2020-07-10 12:42:00 +0200

    Update the TODO comment

--- src/pages_/[chapterId].tsx
+++ src/pages_/[chapterId].tsx
@@ -17,7 +17,7 @@ type ChapterProps = {
 
 const Chapter: NextPage<ChapterProps> = ({ chapterResponse: { chapter }, versesResponse }) => {
   const { data } = useSWR(
-    makeUrl(`/chapters/${chapter.id}/verses?translations=20`), // TODO: select the translation using a helper function
+    makeUrl(`/chapters/${chapter.id}/verses?translations=20`), // TODO: select the translation using the user preference
     fetcher,
     {
       initialData: versesResponse,
