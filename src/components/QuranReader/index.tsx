cfc6b45c86bb09a43cedba76cb91ea080cad6a8c
Abdellatif Abdelfattah <tifa3333@gmail.com>
2020-07-13 17:02:18 +0200

    Add useSWRInfinite for paginating requests (#81)

--- src/components/QuranReader/index.tsx
+++ src/components/QuranReader/index.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import VerseType from '../../../types/VerseType';
+import ChapterType from 'types/ChapterType';
+import { fetcher } from 'src/api';
+import { useSWRInfinite } from 'swr';
+import { makeUrl } from 'src/utils/api';
+import VerseType from 'types/VerseType';
 import QuranPageView from './QuranPageView';
 import TranslationView from './TranslationView';
 import { selectReadingView } from '../../redux/slices/QuranReader/readingView';
 import { ReadingView } from './types';
 
 type QuranReaderProps = {
-  verses: VerseType[];
+  initialData: { verses: VerseType[] };
+  chapter: ChapterType;
 };
 
-const QuranReader = ({ verses }: QuranReaderProps) => {
+const QuranReader = ({ initialData, chapter }: QuranReaderProps) => {
+  const { data } = useSWRInfinite(
+    (index) => {
+      return makeUrl(`/chapters/${chapter.id}/verses`, { translations: 20, page: index }); // TODO: select the translation using the user preference
+    },
+    fetcher,
+    {
+      initialData: initialData.verses,
+      revalidateOnFocus: false,
+    },
+  );
+
   const readingView = useSelector(selectReadingView);
 
   if (readingView === ReadingView.QuranPage) {
-    return <QuranPageView verses={verses} />;
+    return <QuranPageView verses={data} />;
   }
 
-  return <TranslationView verses={verses} />;
+  return <TranslationView verses={data} />;
 };
 
 export default QuranReader;
