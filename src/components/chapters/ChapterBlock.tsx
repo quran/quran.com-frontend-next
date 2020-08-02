8b3c27a152a2c5f322a8d71d4e63be9c5f1044d1
Abdellatif <tifa3333@gmail.com>
2020-08-02 13:06:00 +0200

    fix nextjs 9.5 storybook error

--- src/components/chapters/ChapterBlock.tsx
+++ src/components/chapters/ChapterBlock.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import Link from 'next-translate/Link';
+import ChapterType from 'types/ChapterType';
 import ChapterIcon from './ChapterIcon';
-import ChapterType from '../../../types/ChapterType';
 
 const Item = styled.li`
   list-style: none;
