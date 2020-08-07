6adbc440d299e0ea083959d9f96619d239e29dcf
Abdellatif Abdelfattah <tifa3333@gmail.com>
2020-08-07 20:05:58 +0200

    Create the Page component and Memoize function calls (#93)

--- src/components/QuranReader/PageView/index.tsx
+++ src/components/QuranReader/PageView/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
 import VerseType from '../../../../types/VerseType';
-import Line from './Line';
-import groupLinesByVerses from './groupLinesByVerses';
+import Page from './Page';
+import groupPagesByVerses from './groupPagesByVerses';
 
 type PageViewProps = {
   verses: VerseType[];
 };
 
 const PageView = ({ verses }: PageViewProps) => {
-  const lines = groupLinesByVerses(verses);
+  const pages = useMemo(() => groupPagesByVerses(verses), [verses]);
 
   return (
     <StyledPageView>
-      {Object.keys(lines).map((key) => (
-        <Line words={lines[key]} key={key} />
+      {Object.keys(pages).map((key) => (
+        <Page verses={pages[key]} key={key} />
       ))}
     </StyledPageView>
   );
@@ -23,7 +23,6 @@ const PageView = ({ verses }: PageViewProps) => {
 const StyledPageView = styled.div`
   max-width: 100%;
   direction: rtl;
-  margin: 1rem auto;
 `;
 
-export default PageView;
+export default React.memo(PageView);
