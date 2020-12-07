591be995531dbe437569adf9d36664719c8e4c04
Abdellatif <tifa3333@gmail.com>
2020-12-07 19:51:00 +0100

    Add next 10 storybook support

--- src/components/dls/Cards/CardRow.stories.tsx
+++ src/components/dls/Cards/CardRow.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import CardRow from './CardRow';
 import Card from './Card';
-import image from '../../../../public/images/sunnah.png';
 
 export default {
   title: 'dls|CardRow',
@@ -9,8 +8,8 @@ export default {
 
 export const normal = () => (
   <CardRow>
-    <Card title="Title" subtitle="Subtitle" image={image} />
-    <Card title="Title" subtitle="Subtitle" image={image} />
-    <Card title="Title" subtitle="Subtitle" image={image} />
+    <Card title="Title" subtitle="Subtitle" image="/sample-image.png" />
+    <Card title="Title" subtitle="Subtitle" image="/sample-image.png" />
+    <Card title="Title" subtitle="Subtitle" image="/sample-image.png" />
   </CardRow>
 );
