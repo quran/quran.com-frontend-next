542234431a043014d5d00cf79f45751fe50dc58f
Abdellatif Abdelfattah <abdellatif@abdellatifs-macbook-pro-2.home>
2021-01-11 16:36:00 +0100

    Add developer utilities

---
+++ src/components/developerUtility/FontAdjustment.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  decreaseQuranTextSize,
+  decreaseTranslationTextSize,
+  increaseQuranTextSize,
+  increaseTranslationTextSize,
+  selectQuranReaderStyles,
+  setQuranFont,
+} from 'src/redux/slices/QuranReader/styles';
+import { QuranFont } from '../QuranReader/types';
+
+/**
+ * Adjusts the font type and styles
+ */
+const FontAdjustment = () => {
+  const dispatch = useDispatch();
+  const quranReaderStyles = useSelector(selectQuranReaderStyles);
+  const availableFonts = [];
+
+  Object.values(QuranFont).forEach((font) => availableFonts.push(font));
+
+  return (
+    <>
+      {/* Font style */}
+      <label htmlFor="font-styles">
+        Font style{' '}
+        <select
+          name="font-styles"
+          onChange={(event) => dispatch({ type: setQuranFont.type, payload: event.target.value })}
+          value={quranReaderStyles.quranFont}
+        >
+          {availableFonts.map((font) => (
+            <option key={font} value={font}>
+              {font}
+            </option>
+          ))}
+        </select>
+      </label>
+      {/* Quran Font size */}
+      <div>
+        Quran font size{' '}
+        <button onClick={() => dispatch({ type: decreaseQuranTextSize.type })} type="button">
+          -
+        </button>{' '}
+        <button onClick={() => dispatch({ type: increaseQuranTextSize.type })} type="button">
+          +
+        </button>
+      </div>
+      {/* Translation Font size */}
+      <div>
+        Translation font size{' '}
+        <button onClick={() => dispatch({ type: decreaseTranslationTextSize.type })} type="button">
+          -
+        </button>{' '}
+        <button onClick={() => dispatch({ type: increaseTranslationTextSize.type })} type="button">
+          +
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default FontAdjustment;
---
+++ src/utils/environment.ts
@@ -0,0 +1,6 @@
+// TODO @Abdellatif: Implement the environment checks
+export const IS_LOCALHOST = true;
+
+export const IS_DEVELOPMENT = true;
+
+export const IS_PRODUCTION = true;
