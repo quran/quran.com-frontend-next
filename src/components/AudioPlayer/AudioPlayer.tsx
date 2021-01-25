06fe40fbba405949d8d44df2ad891ff486cf02ad
Abdellatif Abdelfattah <tifa3333@gmail.com>
2021-01-25 18:49:26 +0200

    Add placeholder Navbar and audio player (#123)

---
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AudioPlayer = () => {
+  return <StyledContainer>[Placeholder AudioPlayer]</StyledContainer>;
+};
+
+const StyledContainer = styled.div`
+  position: sticky;
+  min-height: 40px;
+  width: 100%;
+  bottom: 0;
+  text-align: center;
+  background: #ffebab; // placeholder
+`;
+export default AudioPlayer;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Navbar = () => {
+  return <StyledNav>[Placeholder Navbar]</StyledNav>;
+};
+
+const StyledNav = styled.nav`
+  position: relative;
+  min-height: 50px;
+  width: 100%;
+  text-align: center;
+  border-bottom: 1px black solid;
+`;
+export default Navbar;
