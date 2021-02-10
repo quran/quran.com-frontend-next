90fa9cf029d5bcd771e79f69bb6d871389f78e65
Abdellatif Abdelfattah <tifa3333@gmail.com>
2021-02-10 20:39:41 +0200

    Improve styled components global styles rendering

--- src/pages_/_document.tsx
+++ src/pages_/_document.tsx
@@ -1,54 +1,31 @@
 import React from 'react';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, { DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-import { makeGlobalCss } from '../styles/GlobalStyles';
-import makeFonts from '../styles/fonts';
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    // Step 1: Create an instance of ServerStyleSheet
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-    // Step 2: Retrieve styles from components in the page
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(
-        <>
-          <App {...props} />
-        </>,
-      ),
-    );
+    try {
+      // eslint-disable-next-line
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
 
-    // Step 3: Extract the styles as <style> tags
-    const styleTags = sheet.getStyleElement();
-
-    // Step 4: Pass styleTags as a prop
-    return { ...page, styleTags };
-  }
-
-  render() {
-    // @ts-ignore
-    const { styleTags } = this.props;
-
-    return (
-      <Html>
-        <Head>
-          {/* Step 5: Output the styles in the head  */}
-          {styleTags}
-          <style
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `
-          ${makeGlobalCss()}
-          ${makeFonts()}
-          `,
-            }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
