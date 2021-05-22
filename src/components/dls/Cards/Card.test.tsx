d4ad6217ea33bb39e5b71ae5216051eb418cdfc9
Abdellatif Abdelfattah <tifa3333@gmail.com>
2021-05-22 13:33:51 +0300

    upgrade to next 10.2.2 (#166)
    * upgrade to next 10.2.2
    
    * update snapshots

--- src/components/dls/Cards/Card.test.tsx
+++
@@ -1,7 +0,0 @@
-import Card from './Card';
-import { render } from '../../../../tests/utils';
-
-test('should render', () => {
-  const { container } = render(<Card title="title" subtitle="subtitle" image="#" />);
-  expect(container).toMatchSnapshot();
-});
