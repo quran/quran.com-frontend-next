f1e06590c35994c02856040c52235a90b7c1d7d1
Abdellatif Abdelfattah <tifa3333@gmail.com>
2021-06-17 10:50:53 +0200

    upgrade to next 11

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const config = {
   images: {
     domains: ['cdn.qurancdn.com', 'vercel.com', 'now.sh', 'quran.com'],
   },
-  future: { webpack5: true },
 };
 
 module.exports = withPlugins([withFonts], config);
