c24c8ba15199d829b506c80095b8a63b6bcc0ba1
Abdellatif <tifa3333@gmail.com>
2021-07-14 10:51:38 +0200

    delete test.js file

--- src/components/DeveloperUtility/test.js
+++
@@ -1,8 +0,0 @@
-// Calculate the seconds between two days
-const calcSecondsBetweenDays = (day1, day2) => {
-  const date1 = new Date(day1);
-  const date2 = new Date(day2);
-  return Math.abs(date1.getTime() - date2.getTime()) / 1000;
-};
-
-export default calcSecondsBetweenDays;
