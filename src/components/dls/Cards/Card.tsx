58160fbb375038f307c5c70627720e702aec2d84
osamasayed <osama.sayed.abdelazim@gmail.com>
2021-08-08 20:20:48 +0700

    scss dls

--- src/components/dls/Cards/Card.tsx
+++ src/components/dls/Cards/Card.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
-import { rgba } from 'polished';
 import Image from 'next/image';
+import styles from './Card.module.scss';
 
 type CardProps = {
   title: string;
@@ -15,62 +14,22 @@ const DIMENSIONS = {
 };
 
 const Card = ({ title, subtitle, image }: CardProps) => (
-  <CardContainer>
-    <StyledImage
+  <figure className={styles.container}>
+    <Image
       src={image}
       role="presentation"
       alt={title}
       width={DIMENSIONS.WIDTH}
       height={DIMENSIONS.HEIGHT}
+      className={styles.image}
     />
-    <Caption>
+    <div className={styles.caption}>
       <div>
-        <Title>{title}</Title>
+        <h3 className={styles.title}>{title}</h3>
         <p>{subtitle}</p>
       </div>
-    </Caption>
-  </CardContainer>
+    </div>
+  </figure>
 );
-
-const CardContainer = styled.figure`
-  position: relative;
-  border-radius: ${(props) => props.theme.borderRadiuses.default};
-  width: 100%;
-  white-space: normal;
-  scroll-snap-align: center;
-  margin: 0;
-  overflow: hidden;
-  height: calc(6 * ${(props) => props.theme.spacing.mega});
-`;
-
-const StyledImage = styled(Image)`
-  max-width: 100%;
-  height: auto;
-  border-radius: ${(props) => props.theme.borderRadiuses.default};
-  width: 100%;
-`;
-
-const Caption = styled.div`
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  background: ${(props) => rgba(props.theme.colors.primary.medium, 0.7)};
-  border-radius: ${(props) => props.theme.borderRadiuses.default};
-  font-family: SFProText-Regular;
-  font-size: ${(props) => props.theme.fontSizes.xlarge};
-  color: ${(props) => props.theme.colors.text.link};
-  padding: ${(props) => props.theme.spacing.medium} ${(props) => props.theme.spacing.mega};
-  align-items: flex-end;
-  display: flex;
-`;
-
-const Title = styled.h3`
-  font-family: SFProText-Regular;
-  font-size: calc(2 * ${(props) => props.theme.fontSizes.large});
-  color: #fff;
-  margin-bottom: ${(props) => props.theme.spacing.xxsmall};
-`;
 
 export default Card;
--- src/components/dls/Cards/CardRow.tsx
+++ src/components/dls/Cards/CardRow.tsx
@@ -1,43 +1,14 @@
 import React, { ReactElement } from 'react';
-import styled from 'styled-components';
-import { space, MarginProps, PaddingProps } from 'styled-system';
-
-const CardRowContainer = styled.div<MarginProps & PaddingProps>`
-  display: grid;
-  grid-gap: ${(props) => props.theme.spacing.small};
-  grid-template-columns: 0;
-  grid-template-rows: auto;
-  grid-auto-flow: column;
-  grid-auto-columns: auto;
-
-  grid-column: 1 / -1;
-
-  scrollbar-width: none;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  overflow-x: scroll;
-  scroll-snap-type: x proximity;
-  padding-bottom: ${(props) => props.theme.spacing.xsmall};
-  margin-bottom: -${(props) => props.theme.spacing.micro};
-  ${space}
-
-  &:before,
-  &:after {
-    content: '';
-    width: ${(props) => props.theme.spacing.xsmall};
-  }
-`;
+import styles from './CardRow.module.scss';
 
 type CardRowProps = {
   children: ReactElement[];
-} & MarginProps &
-  PaddingProps;
+};
 
 const CardRow = ({ children, ...props }: CardRowProps) => (
-  <CardRowContainer {...props}>{children}</CardRowContainer>
+  <div {...props} className={styles.container}>
+    {children}
+  </div>
 );
 
 export default CardRow;
