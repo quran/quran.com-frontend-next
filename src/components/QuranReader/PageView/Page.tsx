1d5dc4fd0a509d14155b1fe446d5b42df6476c82
osamasayed <osamasayed@users.noreply.github.com>
2021-08-09 18:36:09 +0700

    Remove styled-components from QuranReader (#235)

--- src/components/QuranReader/PageView/Page.tsx
+++ src/components/QuranReader/PageView/Page.tsx
@@ -1,8 +1,8 @@
 import React, { useMemo } from 'react';
-import styled from 'styled-components';
 import Verse from '../../../../types/Verse';
 import Line from './Line';
 import groupLinesByVerses from './groupLinesByVerses';
+import styles from './Page.module.scss';
 
 type PageProps = {
   verses: Verse[];
@@ -13,16 +13,12 @@ const Page = ({ verses, page }: PageProps) => {
   const lines = useMemo(() => groupLinesByVerses(verses), [verses]);
 
   return (
-    <StyledPage id={`page-${page}`}>
+    <div id={`page-${page}`} className={styles.container}>
       {Object.keys(lines).map((key) => (
         <Line words={lines[key]} key={key} />
       ))}
-    </StyledPage>
+    </div>
   );
 };
-
-const StyledPage = styled.div`
-  margin: 0 auto;
-`;
 
 export default React.memo(Page);
