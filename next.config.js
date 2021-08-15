f402f187df92906b011f9f1a859a4056efd5e24a
Abdellatif <tifa3333@gmail.com>
2021-08-15 11:02:45 +0200

    add sentry integration

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withFonts = require('next-fonts');
 const nextTranslate = require('next-translate');
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
+const { withSentryConfig } = require('@sentry/nextjs');
 
 const config = {
   images: {
@@ -14,6 +15,17 @@ const config = {
     runtimeCaching,
     publicExcludes: ['!fonts/v1/**/*', '!fonts/v2/**/*'],
   },
+  SentryWebpackPluginOptions: {
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
+
+    silent: true, // Suppresses all logs
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+  },
 };
 
-module.exports = withPlugins([withPWA, withFonts, nextTranslate], config);
+module.exports = withPlugins([withPWA, withFonts, nextTranslate, withSentryConfig], config);
