9fa732e90d9132ed98e7e9ca581cd251efa718af
Muhammad Muhajir <muhajirframe@gmail.com>
2021-08-16 12:36:12 +0700

    enable seek functionality

--- src/components/AudioPlayer/PlaybackControls.tsx
+++ src/components/AudioPlayer/PlaybackControls.tsx
@@ -7,13 +7,17 @@ import PauseIcon from '../../../public/icons/pause-circle-outline.svg';
 import ReplayIcon from '../../../public/icons/replay_10.svg';
 import Button, { ButtonSize } from '../dls/Button/Button';
 import { withStopPropagation } from './util';
-import { triggerPauseAudio, triggerPlayAudio } from './EventTriggers';
+import { triggerPauseAudio, triggerPlayAudio, triggerSeek } from './EventTriggers';
 
 const PlaybackControls = () => {
   const isPlaying = useSelector(selectIsPlaying);
   return (
     <div className={styles.container}>
-      <Button icon={<ReplayIcon />} size={ButtonSize.Medium} />
+      <Button
+        icon={<ReplayIcon />}
+        size={ButtonSize.Medium}
+        onClick={withStopPropagation(() => triggerSeek(-10))}
+      />
       {isPlaying ? (
         <Button
           icon={<PauseIcon />}
@@ -27,7 +31,11 @@ const PlaybackControls = () => {
           onClick={withStopPropagation(triggerPlayAudio)}
         />
       )}
-      <Button icon={<ForwardIcon />} size={ButtonSize.Medium} />
+      <Button
+        icon={<ForwardIcon />}
+        size={ButtonSize.Medium}
+        onClick={withStopPropagation(() => triggerSeek(10))}
+      />
     </div>
   );
 };
