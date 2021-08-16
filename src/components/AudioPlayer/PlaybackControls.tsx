66343d4f2d2a5cd9421c615c5f7daf800d15eacd
Muhammad Muhajir <muhajirframe@gmail.com>
2021-08-16 18:02:50 +0700

    add more icons

--- src/components/AudioPlayer/PlaybackControls.tsx
+++ src/components/AudioPlayer/PlaybackControls.tsx
@@ -5,6 +5,8 @@ import PlayIcon from '../../../public/icons/play-circle-outline.svg';
 import ForwardIcon from '../../../public/icons/forward_10.svg';
 import PauseIcon from '../../../public/icons/pause-circle-outline.svg';
 import ReplayIcon from '../../../public/icons/replay_10.svg';
+import ShareIcon from '../../../public/icons/share.svg';
+import RepeatIcon from '../../../public/icons/ic_repeat_24px 1.svg';
 import Button, { ButtonSize } from '../dls/Button/Button';
 import { withStopPropagation } from './util';
 import { triggerPauseAudio, triggerPlayAudio, triggerSeek } from './EventTriggers';
@@ -13,29 +15,31 @@ const PlaybackControls = () => {
   const isPlaying = useSelector(selectIsPlaying);
   return (
     <div className={styles.container}>
+      <Button icon={<RepeatIcon />} size={ButtonSize.Small} />
       <Button
         icon={<ReplayIcon />}
-        size={ButtonSize.Medium}
+        size={ButtonSize.Small}
         onClick={withStopPropagation(() => triggerSeek(-10))}
       />
       {isPlaying ? (
         <Button
           icon={<PauseIcon />}
-          size={ButtonSize.Medium}
+          size={ButtonSize.Small}
           onClick={withStopPropagation(triggerPauseAudio)}
         />
       ) : (
         <Button
           icon={<PlayIcon />}
-          size={ButtonSize.Medium}
+          size={ButtonSize.Small}
           onClick={withStopPropagation(triggerPlayAudio)}
         />
       )}
       <Button
         icon={<ForwardIcon />}
-        size={ButtonSize.Medium}
+        size={ButtonSize.Small}
         onClick={withStopPropagation(() => triggerSeek(10))}
       />
+      <Button icon={<ShareIcon />} size={ButtonSize.Small} />
     </div>
   );
 };
