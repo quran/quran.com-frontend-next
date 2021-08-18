108464ac7b8500c761e060c780363e81d7d09f7a
Muhammad Muhajir <muhajirframe@gmail.com>
2021-08-18 14:34:38 +0700

    Audio Player Expanded Design (#270)
    * ui logic, setIsMinimized
    
    * initial expanded design
    
    * enable seek functionality
    
    * add more icons
    
    * [wip] still buggy
    
    * fix styling
    
    * update slider position
    
    * update spacing
    
    * move stopPropagation to global util
    
    * update spacing
    
    * add button trigger expand desktop
    
    * update ui
    
    * refactor
    
    * refactor
    
    * hide currenttime on mobile

--- src/components/dls/Button/Button.tsx
+++ src/components/dls/Button/Button.tsx
@@ -5,6 +5,7 @@ import styles from './Button.module.scss';
 
 type ButtonProps = {
   size?: ButtonSize;
+  desktopSize?: ButtonSize;
   text?: string;
   name?: string;
   disabled?: boolean;
@@ -20,23 +21,33 @@ export enum ButtonSize {
   Large = 'large',
 }
 
-const Container = ({ children, disabled, size, href, onClick }) => {
+const Container: React.FC<ButtonProps> = ({
+  children,
+  disabled,
+  size,
+  desktopSize,
+  href,
+  onClick,
+}) => {
+  const classes = {
+    [styles.hoverableContainer]: !disabled,
+    [styles.disabledContainer]: disabled,
+    [styles.xsmallStyledContainer]: size === ButtonSize.XSmall,
+    [styles.smallStyledContainer]: size === ButtonSize.Small,
+    [styles.mediumStyledContainer]: size === ButtonSize.Medium,
+    [styles.largeStyledContainer]: size === ButtonSize.Large,
+    [styles.xsmallDesktopContainer]: desktopSize === ButtonSize.XSmall,
+    [styles.smallDesktopContainer]: desktopSize === ButtonSize.Small,
+    [styles.mediumDesktopContainer]: desktopSize === ButtonSize.Medium,
+    [styles.largeDesktopContainer]: desktopSize === ButtonSize.Large,
+  };
+
   // if href was passed and also the button is not disabled.
   if (href && !disabled) {
     return (
       <Link href={href} passHref>
         <a className={styles.anchor}>
-          <button
-            type="button"
-            className={classNames(styles.styledContainer, {
-              [styles.hoverableContainer]: !disabled,
-              [styles.disabledContainer]: disabled,
-              [styles.xsmallStyledContainer]: size === ButtonSize.XSmall,
-              [styles.smallStyledContainer]: size === ButtonSize.Small,
-              [styles.mediumStyledContainer]: size === ButtonSize.Medium,
-              [styles.largeStyledContainer]: size === ButtonSize.Large,
-            })}
-          >
+          <button type="button" className={classNames(styles.styledContainer, classes)}>
             {children}
           </button>
         </a>
@@ -45,25 +56,28 @@ const Container = ({ children, disabled, size, href, onClick }) => {
   }
 
   return (
-    <button
-      type="button"
-      onClick={onClick}
-      className={classNames(styles.styledContainer, {
-        [styles.hoverableContainer]: !disabled,
-        [styles.disabledContainer]: disabled,
-        [styles.xsmallStyledContainer]: size === ButtonSize.XSmall,
-        [styles.smallStyledContainer]: size === ButtonSize.Small,
-        [styles.mediumStyledContainer]: size === ButtonSize.Medium,
-        [styles.largeStyledContainer]: size === ButtonSize.Large,
-      })}
-    >
+    <button type="button" onClick={onClick} className={classNames(styles.styledContainer, classes)}>
       {children}
     </button>
   );
 };
 
-const Button = ({ size = ButtonSize.Medium, text, disabled, href, icon, onClick }: ButtonProps) => (
-  <Container disabled={disabled} size={size} href={href} onClick={onClick}>
+const Button = ({
+  size = ButtonSize.Medium,
+  text,
+  disabled,
+  href,
+  icon,
+  onClick,
+  desktopSize,
+}: ButtonProps) => (
+  <Container
+    disabled={disabled}
+    size={size}
+    href={href}
+    onClick={onClick}
+    desktopSize={desktopSize}
+  >
     {icon && <div className={styles.iconContainer}>{icon}</div>}
     {text}
   </Container>
