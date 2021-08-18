d226e4b11834b87b6bbc1f72a069be3a088adfb4
Muhammad Muhajir <muhajirframe@gmail.com>
2021-08-18 17:58:01 +0700

    highlight verse

--- src/components/QuranReader/TranslationView/index.tsx
+++ src/components/QuranReader/TranslationView/index.tsx
@@ -5,46 +5,65 @@ import Link from 'next/link';
 import VerseActions from 'src/components/Verse/VerseActions';
 import classNames from 'classnames';
 import ChapterHeader from 'src/components/chapters/ChapterHeader';
+import { useSelector } from 'react-redux';
 import Verse from '../../../../types/Verse';
 import VerseText from '../../Verse/VerseText';
 import Translation from '../../../../types/Translation';
 import styles from './TranslationView.module.scss';
+import selectHighlightedVerse from '../selectIsVerseHighlighted';
 
 type TranslationViewProps = {
   verses: Verse[];
   quranReaderStyles: QuranReaderStyles;
 };
 
+const RenderVerse = ({
+  verse,
+  quranReaderStyles,
+}: {
+  verse: Verse;
+  quranReaderStyles: QuranReaderStyles;
+}) => {
+  const isVerseHighlighted = useSelector((state) => selectHighlightedVerse(state, verse));
+  return (
+    <div key={verse.id}>
+      {verse.verseNumber === 1 && <ChapterHeader chapterId={String(verse.chapterId)} />}
+      <div
+        className={classNames({
+          [styles.highlightedContainer]: isVerseHighlighted,
+        })}
+      >
+        <Link
+          as={`/${verse.chapterId}/${verse.verseNumber}`}
+          href="/[chapterId]/[verseId]"
+          passHref
+        >
+          <p className={styles.verseLink}>{verse.verseKey}</p>
+        </Link>
+        <VerseActions verse={verse} />
+        <VerseText words={verse.words} timestampSegments={verse.timestamps.segments} />
+        {verse.translations?.map((translation: Translation) => (
+          <div key={translation.id}>
+            <div
+              className={classNames(
+                styles.text,
+                styles[`translation-font-size-${quranReaderStyles.translationFontScale}`],
+              )}
+              dangerouslySetInnerHTML={{ __html: translation.text }}
+            />
+            <p className={styles.translationName}>— {translation.resourceName}</p>
+          </div>
+        ))}
+        <hr />
+      </div>
+    </div>
+  );
+};
+
 const TranslationView = ({ verses, quranReaderStyles }: TranslationViewProps) => (
   <div className={styles.container}>
     {verses.map((verse) => (
-      <div key={verse.id}>
-        {verse.verseNumber === 1 && <ChapterHeader chapterId={String(verse.chapterId)} />}
-        <div className={classNames({ [styles.highlightedContainer]: false })}>
-          <Link
-            as={`/${verse.chapterId}/${verse.verseNumber}`}
-            href="/[chapterId]/[verseId]"
-            passHref
-          >
-            <p className={styles.verseLink}>{verse.verseKey}</p>
-          </Link>
-          <VerseActions verse={verse} />
-          <VerseText words={verse.words} timestampSegments={verse.timestamps.segments} />
-          {verse.translations?.map((translation: Translation) => (
-            <div key={translation.id}>
-              <div
-                className={classNames(
-                  styles.text,
-                  styles[`translation-font-size-${quranReaderStyles.translationFontScale}`],
-                )}
-                dangerouslySetInnerHTML={{ __html: translation.text }}
-              />
-              <p className={styles.translationName}>— {translation.resourceName}</p>
-            </div>
-          ))}
-          <hr />
-        </div>
-      </div>
+      <RenderVerse verse={verse} key={verse.id} quranReaderStyles={quranReaderStyles} />
     ))}
   </div>
 );
