394798c93949cb9e8542c4b96684032175e0df83
Muhammad Muhajir <muhajirframe@gmail.com>
2021-08-22 14:43:05 +0700

    remove unused components (#287)

--- src/components/dls/Cards/Card.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Card from './Card';
-
-export default {
-  title: 'dls|Cards',
-};
-
-export const normal = () => <Card title="Title" subtitle="Subtitle" image="/sample-image.png" />;
