75f29274ce26b8ebe9a9b2b3bca042e28dc47967
Abdellatif Abdelfattah <tifa3333@gmail.com>
2021-08-23 09:45:35 +0200

    Add Google Analytics Script (#267)
    * Add Google Analytics Script
    
    * Add Google Analytics Env
    
    * update Google tag manager link
    
    * update env.example
    
    * enable analytics only in prod

---
+++ src/components/ThirdPartyScripts/ThirdPartyScripts.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import GoogleAnalyticsScript from './GoogleAnalyticsScript';
+
+const ThirdPartyScripts = () => (
+  <>
+    <GoogleAnalyticsScript />
+  </>
+);
+
+export default ThirdPartyScripts;
