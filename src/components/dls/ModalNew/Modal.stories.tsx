677c7c02c5dc47b98657475685a71731a6b97f8a
Muhammad Muhajir <muhajirframe@gmail.com>
2021-08-25 16:39:23 +0700

    update API

--- src/components/dls/ModalNew/Modal.stories.tsx
+++ src/components/dls/ModalNew/Modal.stories.tsx
@@ -1,30 +1,30 @@
 /* eslint-disable react/button-has-type */
 import Button from '../Button/Button';
-import { Modal, Trigger, Action, Actions, Title, Body, Subtitle } from './Modal';
+import Modal from './Modal';
 
 export default {
   title: 'dls/Modal',
 };
 
 export const Default = () => (
-  <Modal trigger={<Trigger as={Button}>test</Trigger>}>
-    <Body>
+  <Modal trigger={<Modal.Trigger as={Button}>test</Modal.Trigger>}>
+    <Modal.Body>
       <p>test</p>
       <button>aa</button>
-    </Body>
+    </Modal.Body>
   </Modal>
 );
 
 export const WithAction = () => (
-  <Modal trigger={<Trigger>test</Trigger>}>
-    <Body>
-      <Title>MODAL</Title>
-      <Subtitle>THIS IS A MODAL</Subtitle>
+  <Modal trigger={<Modal.Trigger>test</Modal.Trigger>}>
+    <Modal.Body>
+      <Modal.Title>MODAL</Modal.Title>
+      <Modal.Subtitle>THIS IS A MODAL</Modal.Subtitle>
       <p>Some content contained within this modal</p>
-    </Body>
-    <Actions>
-      <Action>Cancel</Action>
-      <Action>Submit</Action>
-    </Actions>
+    </Modal.Body>
+    <Modal.Actions>
+      <Modal.Action>Cancel</Modal.Action>
+      <Modal.Action>Submit</Modal.Action>
+    </Modal.Actions>
   </Modal>
 );
--- src/components/dls/ModalNew/Modal.tsx
+++ src/components/dls/ModalNew/Modal.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import styles from './Modal.module.scss';
 
-const Root = ({ children, trigger }) => (
+const Modal = ({ children, trigger }) => (
   <DialogPrimitive.Root>
     <DialogPrimitive.Overlay className={styles.overlay} />
     {trigger}
@@ -10,25 +10,30 @@ const Root = ({ children, trigger }) => (
   </DialogPrimitive.Root>
 );
 
-export const Modal = Root;
-export const { Trigger } = DialogPrimitive;
-export const Content = React.forwardRef<HTMLDivElement, any>(
-  ({ children, ...props }, forwardedRef) => (
-    <DialogPrimitive.Content {...props} ref={forwardedRef} className={styles.content}>
-      {children}
-    </DialogPrimitive.Content>
-  ),
-);
+const Content = React.forwardRef<HTMLDivElement, any>(({ children, ...props }, forwardedRef) => (
+  <DialogPrimitive.Content {...props} ref={forwardedRef} className={styles.content}>
+    {children}
+  </DialogPrimitive.Content>
+));
 
-export const Body = ({ children }) => <div className={styles.body}>{children}</div>;
-export const Title = ({ children }) => (
+const Body = ({ children }) => <div className={styles.body}>{children}</div>;
+const Title = ({ children }) => (
   <DialogPrimitive.Title className={styles.title}>{children}</DialogPrimitive.Title>
 );
-export const Subtitle = ({ children }) => (
+const Subtitle = ({ children }) => (
   <DialogPrimitive.Description className={styles.subtitle}>{children}</DialogPrimitive.Description>
 );
 
-export const Actions = ({ children }) => <div className={styles.actionsContainer}>{children}</div>;
-export const Action = ({ children }) => (
+const Actions = ({ children }) => <div className={styles.actionsContainer}>{children}</div>;
+const Action = ({ children }) => (
   <DialogPrimitive.Close className={styles.action}>{children}</DialogPrimitive.Close>
 );
+
+Modal.Trigger = DialogPrimitive.Trigger;
+Modal.Body = Body;
+Modal.Title = Title;
+Modal.Subtitle = Subtitle;
+Modal.Actions = Actions;
+Modal.Action = Action;
+
+export default Modal;
