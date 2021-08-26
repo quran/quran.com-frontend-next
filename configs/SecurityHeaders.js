24a67ede8d58ed6a53d2bad65cdcfacc5eccf235
Abdellatif Abdelfattah <tifa3333@gmail.com>
2021-08-26 19:47:29 +0200

    Update our CSP configuration for Vercel Analytics (#312)

--- configs/SecurityHeaders.js
+++ configs/SecurityHeaders.js
@@ -11,7 +11,7 @@
 */
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://ssl.google-analytics.com;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://ssl.google-analytics.com https://vitals.vercel-insights.com;
   style-src 'self' 'unsafe-inline';
   img-src * data:;
   media-src 'self' *.quranicaudio.com *.qurancdn.com https://qurancdn.com;
