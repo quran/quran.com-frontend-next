9c8881eb2a220976a4b03616cea5b94df12de07b
Muhammad Muhajir <muhajirframe@gmail.com>
2021-08-27 18:48:16 +0700

    share -> download in audioplayer

--- src/components/AudioPlayer/PlaybackControls.tsx
+++ src/components/AudioPlayer/PlaybackControls.tsx
@@ -1,18 +1,20 @@
 import { useSelector } from 'react-redux';
-import { selectIsPlaying } from 'src/redux/slices/AudioPlayer/state';
+import { selectAudioFile, selectIsPlaying } from 'src/redux/slices/AudioPlayer/state';
 import { withStopPropagation } from 'src/utils/event';
+import download from 'src/utils/download';
 import styles from './PlaybackControls.module.scss';
 import PlayIcon from '../../../public/icons/play-arrow.svg';
 import ForwardIcon from '../../../public/icons/forward_10.svg';
 import PauseIcon from '../../../public/icons/pause.svg';
 import ReplayIcon from '../../../public/icons/replay_10.svg';
-import ShareIcon from '../../../public/icons/share.svg';
+import DownloadIcon from '../../../public/icons/download.svg';
 import RepeatIcon from '../../../public/icons/ic_repeat_24px 1.svg';
 import Button, { ButtonShape, ButtonSize, ButtonVariant } from '../dls/Button/Button';
 import { triggerPauseAudio, triggerPlayAudio, triggerSeek } from './EventTriggers';
 
 const PlaybackControls = () => {
   const isPlaying = useSelector(selectIsPlaying);
+  const audioFile = useSelector(selectAudioFile);
   return (
     <div className={styles.container}>
       <Button variant={ButtonVariant.Ghost} shape={ButtonShape.Circle} size={ButtonSize.Large}>
@@ -53,8 +55,13 @@ const PlaybackControls = () => {
       >
         <ForwardIcon />
       </Button>
-      <Button variant={ButtonVariant.Ghost} shape={ButtonShape.Circle} size={ButtonSize.Large}>
-        <ShareIcon />
+      <Button
+        onClick={withStopPropagation(() => download(audioFile.audioUrl))}
+        variant={ButtonVariant.Ghost}
+        shape={ButtonShape.Circle}
+        size={ButtonSize.Large}
+      >
+        <DownloadIcon />
       </Button>
     </div>
   );
