281bb57ad9e0ec9a5fd356023307e64f859c5523
Muhammad Muhajir <muhajirframe@gmail.com>
2021-08-27 18:19:05 +0700

    add argTypes

--- src/components/dls/Spinner/Spinner.stories.tsx
+++ src/components/dls/Spinner/Spinner.stories.tsx
@@ -1,8 +1,16 @@
 import Spinner, { SpinnerSize } from './Spinner';
 
 export default {
-  title: 'dls/Popover',
+  title: 'dls/Spinner',
   component: Spinner,
+  argTypes: {
+    size: {
+      defaultValue: SpinnerSize.Medium,
+      description: `[OPTIONAL] The size of the button`,
+      options: Object.values(SpinnerSize),
+      control: { type: 'radio' },
+    },
+  },
 };
 
 export const Small = () => <Spinner size={SpinnerSize.Small} />;
