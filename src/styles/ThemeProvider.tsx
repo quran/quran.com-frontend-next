7ede32f23abe10235cf65b107035c4eb517e76de
Abdellatif <tifa3333@gmail.com>
2021-08-28 12:31:20 +0200

    make children a descendant of ThemeProvider

--- src/styles/ThemeProvider.tsx
+++ src/styles/ThemeProvider.tsx
@@ -1,15 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectTheme } from 'src/redux/slices/theme';
 import styles from './ThemeProvider.module.scss';
 
 const ThemeProvider = ({ children }) => {
   const theme = useSelector(selectTheme);
-  useEffect(() => {
-    if (theme) {
-      document.body.setAttribute('data-theme', theme.type);
-    }
-  }, [theme]);
+
+  if (typeof window !== 'undefined' && document.body) {
+    document.body.setAttribute('data-theme', theme.type);
+  }
 
   return <div className={styles.container}>{children}</div>;
 };
