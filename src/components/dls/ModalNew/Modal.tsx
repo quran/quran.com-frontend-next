4c2b893b174e89a879fff30481fd3ccba825c3b9
osamasayed <osamasayed@users.noreply.github.com>
2021-08-28 16:41:16 +0700

    Fix advanced copy item styling (#328)

--- src/components/dls/ModalNew/Modal.tsx
+++ src/components/dls/ModalNew/Modal.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
+import classNames from 'classnames';
 import styles from './Modal.module.scss';
 
 type ModalProps = {
   children: React.ReactNode;
   trigger?: React.ReactNode;
+  triggerClassName?: string;
   open?: boolean;
   onClickOutside?: () => void;
 };
-const Modal = ({ children, trigger, open, onClickOutside }: ModalProps) => (
+const Modal = ({ children, trigger, open, onClickOutside, triggerClassName }: ModalProps) => (
   <DialogPrimitive.Root open={open}>
     <DialogPrimitive.Overlay className={styles.overlay} />
     {trigger && (
-      <DialogPrimitive.Trigger as="div" className={styles.trigger}>
+      <DialogPrimitive.Trigger
+        as="div"
+        className={classNames(styles.trigger, { [triggerClassName]: triggerClassName })}
+      >
         {trigger}
       </DialogPrimitive.Trigger>
     )}
