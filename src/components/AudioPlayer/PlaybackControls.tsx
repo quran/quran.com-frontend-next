afbea8af0aa29edfa987a9fb2c1a3e2dca3bfc67
Muhammad Muhajir <muhajirframe@gmail.com>
2021-08-29 13:02:43 +0700

    add loading state to play pause audio player

--- src/components/AudioPlayer/PlaybackControls.tsx
+++ src/components/AudioPlayer/PlaybackControls.tsx
@@ -1,61 +1,38 @@
-import { useSelector } from 'react-redux';
-import { selectIsPlaying } from 'src/redux/slices/AudioPlayer/state';
 import { withStopPropagation } from 'src/utils/event';
-import styles from './PlaybackControls.module.scss';
-import PlayIcon from '../../../public/icons/play-arrow.svg';
-import ForwardIcon from '../../../public/icons/forward_10.svg';
-import PauseIcon from '../../../public/icons/pause.svg';
-import ReplayIcon from '../../../public/icons/replay_10.svg';
-import RepeatIcon from '../../../public/icons/ic_repeat_24px 1.svg';
-import Button, { ButtonShape, ButtonSize, ButtonVariant } from '../dls/Button/Button';
-import { triggerPauseAudio, triggerPlayAudio, triggerSeek } from './EventTriggers';
-import DownloadAudioButton from './DownloadAudioButton';
 
-const PlaybackControls = () => {
-  const isPlaying = useSelector(selectIsPlaying);
-  return (
-    <div className={styles.container}>
-      <Button variant={ButtonVariant.Ghost} shape={ButtonShape.Circle} size={ButtonSize.Large}>
-        <RepeatIcon />
-      </Button>
-      <Button
-        variant={ButtonVariant.Ghost}
-        shape={ButtonShape.Circle}
-        size={ButtonSize.Large}
-        onClick={withStopPropagation(() => triggerSeek(-10))}
-      >
-        <ReplayIcon />
-      </Button>
-      {isPlaying ? (
-        <Button
-          variant={ButtonVariant.Ghost}
-          shape={ButtonShape.Circle}
-          size={ButtonSize.Large}
-          onClick={withStopPropagation(triggerPauseAudio)}
-        >
-          <PauseIcon />
-        </Button>
-      ) : (
-        <Button
-          variant={ButtonVariant.Ghost}
-          shape={ButtonShape.Circle}
-          size={ButtonSize.Large}
-          onClick={withStopPropagation(triggerPlayAudio)}
-        >
-          <PlayIcon />
-        </Button>
-      )}
-      <Button
-        variant={ButtonVariant.Ghost}
-        shape={ButtonShape.Circle}
-        size={ButtonSize.Large}
-        onClick={withStopPropagation(() => triggerSeek(10))}
-      >
-        <ForwardIcon />
-      </Button>
-      <DownloadAudioButton />
-    </div>
-  );
-};
+import ForwardIcon from '../../../public/icons/forward_10.svg';
+import RepeatIcon from '../../../public/icons/ic_repeat_24px 1.svg';
+import ReplayIcon from '../../../public/icons/replay_10.svg';
+import Button, { ButtonShape, ButtonSize, ButtonVariant } from '../dls/Button/Button';
+import DownloadAudioButton from './DownloadAudioButton';
+import { triggerSeek } from './EventTriggers';
+import styles from './PlaybackControls.module.scss';
+import PlayPauseButton from './PlayPauseButton';
+
+const PlaybackControls = () => (
+  <div className={styles.container}>
+    <Button variant={ButtonVariant.Ghost} shape={ButtonShape.Circle} size={ButtonSize.Large}>
+      <RepeatIcon />
+    </Button>
+    <Button
+      variant={ButtonVariant.Ghost}
+      shape={ButtonShape.Circle}
+      size={ButtonSize.Large}
+      onClick={withStopPropagation(() => triggerSeek(-10))}
+    >
+      <ReplayIcon />
+    </Button>
+    <PlayPauseButton />
+    <Button
+      variant={ButtonVariant.Ghost}
+      shape={ButtonShape.Circle}
+      size={ButtonSize.Large}
+      onClick={withStopPropagation(() => triggerSeek(10))}
+    >
+      <ForwardIcon />
+    </Button>
+    <DownloadAudioButton />
+  </div>
+);
 
 export default PlaybackControls;
