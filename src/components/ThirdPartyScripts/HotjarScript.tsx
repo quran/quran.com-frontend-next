bd2ce548df51d80d3b7dc91864520c79cabc73b9
Abdellatif Abdelfattah <tifa3333@gmail.com>
2021-08-30 16:10:40 +0200

    Enable hotjar only on production (#340)

--- src/components/ThirdPartyScripts/HotjarScript.tsx
+++ src/components/ThirdPartyScripts/HotjarScript.tsx
@@ -1,8 +1,15 @@
 import Script from 'next/script';
 
-const HotjarScript = () => (
-  <Script strategy="lazyOnload">
-    {`(function(h,o,t,j,a,r){
+const ENABLE_HOTJAR = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production';
+
+const HotjarScript = () => {
+  if (!ENABLE_HOTJAR) {
+    return <></>;
+  }
+
+  return (
+    <Script strategy="lazyOnload">
+      {`(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:2578202,hjsv:6};
         a=o.getElementsByTagName('head')[0];
@@ -10,7 +17,8 @@ const HotjarScript = () => (
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-  </Script>
-);
+    </Script>
+  );
+};
 
 export default HotjarScript;
