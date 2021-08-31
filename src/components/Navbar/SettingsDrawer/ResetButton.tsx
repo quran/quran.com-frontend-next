d158f04532cac9f4601815654524760d4d2d2f74
Muhammad Muhajir <muhajirframe@gmail.com>
2021-08-31 14:28:23 +0700

    update doc

--- src/components/Navbar/SettingsDrawer/ResetButton.tsx
+++ src/components/Navbar/SettingsDrawer/ResetButton.tsx
@@ -4,7 +4,7 @@ import Button from 'src/components/dls/Button/Button';
 
 // reset button will dispatch a `reset` action
 // reducers will listen to this action
-// for example, check slice/theme.ts. it has extra reducer that listens to `reset` action
+// for example, check slices/theme.ts. it has extra reducer that listens to `reset` action
 const ResetButton = () => {
   const dispatch = useDispatch();
   return <Button onClick={() => dispatch(reset())}>Reset settings</Button>;
