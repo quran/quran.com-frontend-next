00ee7aefbcc40ce27334c5d3ac7494036f622f85
Muhammad Muhajir <muhajirframe@gmail.com>
2021-08-31 14:27:55 +0700

    updae resetButton docs

--- src/components/Navbar/SettingsDrawer/ResetButton.tsx
+++ src/components/Navbar/SettingsDrawer/ResetButton.tsx
@@ -3,7 +3,7 @@ import reset from 'src/redux/slices/reset';
 import Button from 'src/components/dls/Button/Button';
 
 // reset button will dispatch a `reset` action
-// and reducer that is `reset-able` will listen to this action
+// reducers will listen to this action
 // for example, check slice/theme.ts. it has extra reducer that listens to `reset` action
 const ResetButton = () => {
   const dispatch = useDispatch();
