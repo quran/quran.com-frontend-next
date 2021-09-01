52c59b5b77428ff0c44255b75be1f2b9ba91a76d
Muhammad Muhajir <muhajirframe@gmail.com>
2021-09-01 15:43:41 +0700

    update settings style

--- src/components/Navbar/SettingsDrawer/Section.tsx
+++ src/components/Navbar/SettingsDrawer/Section.tsx
@@ -1,12 +1,12 @@
 import classNames from 'classnames';
 import styles from './Section.module.scss';
 
-const Section = ({ children }) => <h2 className={styles.section}>{children}</h2>;
+const Section = ({ children }) => <div className={styles.section}>{children}</div>;
 const Title = ({ children }) => <div className={styles.title}>{children}</div>;
 const Label = ({ children }) => <div className={styles.label}>{children}</div>;
 const Footer = ({ children, visible = true }) => (
   <div
-    className={classNames(styles.description, {
+    className={classNames(styles.footer, {
       [styles.invisible]: !visible,
     })}
   >
