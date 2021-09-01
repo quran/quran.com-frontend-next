2bd35b192f4a40cfa9c3c8fa867b038c926cf98c
Muhammad Muhajir <muhajirframe@gmail.com>
2021-09-01 13:25:19 +0700

    update wrapper referecen

--- src/components/dls/Wrapper/Wrapper.tsx
+++ src/components/dls/Wrapper/Wrapper.tsx
@@ -5,7 +5,7 @@
  * @param props.condition - The condition to wrap the children with the wrapper.
  * @param props.wrapper - The wrapper component.
  *
- * Reference: https://reactjs.org/docs/conditional-renderi
+ * Reference:
  * - https://blog.hackages.io/conditionally-wrap-an-element-in-react-a8b9a47fab2ng.html
  * - https://arjayosma.com/how-to-conditionally-wrap-a-react-component/
  */
