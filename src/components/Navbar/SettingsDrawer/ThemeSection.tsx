56e7c2afa645f7200a8c86edbeea34e31bb5f3d3
osamasayed <osamasayed@users.noreply.github.com>
2021-09-02 14:16:59 +0700

    add Select to DLS and replace Combobox with Select (#359)

--- src/components/Navbar/SettingsDrawer/ThemeSection.tsx
+++ src/components/Navbar/SettingsDrawer/ThemeSection.tsx
@@ -1,18 +1,8 @@
 import { useDispatch, useSelector } from 'react-redux';
-import Combobox from 'src/components/dls/Forms/Combobox';
-import { selectTheme, setTheme } from 'src/redux/slices/theme';
+import Select from 'src/components/dls/Forms/Select';
+import { selectTheme, setTheme, ThemeType } from 'src/redux/slices/theme';
+import { generateSelectOptions } from 'src/utils/input';
 import Section from './Section';
-
-// TODO: internationalize label
-const themes = [
-  { id: 'system', value: 'system', label: 'System', name: 'system' },
-  { id: 'light', value: 'light', label: 'Light', name: 'light' },
-  { id: 'dark', value: 'dark', label: 'Dark', name: 'dark' },
-  { id: 'sepia', value: 'sepia', label: 'Sepia', name: 'sepia' },
-];
-
-// given the id of a theme, return the label. data from the themes array above
-const getLabelById = (id: string) => themes.find((theme) => theme.id === id)?.label;
 
 const ThemeSection = () => {
   const dispatch = useDispatch();
@@ -22,21 +12,27 @@ const ThemeSection = () => {
       <Section.Title>Theme</Section.Title>
       <Section.Row>
         <Section.Label>Mode</Section.Label>
-        <div>
-          <Combobox
-            id="theme-section"
-            value={theme.type}
-            initialInputValue={getLabelById(theme.type)}
-            items={themes}
-            onChange={(value) => dispatch({ type: setTheme.type, payload: value })}
-          />
-        </div>
+        <Select
+          id="theme-section"
+          name="theme"
+          options={themes}
+          value={theme.type}
+          onChange={(value) => dispatch({ type: setTheme.type, payload: value })}
+        />
       </Section.Row>
-      <Section.Footer visible={theme.type === 'system'}>
+      <Section.Footer visible={theme.type === ThemeType.System}>
         The system theme automatically adopts to your light/dark mode settings
       </Section.Footer>
     </Section>
   );
 };
+
+// TODO: internationalize label
+const themes = generateSelectOptions([
+  ThemeType.System,
+  ThemeType.Light,
+  ThemeType.Dark,
+  ThemeType.Sepia,
+]);
 
 export default ThemeSection;
