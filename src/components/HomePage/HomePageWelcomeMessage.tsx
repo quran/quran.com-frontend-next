181565cb5402598ad15ddb444dadcbf42e4a207c
Abdellatif <tifa3333@gmail.com>
2021-09-03 20:19:16 +0200

    replace feedback fish with upvoty

--- src/components/HomePage/HomePageWelcomeMessage.tsx
+++ src/components/HomePage/HomePageWelcomeMessage.tsx
@@ -1,4 +1,3 @@
-import { FeedbackFish } from '@feedback-fish/react';
 import React from 'react';
 import styles from './HomePageWelcomeMessage.module.scss';
 
@@ -7,9 +6,9 @@ const HomePageWelcomeMessage = () => (
     <p>
       Welcome to the pre-release version of the new Quran.com! We are actively adding new features
       and pushing changes. Please give us your{' '}
-      <FeedbackFish projectId="3649f32ef147eb">
-        <a>feedback here</a>
-      </FeedbackFish>
+      <a href="https://quran.upvoty.com" target="_blank" rel="noreferrer">
+        feedback here
+      </a>
       , and join the #beta channel on our{' '}
       <a href="https://discord.gg/H62eG8ss" target="blank">
         discord server
