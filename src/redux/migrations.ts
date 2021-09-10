08218b3bd6c5903b622cd32b860aabdb097de832
osamasayed <osama.sayed.abdelazim@gmail.com>
2021-09-10 14:17:26 +0700

    update migrations

--- src/redux/migrations.ts
+++ src/redux/migrations.ts
@@ -1,16 +1,9 @@
 export default {
-  1: (state) => ({
-    ...state,
-    audioPlayerState: {
-      ...state.audioPlayerState,
-      isMinimized: false,
-    },
-  }),
   2: (state) => ({
     ...state,
     audioPlayerState: {
       ...state.audioPlayerState,
-      isMinimized: state.audioPlayerState.isMinimized,
+      isMinimized: false,
     },
   }),
 };
