b539591f8767f09782e35db3dad70bb7d06dedad
Muhammad Muhajir <muhajirframe@gmail.com>
2021-09-13 11:36:39 +0700

    add reference

--- src/hooks/redux.ts
+++ src/hooks/redux.ts
@@ -1,6 +1,8 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from 'src/redux/store';
 
+// Reference: https://redux.js.org/usage/usage-with-typescript
+
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
