6a55bfae7c7b533976f478b5061bc383740965e1
Muhammad Muhajir <muhajirframe@gmail.com>
2021-09-13 11:41:18 +0700

    update comment

--- src/hooks/redux.ts
+++ src/hooks/redux.ts
@@ -3,6 +3,6 @@ import { AppDispatch, RootState } from 'src/redux/store';
 
 // Reference: https://redux.js.org/usage/usage-with-typescript
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
+// We use this throughout our codebase instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -72,7 +72,6 @@ const store = configureStore({
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
 export const persistor = persistStore(store);
