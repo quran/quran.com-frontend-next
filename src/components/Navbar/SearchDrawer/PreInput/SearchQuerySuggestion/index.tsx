e55a7c706a7c9f3188fd32caff2c71bb19ccf9dc
osamasayed <osama.sayed.abdelazim@gmail.com>
2021-09-13 10:59:46 +0700

    Add remove recent search query button

--- src/components/Navbar/SearchDrawer/PreInput/SearchQuerySuggestion/index.tsx
+++ src/components/Navbar/SearchDrawer/PreInput/SearchQuerySuggestion/index.tsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { MouseEvent, KeyboardEvent } from 'react';
 import styles from './SearchQuerySuggestion.module.scss';
+import IconClose from '../../../../../../public/icons/close.svg';
 
 interface Props {
   searchQuery: string;
   onSearchKeywordClicked: (searchQuery: string) => void;
+  onRemoveSearchQueryClicked?: (searchQuery: string) => void;
 }
 
-const SearchQuerySuggestion: React.FC<Props> = ({ searchQuery, onSearchKeywordClicked }) => (
-  <button
-    type="button"
-    className={styles.container}
-    onClick={() => onSearchKeywordClicked(searchQuery)}
-  >
-    {searchQuery}
-  </button>
-);
+const SearchQuerySuggestion: React.FC<Props> = ({
+  searchQuery,
+  onSearchKeywordClicked,
+  onRemoveSearchQueryClicked,
+}) => {
+  const onRemoveClicked = (
+    event: MouseEvent<HTMLElement> | KeyboardEvent<HTMLElement>,
+    toBeDeletedSearchQuery: string,
+  ) => {
+    event.stopPropagation();
+    onRemoveSearchQueryClicked(toBeDeletedSearchQuery);
+  };
+
+  return (
+    <button
+      type="button"
+      className={styles.container}
+      onClick={() => onSearchKeywordClicked(searchQuery)}
+    >
+      <p>{searchQuery}</p>
+      {onRemoveSearchQueryClicked && (
+        <div
+          role="button"
+          tabIndex={0}
+          className={styles.removeIcon}
+          onKeyPress={(event) => onRemoveClicked(event, searchQuery)}
+          onClick={(event) => onRemoveClicked(event, searchQuery)}
+        >
+          <IconClose />
+        </div>
+      )}
+    </button>
+  );
+};
 
 export default SearchQuerySuggestion;
--- src/components/Navbar/SearchDrawer/SearchHistory/index.tsx
+++ src/components/Navbar/SearchDrawer/SearchHistory/index.tsx
@@ -1,7 +1,6 @@
-/* eslint-disable react/no-array-index-key */
-import React from 'react';
-import { selectSearchHistory } from 'src/redux/slices/Search/search';
-import { useSelector } from 'react-redux';
+import React, { useCallback } from 'react';
+import { removeSearchHistoryRecord, selectSearchHistory } from 'src/redux/slices/Search/search';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './SearchHistory.module.scss';
 import Header from '../PreInput/Header';
 import SearchQuerySuggestion from '../PreInput/SearchQuerySuggestion';
@@ -12,6 +11,15 @@ interface Props {
 
 const SearchHistory: React.FC<Props> = ({ onSearchKeywordClicked }) => {
   const searchHistory = useSelector(selectSearchHistory);
+  const dispatch = useDispatch();
+
+  const onRemoveSearchQueryClicked = useCallback(
+    (searchQuery: string) => {
+      dispatch({ type: removeSearchHistoryRecord.type, payload: searchQuery });
+    },
+    [dispatch],
+  );
+
   // if there are no recent search queries.
   if (!searchHistory.length) {
     return <></>;
@@ -24,6 +32,7 @@ const SearchHistory: React.FC<Props> = ({ onSearchKeywordClicked }) => {
           searchQuery={recentSearchQuery}
           key={`${recentSearchQuery}`}
           onSearchKeywordClicked={onSearchKeywordClicked}
+          onRemoveSearchQueryClicked={onRemoveSearchQueryClicked}
         />
       ))}
     </div>
