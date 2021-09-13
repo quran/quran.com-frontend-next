7d617ff690a7ca06063e5aeca7694267c60e5ba4
osamasayed <osamasayed@users.noreply.github.com>
2021-09-13 18:45:26 +0700

    Add remove recent search query button (#419)
    * Add remove recent search query button
    
    * update styling
    
    * update styling

--- src/components/Navbar/SearchDrawer/PreInput/SearchQuerySuggestion/index.tsx
+++ src/components/Navbar/SearchDrawer/PreInput/SearchQuerySuggestion/index.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { MouseEvent, KeyboardEvent } from 'react';
 import styles from './SearchQuerySuggestion.module.scss';
 
 interface Props {
   searchQuery: string;
   onSearchKeywordClicked: (searchQuery: string) => void;
+  onRemoveSearchQueryClicked?: (searchQuery: string) => void;
 }
 
-const SearchQuerySuggestion: React.FC<Props> = ({ searchQuery, onSearchKeywordClicked }) => (
-  <button
-    type="button"
-    className={styles.container}
-    onClick={() => onSearchKeywordClicked(searchQuery)}
-  >
-    {searchQuery}
-  </button>
-);
+const SearchQuerySuggestion: React.FC<Props> = ({
+  searchQuery,
+  onSearchKeywordClicked,
+  onRemoveSearchQueryClicked,
+}) => {
+  const onRemoveClicked = (
+    event: MouseEvent<HTMLElement> | KeyboardEvent<HTMLElement>,
+    toBeDeletedSearchQuery: string,
+  ) => {
+    event.stopPropagation();
+    onRemoveSearchQueryClicked(toBeDeletedSearchQuery);
+  };
+
+  return (
+    <button
+      type="button"
+      className={styles.container}
+      onClick={() => onSearchKeywordClicked(searchQuery)}
+    >
+      <p>{searchQuery}</p>
+
+      {onRemoveSearchQueryClicked && (
+        <button
+          type="button"
+          className={styles.removeButton}
+          onClick={(event) => onRemoveClicked(event, searchQuery)}
+        >
+          Remove
+        </button>
+      )}
+    </button>
+  );
+};
 
 export default SearchQuerySuggestion;
