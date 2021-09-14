d04870d3a7baaca7ebab0e0a4ddbe84da6544881
Muhammad Muhajir <muhajirframe@gmail.com>
2021-09-14 14:15:42 +0700

    hot-fix-shallow-equal

--- src/components/QuranReader/TranslationView/BookmarkIcon.tsx
+++ src/components/QuranReader/TranslationView/BookmarkIcon.tsx
@@ -1,11 +1,10 @@
 import Button, { ButtonType } from 'src/components/dls/Button/Button';
-import { useSelector } from 'react-redux';
+import { shallowEqual, useSelector } from 'react-redux';
 import { selectBookmarks } from 'src/redux/slices/QuranReader/bookmarks';
-import { areArraysEqual } from 'src/utils/array';
 import StarIcon from '../../../../public/icons/star.svg';
 
 const BookmarkIcon = ({ verseKey }: { verseKey: string }) => {
-  const bookmarkedVerses = useSelector(selectBookmarks, areArraysEqual);
+  const bookmarkedVerses = useSelector(selectBookmarks, shallowEqual);
   const isVerseBookmarked = !!bookmarkedVerses[verseKey];
 
   if (!isVerseBookmarked) return null;
