6a820a21ef6a79a43135aab8d32b64d1e278a524
Muhammad Muhajir <muhajirframe@gmail.com>
2021-09-15 12:12:28 +0700

    update storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -24,12 +24,10 @@ export const globalTypes = {
   theme: {
     name: 'Theme',
     description: 'Global theme for components',
-    defaultValue: 'dark',
+    defaultValue: 'light',
     toolbar: {
       icon: 'circlehollow',
-      // Array of plain string values or MenuItem shape (see below)
       items: ['light', 'dark'],
-      // Property that specifies if the name of the item will be displayed
       showName: true,
     },
   },
