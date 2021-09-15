a49cf8353e76a336c8e06e3bf0bc06bee78fb847
osamasayed <osama.sayed.abdelazim@gmail.com>
2021-09-15 18:47:17 +0700

    Select multiple languages/translations on search page

--- src/components/Search/TranslationsFilter.tsx
+++ src/components/Search/TranslationsFilter.tsx
@@ -5,18 +5,25 @@ import Combobox from '../dls/Forms/Combobox';
 import styles from './Filter.module.scss';
 
 interface Props {
-  onTranslationChange: (translationId: string) => void;
-  selectedTranslation: string;
+  onTranslationChange: (translationId: string[]) => void;
+  selectedTranslations: string;
   translations: AvailableTranslation[];
 }
 
 const TranslationsFilter: React.FC<Props> = memo(
-  ({ translations, selectedTranslation, onTranslationChange }) => {
-    // we need to match the selectedTranslation because if it comes from the URL's query param and the user had entered an invalid languageCode in the url or if he didn't select a translation at all, we should set a different text
-    const matchedTranslation = translations.find(
-      (translation) => translation.id.toString() === selectedTranslation,
-    );
-    const initialInputValue = matchedTranslation ? matchedTranslation.translatedName.name : '';
+  ({ translations, selectedTranslations, onTranslationChange }) => {
+    /*
+      We need to only pick the translations that exist in the list of available translations
+      to avoid selecting non-existent items since the translations can come from the URL path
+      which the user can edit to enter invalid values.
+    */
+    const matchedTranslations = selectedTranslations
+      ? translations
+          .filter((translation) =>
+            selectedTranslations.split(',').includes(translation.id.toString()),
+          )
+          .map((translation) => translation.id.toString())
+      : [];
 
     const translationsItems = useMemo(
       () =>
@@ -34,11 +41,11 @@ const TranslationsFilter: React.FC<Props> = memo(
 
     return (
       <Combobox
+        isMultiSelect
         id="translationsFilter"
-        value={selectedTranslation || ''}
+        value={matchedTranslations}
         items={translationsItems}
         onChange={onTranslationChange}
-        initialInputValue={initialInputValue}
         placeholder="Select a translation"
         label={<div className={styles.dropdownLabel}>Translation</div>}
       />
