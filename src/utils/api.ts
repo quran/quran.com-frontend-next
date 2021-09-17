8874d23848802c989cf4ee3c4fc5b0ed5a252e65
Abdellatif Abdelfattah <tifa3333@gmail.com>
2021-09-17 10:09:38 +0200

    update default Mushaf text (#456)

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,7 @@ import { stringify } from 'qs';
 import { QuranFont } from 'src/components/QuranReader/types';
 
 export const ITEMS_PER_PAGE = 10;
-const DEFAULT_MUSHAF = 4; // King Fahad Quran Complex 15 Lines Hafs text.
+const DEFAULT_MUSHAF = 5; // KFGQPC HAFS Text
 
 const STAGING_API_HOST = 'https://staging.quran.com/api/qdc';
 const PRODUCTION_API_HOST = 'https://api.quran.com/api/qdc';
