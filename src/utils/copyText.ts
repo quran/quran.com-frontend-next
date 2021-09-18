a076fd085f199fb17f084b14fb960dd4fe2dfa95
Muhammad Muhajir <muhajirframe@gmail.com>
2021-09-18 14:22:25 +0700

    Update copyText.ts

--- src/utils/copyText.ts
+++ src/utils/copyText.ts
@@ -2,7 +2,7 @@ import clipboardCopy from 'clipboard-copy';
 
 const copyText = async (textBlobPromise: Promise<Blob>) => {
   try {
-    // Try to copy with promise value (works in safar)
+    // Try to copy with promise value (works in safari)
     navigator.clipboard.write([new ClipboardItem({ 'text/plain': textBlobPromise })]);
   } catch (e) {
     // otherwise fallback to use clipboardCopy library
