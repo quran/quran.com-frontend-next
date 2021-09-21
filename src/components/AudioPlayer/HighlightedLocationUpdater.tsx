0d43e6377c9c03d555a2b260cc07274566f85f04
Muhammad Muhajir <muhajirframe@gmail.com>
2021-09-21 15:09:59 +0700

    try memoizing value

--- src/components/AudioPlayer/HighlightedLocationUpdater.tsx
+++ src/components/AudioPlayer/HighlightedLocationUpdater.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
 import { shallowEqual, useDispatch } from 'react-redux';
 import useSWRImmutable from 'swr/immutable';
-
-import getHighlightedLocation from './getHighlightedLocation';
 
 import { getChapterAudioFile } from 'src/api';
 import {
@@ -11,6 +9,9 @@ import {
   setHighlightedLocation,
 } from 'src/redux/slices/QuranReader/highlightedLocation';
 import { makeChapterAudioFiles } from 'src/utils/apiPaths';
+import { getVerseAndChapterNumbersFromKey } from 'src/utils/verse';
+import AudioFile from 'types/AudioFile';
+import VerseTiming from 'types/VerseTiming';
 
 type AudioTimestampsHighlightListenerProps = {
   reciterId: number;
@@ -29,29 +30,91 @@ const HighlightedLocationUpdater = ({
   currentTime,
 }: AudioTimestampsHighlightListenerProps) => {
   const dispatch = useDispatch();
-  const { data } = useSWRImmutable(makeChapterAudioFiles(reciterId, chapterId, true), () =>
-    getChapterAudioFile(reciterId, chapterId, true),
+  const { data: audioFileData } = useSWRImmutable(
+    makeChapterAudioFiles(reciterId, chapterId, true),
+    () => getChapterAudioFile(reciterId, chapterId, true),
   );
+  const currentTimeInMs = currentTime * 1000;
 
   const lastHighlightedLocation = useRef(defaultHighlightedLocation);
 
-  // eslint-disable-next-line consistent-return
+  const currentHighlightedVerseTiming = useMemoizedHighlightedVerseTiming(
+    currentTimeInMs,
+    audioFileData,
+  );
+  const currentHighlightedWordLocation = useMemoizedHighlightedWordLocation(
+    currentTimeInMs,
+    currentHighlightedVerseTiming,
+  );
+
   useEffect(() => {
-    if (!data) return;
-
     const highlightedLocation = getHighlightedLocation(
-      window.audioPlayerEl.currentTime * 1000, // convert currentTime to milliseconds
-      data.verseTimings,
+      currentHighlightedVerseTiming,
+      currentHighlightedWordLocation,
     );
-
     // only update redux value if current highlight value does not equal prev value
     if (!shallowEqual(highlightedLocation, lastHighlightedLocation.current)) {
       dispatch(setHighlightedLocation(highlightedLocation));
       lastHighlightedLocation.current = highlightedLocation;
     }
-  }, [data, dispatch, currentTime]);
+  }, [currentHighlightedVerseTiming, currentHighlightedWordLocation, dispatch]);
 
   return null;
+};
+
+const useMemoizedHighlightedVerseTiming = (currentTime: number, audioFileData: AudioFile) => {
+  const lastHighlightedVerse = useRef<VerseTiming>(null);
+  if (!audioFileData) return null;
+
+  if (
+    lastHighlightedVerse.current &&
+    currentTime >= lastHighlightedVerse.current.timestampFrom &&
+    currentTime <= lastHighlightedVerse.current.timestampTo
+  )
+    return lastHighlightedVerse.current;
+
+  return audioFileData.verseTimings.find(
+    (verse) => currentTime >= verse.timestampFrom && currentTime <= verse.timestampTo,
+  );
+};
+
+type Segment = [number, number, number];
+const useMemoizedHighlightedWordLocation = (
+  currentTime: number,
+  currentHighlightedVerseTiming: VerseTiming,
+) => {
+  const lastHighlightedWordLocation = useRef<Segment>(null);
+  if (!currentHighlightedVerseTiming) return null;
+
+  if (lastHighlightedWordLocation.current) {
+    const [, timestampFrom, timestampTo] = lastHighlightedWordLocation.current;
+    if (currentTime >= timestampFrom && currentTime <= timestampTo)
+      return lastHighlightedWordLocation.current;
+  }
+
+  return currentHighlightedVerseTiming.segments.find((segment) => {
+    const [, timestampFrom, timestampTo] = segment; // the structure of the segment is: [wordLocation, timestampFrom, timestampTo]
+    return currentTime >= timestampFrom && currentTime <= timestampTo;
+  });
+};
+
+const getHighlightedLocation = (
+  currentHighlightedVerseTiming: VerseTiming,
+  currentHighlightedWordLocation: Segment,
+) => {
+  const highlightedVerseKey = currentHighlightedVerseTiming?.verseKey;
+  const highlightedLocation = { ...defaultHighlightedLocation };
+  if (highlightedVerseKey) {
+    const [chapter, verse] = getVerseAndChapterNumbersFromKey(highlightedVerseKey);
+    highlightedLocation.chapter = Number(chapter);
+    highlightedLocation.verse = Number(verse);
+  }
+  if (currentHighlightedWordLocation) {
+    const word = currentHighlightedWordLocation[0] + 1; // word location starts at 1 instead of 0, while the the data from API starts at 0
+    highlightedLocation.word = word;
+  }
+
+  return highlightedLocation;
 };
 
 export default HighlightedLocationUpdater;
