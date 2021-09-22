e1c3f588c1effba67494d3bf1cb8141702697856
Muhammad Muhajir <muhajirframe@gmail.com>
2021-09-22 18:13:32 +0700

    memoize TranslationViewCell

--- src/components/QuranReader/TranslationView/TranslationViewCell.tsx
+++ src/components/QuranReader/TranslationView/TranslationViewCell.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import classNames from 'classnames';
 import { shallowEqual, useSelector } from 'react-redux';
 
@@ -65,4 +67,4 @@ const TranslationViewCell = ({ verse, isHighlighted }: TranslationViewCellProps)
   );
 };
 
-export default TranslationViewCell;
+export default React.memo(TranslationViewCell);
