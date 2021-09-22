4b3de3cb0da1ab8e6bde899862ca4b37d0bd07d3
Muhammad Muhajir <muhajirframe@gmail.com>
2021-09-22 17:26:52 +0700

    Translation view : click bookmark icon to remove (#484)
    * toggle bookmark icon
    
    * add "remove bookmark" tooltip

--- src/components/QuranReader/TranslationView/BookmarkIcon.tsx
+++ src/components/QuranReader/TranslationView/BookmarkIcon.tsx
@@ -1,18 +1,23 @@
-import { shallowEqual, useSelector } from 'react-redux';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 
 import StarIcon from '../../../../public/icons/star.svg';
 
 import Button, { ButtonType } from 'src/components/dls/Button/Button';
-import { selectBookmarks } from 'src/redux/slices/QuranReader/bookmarks';
+import { selectBookmarks, toggleVerseBookmark } from 'src/redux/slices/QuranReader/bookmarks';
 
 const BookmarkIcon = ({ verseKey }: { verseKey: string }) => {
+  const dispatch = useDispatch();
   const bookmarkedVerses = useSelector(selectBookmarks, shallowEqual);
   const isVerseBookmarked = !!bookmarkedVerses[verseKey];
 
   if (!isVerseBookmarked) return null;
 
   return (
-    <Button type={ButtonType.Secondary}>
+    <Button
+      type={ButtonType.Secondary}
+      onClick={() => dispatch(toggleVerseBookmark(verseKey))}
+      tooltip="Remove bookmark"
+    >
       <StarIcon />
     </Button>
   );
