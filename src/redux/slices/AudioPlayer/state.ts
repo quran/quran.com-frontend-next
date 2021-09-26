a610a977d7b1cfd27f95fbf976fc5a20881fc8e2
Muhammad Muhajir <muhajirframe@gmail.com>
2021-09-26 13:59:46 +0700

    TranslationViewCell verse play/pause button (#489)
    * [WIP]: TranslationViewCell verse play/pause button
    
    * add loading support
    
    * remove console.log
    
    * Revert "add loading support"
    
    This reverts commit dc1b81fb003dd82fa6cd2a6331d5e38cc00182f0.
    
    * checkpoint
    
    * add loading state when changing reciter

--- src/redux/slices/AudioPlayer/state.ts
+++ src/redux/slices/AudioPlayer/state.ts
@@ -88,6 +88,7 @@ export const loadAndPlayAudioFile = createAsyncThunk<void, number, { state: Root
 export const setReciterAndPauseAudio = createAsyncThunk<void, Reciter, { state: RootState }>(
   'audioPlayerState/setReciterAndPlayAudio',
   async (reciter, thunkAPI) => {
+    thunkAPI.dispatch(setAudioStatus(AudioFileStatus.Loading));
     triggerPauseAudio();
 
     thunkAPI.dispatch(setReciter(reciter));
