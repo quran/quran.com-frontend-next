a8dda0443b14299ff77f7d6f1482872f7a9fa0f9
Abdellatif Abdelfattah <tifa3333@gmail.com>
2021-09-26 09:10:53 +0200

    Remove hotjar (#500)

--- src/components/ThirdPartyScripts/ThirdPartyScripts.tsx
+++ src/components/ThirdPartyScripts/ThirdPartyScripts.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 import GoogleAnalyticsScript from './GoogleAnalyticsScript';
-import HotjarScript from './HotjarScript';
 
 const ThirdPartyScripts = () => (
   <>
     <GoogleAnalyticsScript />
-    <HotjarScript />
   </>
 );
 
