97a83218918bf14c584c3d07a92bc3a016531a01
Muhammad Muhajir <muhajirframe@gmail.com>
2021-09-27 14:37:47 +0700

    when clicked from translationViewCell use page's chapterId

--- src/components/Verse/VerseActionRepeatAudio.tsx
+++ src/components/Verse/VerseActionRepeatAudio.tsx
@@ -1,32 +1,28 @@
 import { useState } from 'react';
 
-import { shallowEqual, useSelector } from 'react-redux';
-
 import RepeatIcon from '../../../public/icons/repeat.svg';
 
 import VerseActionsMenuItem from './VerseActionsMenuItem';
 
 import RepeatAudioModal, { RepeatType } from 'src/components/AudioPlayer/RepeatAudioModal';
-import { selectAudioFile } from 'src/redux/slices/AudioPlayer/state';
+import { getChapterNumberFromKey } from 'src/utils/verse';
 
 type VerseActionRepeatAudioProps = {
   verseKey: string;
 };
 const VerseActionRepeatAudio = ({ verseKey }: VerseActionRepeatAudioProps) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const audioFile = useSelector(selectAudioFile, shallowEqual);
+  const chapterId = getChapterNumberFromKey(verseKey);
 
   return (
     <>
-      {audioFile && (
-        <RepeatAudioModal
-          defaultRepeatType={RepeatType.Single}
-          defaultSelectedVerse={verseKey}
-          chapterId={audioFile?.chapterId.toString()}
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-        />
-      )}
+      <RepeatAudioModal
+        defaultRepeatType={RepeatType.Single}
+        defaultSelectedVerse={verseKey}
+        chapterId={chapterId.toString()}
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
       <VerseActionsMenuItem
         title="Repeat Verse"
         icon={<RepeatIcon />}
