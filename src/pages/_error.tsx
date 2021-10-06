1599554f87799d13b5dbf609e394fc6505bb4793
Muhammad Muhajir <muhajirframe@gmail.com>
2021-10-06 14:33:22 +0700

    update error page

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,9 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useRouter } from 'next/router';
+
 import styles from './_error.module.scss';
 
 import Link, { LinkVariant } from 'src/components/dls/Link/Link';
 
 // reference: https://nextjs.org/docs/advanced-features/custom-error-page#more-advanced-error-page-customizing
 const Error = () => {
+  const router = useRouter();
+
+  // if previous page url exist, go back, otherwise go to home
+  const onGoBack = () => {
+    if (document.referrer) {
+      router.back();
+      return;
+    }
+    router.push('/'); // go to home
+  };
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Sorry, something went wrong</h1>
@@ -13,6 +26,13 @@ const Error = () => {
           report a bug
         </Link>
       </p>
+      <div className={styles.goBack}>
+        <Link variant={LinkVariant.Highlight} href="#">
+          <span role="link" tabIndex={0} onKeyPress={onGoBack} onClick={onGoBack}>
+            Go Back
+          </span>
+        </Link>
+      </div>
     </div>
   );
 };
