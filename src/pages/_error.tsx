9fa36d3331e50c8da8eb8024f840f4b40f9eb573
Muhammad Muhajir <muhajirframe@gmail.com>
2021-10-06 14:17:12 +0700

    update error page

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,44 +1,20 @@
 import styles from './_error.module.scss';
 
-import Button, { ButtonType } from 'src/components/dls/Button/Button';
+import Link, { LinkVariant } from 'src/components/dls/Link/Link';
 
 // reference: https://nextjs.org/docs/advanced-features/custom-error-page#more-advanced-error-page-customizing
-const Error = ({ statusCode }) => {
+const Error = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>
-        {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : 'An error occurred on your browser'}
-      </h1>
-      <div className={styles.actions}>
-        <div className={styles.actionItem}>
-          <Button href="https://feedback.quran.com">Report the bug</Button>
-        </div>
-        <div className={styles.actionItem}>
-          <Button href="https://discord.gg/ZHcFfEHs" type={ButtonType.Secondary}>
-            Join Discord server
-          </Button>
-        </div>
-      </div>
+      <h1 className={styles.title}>Sorry, something went wrong</h1>
+      <p>
+        If the issue persists, please{' '}
+        <Link href="https://feedback.quran.com/" variant={LinkVariant.Highlight}>
+          report a bug
+        </Link>
+      </p>
     </div>
   );
-};
-
-const DEFAULT_STATUS_CODE = 404;
-Error.getInitialProps = ({ res, err }) => {
-  let statusCode;
-  if (res) {
-    const responseStatusCode = res.statusCode;
-    statusCode = responseStatusCode;
-  } else if (err) {
-    const errorStatusCode = err.statusCode;
-    statusCode = errorStatusCode;
-  } else {
-    statusCode = DEFAULT_STATUS_CODE;
-  }
-
-  return { statusCode };
 };
 
 export default Error;
