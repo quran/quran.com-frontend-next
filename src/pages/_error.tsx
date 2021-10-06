28cbf29402a12d8e910fc326f74ed19db2a5fae7
Muhammad Muhajir <muhajirframe@gmail.com>
2021-10-06 18:15:49 +0700

    update placement

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 
 import styles from './_error.module.scss';
 
+import Button from 'src/components/dls/Button/Button';
 import Link, { LinkVariant } from 'src/components/dls/Link/Link';
 
 // reference: https://nextjs.org/docs/advanced-features/custom-error-page#more-advanced-error-page-customizing
@@ -10,8 +11,8 @@ const Error = () => {
   const router = useRouter();
 
   // if previous page url exist, go back, otherwise go to home
-  const onGoBack = () => {
-    if (document.referrer) {
+  const onBackButtonClicked = () => {
+    if (document && document.referrer) {
       router.back();
       return;
     }
@@ -20,19 +21,15 @@ const Error = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Sorry, something went wrong</h1>
-      <p>
+      <div className={styles.goBack}>
+        <Button onClick={onBackButtonClicked}>Go Back</Button>
+      </div>
+      <p className={styles.reportBug}>
         If the issue persists, please{' '}
         <Link href="https://feedback.quran.com/" variant={LinkVariant.Highlight}>
           report a bug
         </Link>
       </p>
-      <div className={styles.goBack}>
-        <Link variant={LinkVariant.Highlight} href="#">
-          <span role="link" tabIndex={0} onKeyPress={onGoBack} onClick={onGoBack}>
-            Go Back
-          </span>
-        </Link>
-      </div>
     </div>
   );
 };
