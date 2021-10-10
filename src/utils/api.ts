6c4c5b6986d554455dbb58ebede2c44d5bccc271
osamasayed <osamasayed@users.noreply.github.com>
2021-10-10 14:56:31 +0700

    fix API root path (#558)

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -9,7 +9,7 @@ export const ITEMS_PER_PAGE = 10;
 const STAGING_API_HOST = 'https://staging.quran.com';
 const PRODUCTION_API_HOST = 'https://api.qurancdn.com';
 
-const API_ROOT_PATH = '/api/qdc/';
+const API_ROOT_PATH = '/api/qdc';
 
 // env variables in Vercel can't be dynamic, we have to hardcode the urls here. https://stackoverflow.com/questions/44342226/next-js-error-only-absolute-urls-are-supported
 export const API_HOST =
