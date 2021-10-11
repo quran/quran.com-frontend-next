61148c32b098b9836e37843ef37b9631fe364927
osamasayed <osama.sayed.abdelazim@gmail.com>
2021-10-11 17:52:49 +0700

    Add threshold to hide the navBar

--- src/components/GlobalScrollListener.tsx
+++ src/components/GlobalScrollListener.tsx
@@ -17,7 +17,7 @@ const GlobalScrollListener = () => {
        * zero then the browser forces the view to go to exactly 0 again so the hook detects it's
        * a down direction and hides the navbar, context menu and audioPlayer.
        */
-      if (newYPosition > 0 && direction === ScrollDirection.Down) {
+      if (newYPosition > 50 && direction === ScrollDirection.Down) {
         dispatch({ type: setIsMobileMinimizedForScrolling.type, payload: true });
         dispatch({ type: setIsExpanded.type, payload: false });
         dispatch({ type: setIsVisible.type, payload: false });
