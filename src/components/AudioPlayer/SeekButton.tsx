f01e81b0177fd1ca040026f31f77607c7479787d
Muhammad Muhajir <muhajirframe@gmail.com>
2021-10-11 11:14:06 +0700

    audio player: next and prev verse (#548)
    * audio player: next and prev verse
    
    * disable button for first and last verse
    
    * handle case: chapterAudioData not ready on slow internet connection
    
    * disable button when no highlighted verse is available
    
    * use new icon and reset repeat progress on seek
    
    * repeat each verse feature on backward

--- src/components/AudioPlayer/SeekButton.tsx
+++ src/components/AudioPlayer/SeekButton.tsx
@@ -1,14 +1,31 @@
-import ForwardIcon from '../../../public/icons/forward_10.svg';
-import ReplayIcon from '../../../public/icons/replay_10.svg';
+import { useMemo } from 'react';
 
-import { triggerSeek } from './EventTriggers';
+import { useDispatch, useSelector } from 'react-redux';
+import useSWRImmutable from 'swr/immutable';
 
+import BackwardIcon from '../../../public/icons/backward.svg';
+import ForwardIcon from '../../../public/icons/forward.svg';
+
+import { triggerSetCurrentTime } from './EventTriggers';
+
+import { getChapterAudioData } from 'src/api';
 import Button, { ButtonShape, ButtonVariant } from 'src/components/dls/Button/Button';
-import { withStopPropagation } from 'src/utils/event';
+import {
+  finishRepeatEachVerseProgress,
+  resetRepeatEachVerseProgress,
+  selectAudioData,
+  selectIsInRepeatMode,
+  selectReciter,
+} from 'src/redux/slices/AudioPlayer/state';
+import { selectHighlightedLocation } from 'src/redux/slices/QuranReader/highlightedLocation';
+import { makeChapterAudioDataUrl } from 'src/utils/apiPaths';
+import { getVerseTimingByVerseKey } from 'src/utils/audio';
+import { getChapterData } from 'src/utils/chapter';
+import { makeVerseKey } from 'src/utils/verse';
 
 export enum SeekButtonType {
-  FastForward = 'fastForward',
-  Rewind = 'rewind',
+  NextAyah = 'nextAyah',
+  PrevAyah = 'prevAyah',
 }
 
 type SeekButtonProps = {
@@ -16,15 +33,58 @@ type SeekButtonProps = {
   isLoading: boolean;
 };
 const SeekButton = ({ type, isLoading }: SeekButtonProps) => {
+  const dispatch = useDispatch();
+  const { highlightedChapter, highlightedVerse } = useSelector(selectHighlightedLocation);
+  const reciter = useSelector(selectReciter);
+  const audioData = useSelector(selectAudioData);
+  const isInRepeatMode = useSelector(selectIsInRepeatMode);
+  const chapterData = useMemo(
+    () => getChapterData(highlightedChapter?.toString()),
+    [highlightedChapter],
+  );
+
+  const { data: chapterAudioData } = useSWRImmutable(
+    reciter.id && audioData?.chapterId
+      ? makeChapterAudioDataUrl(reciter.id, audioData?.chapterId, true)
+      : null, // only fetch when reciterId and chapterId is available
+    () => getChapterAudioData(reciter.id, audioData?.chapterId, true),
+  );
+
+  const verseTimingData = chapterAudioData?.verseTimings || [];
+
+  const onSeek = () => {
+    if (isInRepeatMode) {
+      // when in repeatMode, finish the repeat progress for current ayah
+      // otherwise the AudioRepeatManager will replay the current Ayah when we set the new timestamp
+      if (type === SeekButtonType.NextAyah) dispatch(finishRepeatEachVerseProgress());
+      else dispatch(resetRepeatEachVerseProgress());
+    }
+    const newVerse = type === SeekButtonType.PrevAyah ? highlightedVerse - 1 : highlightedVerse + 1;
+    const verseKey = makeVerseKey(highlightedChapter, newVerse);
+
+    const selectedVerseTiming = getVerseTimingByVerseKey(verseKey, verseTimingData);
+    triggerSetCurrentTime(selectedVerseTiming.timestampFrom / 1000); // AudioPlayer accept 'seconds' instead of 'ms'
+  };
+
+  // disable the button if loading, or chapterAudioData not available, or highlighted verse not available
+  // or currently playing first verse or last verse
+  const isDisabled =
+    isLoading ||
+    !highlightedChapter ||
+    !highlightedVerse ||
+    !chapterAudioData ||
+    (type === SeekButtonType.PrevAyah && highlightedVerse <= 1) ||
+    (type === SeekButtonType.NextAyah && highlightedVerse >= chapterData?.versesCount);
+
   return (
     <Button
-      tooltip={type === SeekButtonType.Rewind ? 'Rewind 10 seconds' : 'Fast forward 10 seconds'}
+      tooltip={type === SeekButtonType.PrevAyah ? 'Previous Ayah' : 'Next Ayah'}
       variant={ButtonVariant.Ghost}
       shape={ButtonShape.Circle}
-      disabled={isLoading}
-      onClick={withStopPropagation(() => triggerSeek(type === SeekButtonType.Rewind ? -10 : 10))}
+      disabled={isDisabled}
+      onClick={onSeek}
     >
-      {type === SeekButtonType.Rewind ? <ReplayIcon /> : <ForwardIcon />}
+      {type === SeekButtonType.PrevAyah ? <BackwardIcon /> : <ForwardIcon />}
     </Button>
   );
 };
