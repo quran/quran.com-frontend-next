4206ce10a99a3c0baccbde92f02e4d738519fb16
Muhammad Muhajir <muhajirframe@gmail.com>
2021-10-14 15:57:54 +0700

    add hover underline and update copy right

--- src/components/dls/Footer/Footer.tsx
+++ src/components/dls/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import QSimpleIcon from '../../../../public/icons/Q_simple.svg';
 
 import styles from './Footer.module.scss';
 
-import Link from 'src/components/dls/Link/Link';
+import Link, { LinkVariant } from 'src/components/dls/Link/Link';
 
 const Footer = () => (
   <div className={styles.container}>
@@ -13,19 +13,27 @@ const Footer = () => (
       <div className={styles.title}>Read, study, and learn The Noble Quran.</div>
       <div className={styles.itemsContainer}>
         <div>
-          <Link href="/privacy">Privacy</Link>
+          <Link variant={LinkVariant.Primary} href="/privacy">
+            Privacy
+          </Link>
         </div>
         <div>
-          <Link href="/terms">Terms</Link>
+          <Link variant={LinkVariant.Primary} href="/terms">
+            Terms
+          </Link>
         </div>
         <div>
-          <Link href="/sitemap">Sitemap</Link>
+          <Link variant={LinkVariant.Primary} href="/sitemap">
+            Sitemap
+          </Link>
         </div>
         <div>
-          <Link href="https://feedback.quran.com">Provide Feedback</Link>
+          <Link variant={LinkVariant.Primary} href="https://feedback.quran.com">
+            Provide Feedback
+          </Link>
         </div>
       </div>
-      <div className={styles.copyright}>© 2021 Quran.corp 501(c), All Rights Reserved.</div>
+      <div className={styles.copyright}>© 2021 Quran.com. All Rights Reserved</div>
     </div>
   </div>
 );
