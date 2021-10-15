28ee3c9eaab564cada1befa7398c5e2159b6a952
Muhammad Muhajir <muhajirframe@gmail.com>
2021-10-15 15:44:53 +0700

    Homepage surah component (#570)
    * .
    
    * create surah DLS
    
    * create surah block component
    
    * .
    
    * .
    
    * update background color
    
    * create utlity lighten-background-color
    
    * update surah preview
    
    * use scss variable

---
+++ src/components/dls/SurahPreview/SurahPreviewBlock.tsx
@@ -0,0 +1,46 @@
+import styles from './SurahPreviewBlock.module.scss';
+
+import ChapterIconContainer, {
+  ChapterIconsSize,
+} from 'src/components/chapters/ChapterIcon/ChapterIconContainer';
+
+type SurahPreviewBlockProps = {
+  surahNumber: number;
+  surahName: string;
+  translatedSurahName: string;
+  chapterId: number;
+  description?: string;
+};
+
+const SurahPreviewBlock = ({
+  chapterId,
+  surahName,
+  surahNumber,
+  translatedSurahName,
+  description,
+}: SurahPreviewBlockProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div>
+          <div className={styles.translatedSurahName}>{translatedSurahName}</div>
+          <div className={styles.surahName}>
+            Surah <br />
+            {surahName}
+          </div>
+        </div>
+        <div className={styles.surahNumber}>{surahNumber}</div>
+      </div>
+      <div className={styles.surahIcon}>
+        <ChapterIconContainer
+          chapterId={chapterId.toString()}
+          hasSurahPrefix={false}
+          size={ChapterIconsSize.Large}
+        />
+        {description && <div className={styles.description}>{description}</div>}
+      </div>
+    </div>
+  );
+};
+
+export default SurahPreviewBlock;
---
+++ src/components/dls/SurahPreview/SurahPreviewRow.tsx
@@ -0,0 +1,40 @@
+import styles from './SurahPreviewRow.module.scss';
+
+import ChapterIconContainer from 'src/components/chapters/ChapterIcon/ChapterIconContainer';
+
+type SurahPreviewRowProps = {
+  surahNumber: number;
+  surahName: string;
+  translatedSurahName: string;
+  description: string;
+  chapterId: number;
+};
+const SurahPreviewRow = ({
+  surahName,
+  surahNumber,
+  translatedSurahName,
+  description,
+  chapterId,
+}: SurahPreviewRowProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.surahNumber}>
+          <span>{surahNumber}</span>
+        </div>
+        <div className={styles.surahNameContainer}>
+          <div className={styles.surahName}>{surahName}</div>
+          <div className={styles.translatedSurahName}>{translatedSurahName}</div>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div>
+          <ChapterIconContainer chapterId={chapterId.toString()} hasSurahPrefix={false} />
+        </div>
+        {description && <div className={styles.description}>{description}</div>}
+      </div>
+    </div>
+  );
+};
+
+export default SurahPreviewRow;
