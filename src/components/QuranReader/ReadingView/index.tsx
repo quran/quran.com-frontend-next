a728bb8291428884d319bbe2494aceecc6b6c2b7
osamasayed <osama.sayed.abdelazim@gmail.com>
2021-10-15 18:42:31 +0700

    Optimize QuranReader performance

--- src/components/QuranReader/ReadingView/index.tsx
+++ src/components/QuranReader/ReadingView/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, memo } from 'react';
 
 import groupPagesByVerses from './groupPagesByVerses';
 import Page from './Page';
@@ -22,4 +22,22 @@ const ReadingView = ({ verses }: ReadingViewProps) => {
   );
 };
 
-export default React.memo(ReadingView);
+/**
+ * Since we are passing verses and it's an array
+ * even if the same verses are passed, their reference will change
+ * on fetching a new page and since Memo only does shallow comparison,
+ * we need to use custom comparing logic:
+ *
+ *  1. Check if the number of verses are the same.
+ *
+ * If the above condition is met, it's safe to assume that the result
+ * of both renders are the same.
+ *
+ * @param {ReadingViewProps} prevProps
+ * @param {ReadingViewProps} nextProps
+ * @returns {boolean}
+ */
+const areEqual = (prevProps: ReadingViewProps, nextProps: ReadingViewProps): boolean => {
+  return prevProps.verses.length === nextProps.verses.length;
+};
+export default memo(ReadingView, areEqual);
