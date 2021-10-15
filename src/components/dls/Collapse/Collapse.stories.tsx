74b130961c5829d73651df7e9b91df76022fe577
Muhammad Muhajir <muhajirframe@gmail.com>
2021-10-15 10:11:14 +0700

    .

--- src/components/dls/Collapse/Collapse.stories.tsx
+++ src/components/dls/Collapse/Collapse.stories.tsx
@@ -9,33 +9,38 @@ export default {
 };
 
 export const Preview = () => {
-  const [selected, setSelected] = useState('surah');
+  const [selected, setSelected] = useState('trending');
   return (
     <div style={{ maxWidth: 400 }}>
       <Collapse>
+        <Collapse.Item id="recently-read" title="Recently Read">
+          test
+        </Collapse.Item>
+        <Collapse.Item id="Collections" title="Collections">
+          test
+        </Collapse.Item>
         <Collapse.Item
+          id="popular-trending-bookmarks"
           headerAction={
             <Tabs
               hasBorderBottom={false}
               tabs={[
-                { title: 'Surah', value: 'surah' },
-                { title: 'Juz', value: 'juz' },
-                { title: 'Juz', value: 'uz' },
+                { title: 'Popular', value: 'popular' },
+                { title: 'Trending', value: 'trending' },
+                { title: 'Bookmarks', value: 'Bookmarks' },
               ]}
               selected={selected}
               onSelect={(value) => setSelected(value)}
             />
           }
-          id="test1"
-          title="Surah"
         >
           test
         </Collapse.Item>
-        <Collapse.Item id="test2" title="how">
-          test
-        </Collapse.Item>
-        <Collapse.Item id="test3" title="?">
-          test
+
+        <Collapse.Item id="faq-question" title="Some FAQ Question ?">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatem beatae aliquam
+          nostrum dolorum facere repudiandae accusamus, hic dolore id harum assumenda quas obcaecati
+          suscipit voluptatum inventore cum quod accusantium.
         </Collapse.Item>
       </Collapse>
     </div>
