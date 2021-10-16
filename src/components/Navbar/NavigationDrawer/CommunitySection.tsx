815997bcf6a5597e6b8becddbe6e3cb4f0cd96c8
Muhammad Muhajir <muhajirframe@gmail.com>
2021-10-16 15:59:45 +0700

    update PR based on feedback

--- src/components/Navbar/NavigationDrawer/CommunitySection.tsx
+++ src/components/Navbar/NavigationDrawer/CommunitySection.tsx
@@ -1,24 +1,19 @@
-import DiscordIconBlackWhite from '../../../../public/icons/discord-icon-black-white.svg';
-import DiscordIconColor from '../../../../public/icons/discord-icon-color.svg';
+import DiscordIcon from '../../../../public/icons/discord-icon.svg';
 
 import styles from './CommunitySection.module.scss';
 
 import Button, { ButtonType } from 'src/components/dls/Button/Button';
 import IconContainer, { IconColor, IconSize } from 'src/components/dls/IconContainer/IconContainer';
-import { DISCORD_URL } from 'src/constants';
+import Link from 'src/components/dls/Link/Link';
 
 const CommunitySection = () => {
   return (
     <div className={styles.container}>
       <div className={styles.platformLogoBackground}>
-        <DiscordIconColor />
+        <DiscordIcon />
       </div>
       <div className={styles.platformLogo}>
-        <IconContainer
-          icon={<DiscordIconBlackWhite />}
-          size={IconSize.Xsmall}
-          color={IconColor.secondary}
-        />
+        <IconContainer icon={<DiscordIcon />} size={IconSize.Xsmall} color={IconColor.secondary} />
       </div>
       <div className={styles.flow}>
         <div className={styles.title}>
@@ -28,9 +23,11 @@ const CommunitySection = () => {
           Sign up for the Quran.com Discord to particpate and collaborate with the leading community
           building Islamic tech.
         </div>
-        <Button href={DISCORD_URL} type={ButtonType.Success}>
-          Join Community
-        </Button>
+        <Link href="https://discord.gg/FxRWSBfWxn" newTab>
+          <Button href="" type={ButtonType.Success}>
+            Join Community
+          </Button>
+        </Link>
       </div>
     </div>
   );
--- src/constants.ts
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const DISCORD_URL = 'https://discord.gg/FxRWSBfWxn';
