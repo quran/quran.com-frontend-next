1c8d62fea75bd44e87765695cfc3594dac06adf4
Muhammad Muhajir <muhajirframe@gmail.com>
2021-10-18 06:21:35 +0700

    add docs

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -4,11 +4,22 @@ export const getVerseTimingByVerseKey = (verseKey: string, verseTimings: VerseTi
   return verseTimings.find((verseTiming) => verseTiming.verseKey === verseKey);
 };
 
-// format the number to match the file name structure
+// format the number to match the the mp3 file name structure
 // e.g formatNumber(3) => '003'
 //     formatNumber(10) => '010'
 const formatNumber = (num: number) => num.toString().padStart(3, '0');
 const BASE_URL = 'https://audio.qurancdn.com/wbw';
+
+/**
+ * Given chapter, verse, and wordLocation. Get the mp3 audio url
+ * For example
+ * getWordByWordAudioUrl(1,2,1) => `'https://audio.qurancdn.com/wbw/001_002_001.mp3';
+ *
+ * @param {number} chapter the chapterId
+ * @param {number} verse the verse number
+ * @param {number} wordLocation the location of the word within a verse
+ * @returns {string} audio url
+ */
 export const getWordByWordAudioUrl = (chapter: number, verse: number, wordLocation: number) => {
   const formattedChapter = formatNumber(chapter);
   const formattedVerse = formatNumber(verse);
