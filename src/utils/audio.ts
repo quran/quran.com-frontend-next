ab2d4afd2c75de37a3f3ce6d6f10ed17f77f4730
Muhammad Muhajir <muhajirframe@gmail.com>
2021-10-18 06:12:21 +0700

    prepare util for word by word audio

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -1,6 +1,18 @@
 import VerseTiming from 'types/VerseTiming';
 
-// eslint-disable-next-line import/prefer-default-export
 export const getVerseTimingByVerseKey = (verseKey: string, verseTimings: VerseTiming[]) => {
   return verseTimings.find((verseTiming) => verseTiming.verseKey === verseKey);
+};
+
+// format the number to match the file name structure
+// e.g formatNumber(3) => '003'
+//     formatNumber(10) => '010'
+const formatNumber = (num: number) => num.toString().padStart(3, '0');
+const BASE_URL = 'https://audio.qurancdn.com/wbw';
+export const getWordByWordAudioUrl = (chapter: number, verse: number, wordLocation: number) => {
+  const formattedChapter = formatNumber(chapter);
+  const formattedVerse = formatNumber(verse);
+  const formattedWordLocation = formatNumber(wordLocation);
+
+  return `${BASE_URL}/${formattedChapter}_${formattedVerse}_${formattedWordLocation}.mp3`;
 };
