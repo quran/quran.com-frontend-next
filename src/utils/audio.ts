8d8adcbe82b41bd564014e812550b5e46ae37140
Muhammad Muhajir <muhajirframe@gmail.com>
2021-10-20 16:17:12 +0700

    prepare util for word by word audio (#594)
    * prepare util for word by word audio
    
    * add docs

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -1,6 +1,29 @@
 import VerseTiming from 'types/VerseTiming';
 
-// eslint-disable-next-line import/prefer-default-export
 export const getVerseTimingByVerseKey = (verseKey: string, verseTimings: VerseTiming[]) => {
   return verseTimings.find((verseTiming) => verseTiming.verseKey === verseKey);
+};
+
+// format the number to match the the mp3 file name structure
+// e.g formatNumber(3) => '003'
+//     formatNumber(10) => '010'
+const formatNumber = (num: number) => num.toString().padStart(3, '0');
+const BASE_URL = 'https://audio.qurancdn.com/wbw';
+
+/**
+ * Given chapter, verse, and wordLocation. Get the mp3 audio url
+ * For example
+ * getWordByWordAudioUrl(1,2,1) => `'https://audio.qurancdn.com/wbw/001_002_001.mp3';
+ *
+ * @param {number} chapter the chapterId
+ * @param {number} verse the verse number
+ * @param {number} wordLocation the location of the word within a verse
+ * @returns {string} audio url
+ */
+export const getWordByWordAudioUrl = (chapter: number, verse: number, wordLocation: number) => {
+  const formattedChapter = formatNumber(chapter);
+  const formattedVerse = formatNumber(verse);
+  const formattedWordLocation = formatNumber(wordLocation);
+
+  return `${BASE_URL}/${formattedChapter}_${formattedVerse}_${formattedWordLocation}.mp3`;
 };
