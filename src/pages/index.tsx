cf7a5e20e4a5dfaab4e5e3ed1d894756a7caa449
Muhammad Muhajir <muhajirframe@gmail.com>
2021-10-28 14:35:14 +0700

    test-quality-100

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,6 +30,7 @@ const Index: NextPage<IndexProps> = ({ chaptersResponse: { chapters } }) => (
             src={homepageImage}
             objectFit="cover"
             sizes="(max-width: 425px) 95vw, 80vw"
+            quality={100}
             priority
             layout="fill" // the image will scale the dimensions down for smaller viewports and scale up for larger viewports
             placeholder="blur" // to have a blur effect while loading.
