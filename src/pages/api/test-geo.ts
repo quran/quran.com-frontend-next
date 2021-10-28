97f080154e2b90101b7cd9d5f9800852ddc27d31
Muhammad Muhajir <muhajirframe@gmail.com>
2021-10-28 20:10:08 +0700

    test stringify headers

--- src/pages/api/test-geo.ts
+++ src/pages/api/test-geo.ts
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
   const { geo } = req;
   res.setHeader('Cache-Control', 's-maxage=1000');
-  res.status(200).json({ name: 'John Doe', geo });
+  res.status(200).json({ name: 'John Doe', geo, req: JSON.stringify(req.headers) });
 }
