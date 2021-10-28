1f1001bf23093a578845f053e7e7ce81110f20af
Muhammad Muhajir <muhajirframe@gmail.com>
2021-10-28 21:14:11 +0700

    test api

--- src/pages/test-geo/_middleware.ts
+++ src/pages/test-geo/_middleware.ts
@@ -1,13 +1,33 @@
 /* eslint-disable import/prefer-default-export */
+import adhan from 'adhan';
 import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
   const { geo } = req;
 
-  return new Response(JSON.stringify({ message: 'hello world!', geo }), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
+  // @ts-ignore
+  const coordinates = new adhan.Coordinates(geo.latitude, geo.longitude);
+  const date = new Date();
+  const params = adhan.CalculationMethod.MuslimWorldLeague();
+  const prayerTimes = new adhan.PrayerTimes(coordinates, date, params);
+
+  return new Response(
+    JSON.stringify({
+      geo,
+      prayerTimes: {
+        fajrTime: prayerTimes.fajr,
+        sunriseTime: prayerTimes.sunrise,
+        dhuhrTime: prayerTimes.dhuhr,
+        asrTime: prayerTimes.asr,
+        maghribTime: prayerTimes.maghrib,
+        ishaTime: prayerTimes.isha,
+      },
+    }),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
     },
-  });
+  );
 }
