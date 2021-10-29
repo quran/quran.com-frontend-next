2e03270f43e15a01a416d89293db63a06c352b6e
osamasayed <osamasayed@users.noreply.github.com>
2021-10-29 13:05:28 +0700

    Add localizable strings to static pages (#633)

--- src/pages/support.tsx
+++ src/pages/support.tsx
@@ -1,111 +1,120 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import Trans from 'next-translate/Trans';
+
 import styles from './contentPage.module.scss';
 
 import Link, { LinkVariant } from 'src/components/dls/Link/Link';
 
 const SupportPage = () => (
   <div className={styles.contentPage}>
-    <h1>Help and Feedback</h1>
+    <h1>
+      <Trans i18nKey="support:header" />
+    </h1>
     <p>
-      Please check the FAQ to see if your question has been already answered. If needed you can{' '}
-      <a data-controller="ajax-modal" data-url="/popups/feedback">
-        contact us
-      </a>{' '}
-      &amp; we&apos;ll do our best to get back to you as soon as possible, but just so you know
-      we&apos;re a small team so please be nice.
+      <Trans
+        i18nKey="support:main-desc"
+        components={[<a target="_blank" href="https://feedback.quran.com/" rel="noreferrer" />]}
+      />
+    </p>
+    <h2>
+      <Trans i18nKey="support:download-q" />
+    </h2>
+    <p>
+      <Trans i18nKey="support:download-a" />
+    </p>
+    <h2>
+      <Trans i18nKey="support:other-languages-q" />
+    </h2>
+    <p>
+      <Trans i18nKey="support:other-languages-a" />
+    </p>
+    <h2>
+      <Trans i18nKey="support:bug-q" />
+    </h2>
+    <p>
+      <Trans
+        i18nKey="support:bug-a"
+        components={[<a target="_blank" href="https://feedback.quran.com/" rel="noreferrer" />]}
+      />
+    </p>
+    <h2>
+      <Trans i18nKey="support:site-down-q" />
+    </h2>
+    <p>
+      <Trans
+        i18nKey="support:site-down-a"
+        components={[<a target="_blank" href="https://feedback.quran.com/" rel="noreferrer" />]}
+      />
+    </p>
+    <h2>
+      <Trans i18nKey="support:developer-q" />
+    </h2>
+    <p>
+      <Trans
+        i18nKey="support:developer-a"
+        components={[<Link href="/developers" variant={LinkVariant.Blend} />]}
+      />
+    </p>
+    <h2>
+      <Trans i18nKey="support:fiqh-q" />
+    </h2>
+    <p>
+      <Trans i18nKey="support:fiqh-a" />
+    </p>
+    <h2>
+      <Trans i18nKey="support:tafsir-q" />
+    </h2>
+    <p>
+      <Trans i18nKey="support:tafsir-a" />
     </p>
 
-    <h2>Can I download the Quran.com to my computer?</h2>
+    <h2>
+      <Trans i18nKey="support:translations-q" />
+    </h2>
     <p>
-      Unfortunately, no. We do not provide functionality to download our website or the Quran to
-      your computer yet. You can, however install our mobile app for offline reading.
+      <Trans
+        i18nKey="support:translations-a"
+        components={[<a href="https://feedback.quran.com/" target="_blank" rel="noreferrer" />]}
+      />
     </p>
-
-    <h2>Can I browse site in other languages?</h2>
+    <h2>
+      <Trans i18nKey="support:reciters-q" />
+    </h2>
     <p>
-      To change your preferred language, there is a drop down on the top right corner on each page(
-      to right corner in the left side menu on mobile). Use this dropdown to choose your preferred
-      language.
+      <Trans
+        i18nKey="support:reciters-a"
+        components={[<a target="_blank" href="https://feedback.quran.com/" rel="noreferrer" />]}
+      />
     </p>
-
-    <h2>I found a translation bug, where do I file it?</h2>
-
+    <h2>
+      <Trans i18nKey="support:mobile-q" />
+    </h2>
     <p>
-      Please report this bug{' '}
-      <a target="_blank" href="https://feedback.quran.com/" rel="noreferrer">
-        here
-      </a>{' '}
-      and we&apos;ll fix this bug ASAP inshAllah.
+      <Trans
+        i18nKey="support:mobile-a"
+        components={[
+          <a
+            target="_blank"
+            href="https://play.google.com/store/apps/details?id=com.quran.labs.androidquran&utm_source=quran-com&utm_campaign=download"
+            rel="noreferrer"
+          />,
+          <a
+            target="_blank"
+            href="https://apps.apple.com/us/app/quran-by-quran-com-qran/id1118663303"
+            rel="noreferrer"
+          />,
+        ]}
+      />
     </p>
-
-    <h2>The site is not working, how do I tell you?</h2>
+    {/* <h2>
+      <Trans i18nKey="support:donate-q" />
+    </h2>
     <p>
-      That&apos;s not good! If the site is not working at all or perhaps you see a white screen with
-      &apos;502&apos;, we appreciate it if you can email us immediately to let us know. You can
-      email us at fire@quran.zendesk.com or report this issue{' '}
-      <a target="_blank" href="https://feedback.quran.com/" rel="noreferrer">
-        here
-      </a>
-      .
-    </p>
-
-    <h2>I&apos;m a developer. How can I contribute?</h2>
-    <p>
-      Please see{' '}
-      <Link href="/developers" variant={LinkVariant.Blend}>
-        developers page
-      </Link>{' '}
-      for more info.
-    </p>
-
-    <h2>Islamic/ Fiqh / Fatwa related questions</h2>
-
-    <p>
-      Quran.com is an online reading, listening and studying tool. The team behind Quran.com is made
-      up of software engineers, designers, and product managers. Unfortunately, that is the
-      limitation of our skill set we do not have scholars, imams or sheikhs as part of the team to
-      assist with Islamic, Fiqh or Fatwa related questions. We try to refrain from answering any of
-      those questions and advise you speak to your local imam at a mosque or to a sheikh.
-    </p>
-
-    <h2>Is Tafsir available?</h2>
-
-    <p>
-      Yes, we do have some Tafsirs. Click on <span className="quran-icon icon-menu" /> icon shown
-      beside each ayah, then click on tafisrs. App will show you list of available tafsirs. Click on
-      the tafsir you want to read.
-    </p>
-
-    <h2>Add another translations</h2>
-
-    <p>
-      Open a new issue{' '}
-      <a href="https://feedback.quran.com/" target="_blank" rel="noreferrer">
-        here
-      </a>{' '}
-      with all the detail, link to translation and we&apos;ll try our best to add this.
-    </p>
-
-    <h2>Adding more reciters</h2>
-    <p>
-      Submit more info about this reciter{' '}
-      <a target="_blank" href="https://feedback.quran.com/" rel="noreferrer">
-        here
-      </a>
-    </p>
-
-    <h2>Does Quran.com have mobile app?</h2>
-    <p>
-      Yes! You can download our Application on{' '}
-      <a href="https://play.google.com/store/apps/details?id=com.quran.labs.androidquran&utm_source=quran-com&utm_campaign=download">
-        Play Store
-      </a>{' '}
-      or <a href="https://apps.apple.com/us/app/quran-by-quran-com-qran/id1118663303">App Store</a>
-    </p>
-
-    {/* <h2>How can I donate?</h2>
-    <p>
-      Firstly, we really appreciate your interest to contribute. Please visit this{' '}
-      <a href="/donations">this link</a>.
+      <Trans
+        i18nKey="support:donate-a"
+        components={[<Link href="/donations" variant={LinkVariant.Blend} />]}
+      />
     </p> */}
   </div>
 );
