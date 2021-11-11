c70ac35e9a6aa6959463e05bfe524c242d093346
osamasayed <osama.sayed.abdelazim@gmail.com>
2021-11-11 08:09:46 +0700

    Persist selected locale

--- src/components/Navbar/LanguageSelector.tsx
+++ src/components/Navbar/LanguageSelector.tsx
@@ -33,11 +33,30 @@ const options = locales.map((lng) => ({
   value: lng,
 }));
 
+const COOKIE_PERSISTENCE_PERIOD_MS = 100 * 24 * 60 * 60 * 1000; // 100 days
+
 const LanguageSelector = () => {
   const { lang } = useTranslation();
 
-  const onChange = async (value: string) => {
-    await setLanguage(value);
+  /**
+   * When the user changes the language, we will:
+   *
+   * 1. Call next-translate's setLanguage with the new value.
+   * 2. Store the new value of the locale in the cookies so that next time the user
+   * lands on the `/` route, he will be redirected to the homepage with the
+   * saved locale. This is to over-ride next.js's default behavior which takes
+   * into consideration `Accept-language` header {@see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language}
+   * as a locale detection mechanism. For further reading on Next.js's behavior
+   * {@see https://nextjs.org/docs/advanced-features/i18n-routing}.
+   *
+   * @param {string} newLocale
+   */
+  const onChange = async (newLocale: string) => {
+    await setLanguage(newLocale);
+    const date = new Date();
+    date.setTime(date.getTime() + COOKIE_PERSISTENCE_PERIOD_MS);
+    // eslint-disable-next-line i18next/no-literal-string
+    document.cookie = `NEXT_LOCALE=${newLocale};expires=${date.toUTCString()};path=/`;
   };
 
   return (
