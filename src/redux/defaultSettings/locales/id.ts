10f3bcac78d9f7ecac3efdac42db49af6768119b
osamasayed <osama.sayed.abdelazim@gmail.com>
2021-11-15 17:50:04 +0700

    Update locales default settings

--- src/redux/defaultSettings/locales/id.ts
+++ src/redux/defaultSettings/locales/id.ts
@@ -1,3 +1,9 @@
 import DEFAULT_SETTINGS, { DefaultSettings } from '../defaultSettings';
 
-export default DEFAULT_SETTINGS as DefaultSettings;
+export default {
+  ...DEFAULT_SETTINGS,
+  readingPreferences: {
+    ...DEFAULT_SETTINGS.readingPreferences,
+    selectedWordByWordLocale: 'id',
+  },
+} as DefaultSettings;
