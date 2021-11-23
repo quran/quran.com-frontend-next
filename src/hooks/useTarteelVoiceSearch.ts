67688c60cc3d4906e7aa6314efa1a6b5e8a79c6d
Abdellatif <tifa3333@gmail.com>
2021-11-23 23:49:13 +0100

    fix failing tests

--- src/hooks/useTarteelVoiceSearch.ts
+++ src/hooks/useTarteelVoiceSearch.ts
@@ -38,6 +38,7 @@ const AUDIO_CONSTRAINTS = {
 const USER_MEDIA_NOT_SUPPORTED_ERROR = 'USER_MEDIA_NOT_SUPPORTED';
 const ANALYSER_SMOOTHING_CONSTANT = 0.8;
 const FAST_FOURIER_TRANSFORM_SIZE = 1024;
+// eslint-disable-next-line prefer-destructuring
 const TARTEEL_VS_API_KEY = process.env.TARTEEL_VS_API_KEY;
 
 const useTarteelVoiceSearch = (startRecording = true) => {
@@ -191,7 +192,9 @@ const useTarteelVoiceSearch = (startRecording = true) => {
           .then(() => {
             setIsLoading(true);
             // 3. Start a new websocket
-            webSocket = new WebSocket(`wss://voice-v2.tarteel.io/search/?Authorization=${TARTEEL_VS_API_KEY}`);
+            webSocket = new WebSocket(
+              `wss://voice-v2.tarteel.io/search/?Authorization=${TARTEEL_VS_API_KEY}`,
+            );
             webSocket.onopen = () => {
               setIsLoading(false);
               mediaStream.current = stream;
