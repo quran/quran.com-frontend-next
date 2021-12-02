02ab1c44e7d0ac4312ba0fdbbf4db4fc6145714d
osamasayed <osama.sayed.abdelazim@gmail.com>
2021-12-02 16:27:10 +0700

    updates

--- src/redux/defaultSettings/locales/ar.ts
+++ src/redux/defaultSettings/locales/ar.ts
@@ -2,12 +2,9 @@ import DEFAULT_SETTINGS, { DefaultSettings } from '../defaultSettings';
 
 import { ReadingPreference } from 'types/QuranReader';
 
-// Arabic Qurtubi Tafseer
-const DEFAULT_TAFSIR = 90;
-
 export default {
   ...DEFAULT_SETTINGS,
-  tafsirs: { ...DEFAULT_SETTINGS.tafsirs, selectedTafsirs: [DEFAULT_TAFSIR] },
+  tafsirs: { ...DEFAULT_SETTINGS.tafsirs, selectedTafsirs: [] },
   readingPreferences: {
     ...DEFAULT_SETTINGS.readingPreferences,
     readingPreference: ReadingPreference.Reading,
