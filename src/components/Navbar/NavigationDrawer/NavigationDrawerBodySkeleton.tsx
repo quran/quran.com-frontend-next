cb64b4e4273c62b4312b1f029c52e7de531616b5
Mahmoud Felfel <mahmoudfelfel@gmail.com>
2021-12-06 13:23:38 +0000

    Added Skeleton components for sidemenus and homepage (#750)
    * Added Skeleton components for sidemenus and homepage
    
    * addressed pr comments
    
    * linting css, and minor other fixes
    
    * fix ts linting
    
    * updates
    
    Co-authored-by: Mahmoud Felfel <mahmoudfelfel@192.168.1.122>
    Co-authored-by: osamasayed <osama.sayed.abdelazim@gmail.com>

---
+++ src/components/Navbar/NavigationDrawer/NavigationDrawerBodySkeleton.tsx
@@ -0,0 +1,25 @@
+import Skeleton from '../../dls/Skeleton/Skeleton';
+
+import styles from './NavigationDrawerBodySkeleton.module.scss';
+
+const NAV_ROW_COUNT = 6;
+const rowsArr = Array(NAV_ROW_COUNT).fill(null);
+
+const renderLinesSkeleton = (index) => {
+  return rowsArr.map((k, i) => (
+    // eslint-disable-next-line react/no-array-index-key
+    <Skeleton key={`skeleton_${index}_${i}`} isActive isSquared className={styles.navRow} />
+  ));
+};
+
+const NavigationDrawerBodySkeleton = () => {
+  return (
+    <span className={styles.skeletonContainer}>
+      {renderLinesSkeleton(1)}
+      <Skeleton isActive isSquared className={styles.blockRow} />
+      {renderLinesSkeleton(2)}
+    </span>
+  );
+};
+
+export default NavigationDrawerBodySkeleton;
