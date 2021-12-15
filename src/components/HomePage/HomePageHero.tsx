c5781538f48212ada364016edb16857352895ced
Muhammad Muhajir <muhajirframe@gmail.com>
2021-12-15 14:50:05 +0700

    use next image for homepage image

--- src/components/HomePage/HomePageHero.tsx
+++ src/components/HomePage/HomePageHero.tsx
@@ -1,4 +1,7 @@
+import Image from 'next/image';
+
 import AlQuranulKarimSVG from '../../../public/images/alquranul-karim.svg';
+import BackgroundImage from '../../../public/images/background.jpg';
 
 import styles from './HomePageHero.module.scss';
 import PrayerTimes from './PrayerTimes/PrayerTimes';
@@ -9,7 +12,17 @@ import CommandBarTrigger from 'src/components/CommandBar/CommandBarTrigger';
 const HomePageHero = () => {
   return (
     <div className={styles.outerContainer}>
-      <div className={styles.backgroundImage} />
+      <div className={styles.backgroundImage}>
+        <Image
+          src={BackgroundImage}
+          objectFit="cover"
+          placeholder="empty"
+          layout="fill"
+          priority
+          quality={100}
+          objectPosition="left bottom"
+        />
+      </div>
       <div data-theme="light">
         <PrayerTimes />
         <div className={styles.innerContainer}>
