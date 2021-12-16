920ca3ad43a16565b0c3e3d003b4a503f10f627c
Muhammad Muhajir <muhajirframe@gmail.com>
2021-12-16 14:51:02 +0700

    add floatline to switch dls

--- src/components/dls/Switch/Switch.tsx
+++ src/components/dls/Switch/Switch.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import range from 'lodash/range';
 import { useRouter } from 'next/router';
 
 import styles from './Switch.module.scss';
@@ -44,6 +45,18 @@ const Switch = ({ items, onSelect, selected, size = SwitchSize.Normal }: SwitchP
           {item.name}
         </button>
       ))}
+      {range(1, items.length).map((i) => {
+        return (
+          <div
+            key={i}
+            className={classNames(styles.floatingLine, styles.floatingLineVisible)}
+            style={{
+              width: `${100 / items.length}%`,
+              transform: `translateX(${100 * i}%)`,
+            }}
+          />
+        );
+      })}
       <div
         className={styles.selectedItemBackgroundContainer}
         style={{
