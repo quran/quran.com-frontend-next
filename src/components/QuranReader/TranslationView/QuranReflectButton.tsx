35ab72d078aea4706946249636d11f616e4d967e
Muhammad Muhajir <muhajirframe@gmail.com>
2021-12-16 10:18:56 +0700

    fxi tooltip not working

--- src/components/QuranReader/TranslationView/QuranReflectButton.tsx
+++ src/components/QuranReader/TranslationView/QuranReflectButton.tsx
@@ -4,6 +4,7 @@ import QuranReflectIcon from '../../../../public/icons/QR.svg';
 
 import Button, { ButtonSize, ButtonType } from 'src/components/dls/Button/Button';
 import { getQuranReflectVerseUrl } from 'src/utils/navigation';
+import { navigateToExternalUrl } from 'src/utils/url';
 
 type QuranReflectButtonProps = {
   verseKey: string;
@@ -13,7 +14,7 @@ const QuranReflectButton = ({ verseKey }: QuranReflectButtonProps) => {
   const { t } = useTranslation('common');
   return (
     <Button
-      href={getQuranReflectVerseUrl(verseKey)}
+      onClick={() => navigateToExternalUrl(getQuranReflectVerseUrl(verseKey))}
       size={ButtonSize.Small}
       tooltip={t('q-reflect')}
       type={ButtonType.Secondary}
