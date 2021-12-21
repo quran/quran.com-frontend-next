528fa426071308cbd4755aeb9617cab85b5e6587
Muhammad Muhajir <muhajirframe@gmail.com>
2021-12-21 18:32:50 +0700

    .

--- src/components/QuranReader/TafsirView/TafsirBody.tsx
+++ src/components/QuranReader/TafsirView/TafsirBody.tsx
@@ -6,20 +6,21 @@ import useTranslation from 'next-translate/useTranslation';
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 
 import SurahAndAyahSelection from './SurahAndAyahSelection';
+import TafsirGroupMessage from './TafsirGroupMessage';
 import TafsirSelection from './TafsirSelection';
+import TafsirSkeleton from './TafsirSkeleton';
 import styles from './TafsirView.module.scss';
 
 import DataFetcher from 'src/components/DataFetcher';
 import Separator from 'src/components/dls/Separator/Separator';
-import Spinner from 'src/components/dls/Spinner/Spinner';
 import VerseText from 'src/components/Verse/VerseText';
 import { selectQuranReaderStyles } from 'src/redux/slices/QuranReader/styles';
 import { selectSelectedTafsirs, setSelectedTafsirs } from 'src/redux/slices/QuranReader/tafsirs';
 import QuranReaderStyles from 'src/redux/types/QuranReaderStyles';
 import { getDefaultWordFields } from 'src/utils/api';
-import { makeVersesUrl } from 'src/utils/apiPaths';
+import { makeTafsirContentUrl } from 'src/utils/apiPaths';
 import { areArraysEqual } from 'src/utils/array';
-import { getVerseWords } from 'src/utils/verse';
+import { getVerseWords, makeVerseKey } from 'src/utils/verse';
 import Verse from 'types/Verse';
 
 type TafsirBodyProps = {
@@ -35,13 +36,13 @@ const TafsirBody = ({ verse }: TafsirBodyProps) => {
   const [selectedChapterId, setSelectedChapterId] = useState(verse.chapterId);
   const [selectedVerseNumber, setSelectedVerseNumber] = useState(verse.verseNumber.toString());
   const selectedTafsirs = useSelector(selectSelectedTafsirs, areArraysEqual);
-  const queryKey = makeVersesUrl(selectedChapterId, lang, {
-    page: selectedVerseNumber, // we pass the verse id as a the page and then fetch only 1 verse per page.
-    perPage: 1, // only 1 verse per page
-    translations: null,
-    tafsirs: [tafsirs],
-    wordFields: 'location, verse_key, text_uthmani',
-    tafsirFields: 'resource_name,language_name',
+  const selectedVerseKey = makeVerseKey(Number(selectedChapterId), Number(selectedVerseNumber));
+  const queryKey = makeTafsirContentUrl(tafsirs[0], selectedVerseKey, {
+    // perPage: 1, // only 1 verse per page
+    // translations: null,
+    // wordFields: 'location, verse_key, text_uthmani',
+    // tafsirFields: 'resource_name,language_name',
+    words: true,
     ...getDefaultWordFields(quranReaderStyles.quranFont),
   });
 
@@ -71,25 +72,30 @@ const TafsirBody = ({ verse }: TafsirBodyProps) => {
         )}
       >
         <DataFetcher
+          loading={TafsirSkeleton}
           queryKey={queryKey}
           render={(data) => {
+            if (!data) return <TafsirSkeleton />;
             // @ts-ignore
-            const tafsirVerse = data?.verses[0];
-            const tafsirsData = tafsirVerse?.tafsirs;
+            const tafsirVerses = data?.tafsir.verses;
+            // @ts-ignore
+            const htmlText = data?.tafsir.text;
+            const words = Object.values(tafsirVerses).map(getVerseWords).flat();
 
-            if (!tafsirsData) return <Spinner />;
+            if (!htmlText) return <TafsirSkeleton />;
+
             return (
               <div>
+                {Object.values(tafsirVerses).length > 1 && (
+                  <TafsirGroupMessage {...getFromAndToVerseKeys(tafsirVerses)} />
+                )}
                 <div className={styles.verseTextContainer}>
-                  <VerseText words={getVerseWords(tafsirVerse)} />
+                  <VerseText words={words} />
                 </div>
                 <div className={styles.separatorContainer}>
                   <Separator />
                 </div>
-
-                {tafsirsData?.map((tafsir) => (
-                  <div key={tafsir.id} dangerouslySetInnerHTML={{ __html: tafsir.text }} />
-                ))}
+                <div dangerouslySetInnerHTML={{ __html: htmlText }} />
               </div>
             );
           }}
@@ -97,6 +103,11 @@ const TafsirBody = ({ verse }: TafsirBodyProps) => {
       </div>
     </div>
   );
+};
+
+const getFromAndToVerseKeys = (verses) => {
+  const verseKeys = Object.keys(verses);
+  return { from: verseKeys[0], to: verseKeys[verseKeys.length - 1] };
 };
 
 export default TafsirBody;
