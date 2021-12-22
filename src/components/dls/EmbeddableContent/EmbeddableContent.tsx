d2a8e734d9f174d15f79aef4f5d007ac9e7c5d79
Muhammad Muhajir <muhajirframe@gmail.com>
2021-12-22 20:26:33 +0700

    fix error

--- src/components/dls/EmbeddableContent/EmbeddableContent.tsx
+++ src/components/dls/EmbeddableContent/EmbeddableContent.tsx
@@ -1,9 +1,9 @@
-import * as Dialog from '@radix-ui/react-dialog';
+// import * as Dialog from '@radix-ui/react-dialog';
 
-import CloseIcon from '../../../../public/icons/close.svg';
-import Button, { ButtonShape, ButtonVariant } from '../Button/Button';
+// import CloseIcon from '../../../../public/icons/close.svg';
+// import Button, { ButtonShape, ButtonVariant } from '../Button/Button';
 
-import styles from './EmbeddableContent.module.scss';
+// import styles from './EmbeddableContent.module.scss';
 
 type EmbeddableContentProps = {
   isOpen?: boolean;
@@ -11,30 +11,28 @@ type EmbeddableContentProps = {
   children: React.ReactNode;
   hasCloseButton?: boolean;
 };
-const EmbeddableContent = ({
-  children,
-  isOpen,
-  onClose,
-  hasCloseButton = true,
-}: EmbeddableContentProps) => {
-  return (
-    <Dialog.Root open={isOpen}>
-      <Dialog.Portal>
-        <Dialog.Overlay className={styles.overlay}>
-          <Dialog.Content className={styles.content} onInteractOutside={onClose}>
-            {hasCloseButton && (
-              <Dialog.Close className={styles.closeIcon}>
-                <Button variant={ButtonVariant.Ghost} shape={ButtonShape.Circle} onClick={onClose}>
-                  <CloseIcon />
-                </Button>
-              </Dialog.Close>
-            )}
-            {children}
-          </Dialog.Content>
-        </Dialog.Overlay>
-      </Dialog.Portal>
-    </Dialog.Root>
-  );
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const EmbeddableContent = (props: EmbeddableContentProps) => {
+  return <div>{/* return nothing for now */}</div>;
+  // return (
+  //   <Dialog.Root open={isOpen}>
+  //     <Dialog.Portal>
+  //       <Dialog.Overlay className={styles.overlay}>
+  //         <Dialog.Content className={styles.content} onInteractOutside={onClose}>
+  //           {hasCloseButton && (
+  //             <Dialog.Close className={styles.closeIcon}>
+  //               <Button variant={ButtonVariant.Ghost} shape={ButtonShape.Circle} onClick={onClose}>
+  //                 <CloseIcon />
+  //               </Button>
+  //             </Dialog.Close>
+  //           )}
+  //           {children}
+  //         </Dialog.Content>
+  //       </Dialog.Overlay>
+  //     </Dialog.Portal>
+  //   </Dialog.Root>
+  // );
 };
 
 export default EmbeddableContent;
