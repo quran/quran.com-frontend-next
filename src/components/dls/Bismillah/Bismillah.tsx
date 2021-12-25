cca6eb5b51de415f640bf5ca9b08ed07a8955de7
Muhammad Muhajir <muhajirframe@gmail.com>
2021-12-25 18:07:24 +0700

    fix error

--- src/components/dls/Bismillah/Bismillah.tsx
+++ src/components/dls/Bismillah/Bismillah.tsx
@@ -1,20 +1,8 @@
 /* eslint-disable i18next/no-literal-string */
 import React from 'react';
 
-import classNames from 'classnames';
-
 import BismillahSVG from '../../../../public/fonts/bismillah/bismillah.svg';
 
-export enum BismillahSize {
-  Small = 'small',
-  Medium = 'medium',
-  Large = 'large',
-}
-
-type BismillahProps = {
-  size?: BismillahSize;
-};
-
-const Bismillah = ({ size = BismillahSize.Medium }: BismillahProps) => <BismillahSVG />;
+const Bismillah = () => <BismillahSVG />;
 
 export default Bismillah;
