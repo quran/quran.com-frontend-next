6fe8871a4af575fee58e0a925943408b8d40fb87
Muhammad Muhajir <muhajirframe@gmail.com>
2021-12-25 19:04:17 +0700

    revert to font

--- src/components/dls/Bismillah/Bismillah.tsx
+++ src/components/dls/Bismillah/Bismillah.tsx
@@ -1,8 +1,29 @@
 /* eslint-disable i18next/no-literal-string */
 import React from 'react';
 
-import BismillahSVG from '../../../../public/fonts/bismillah/bismillah.svg';
+import classNames from 'classnames';
 
-const Bismillah = () => <BismillahSVG />;
+import styles from './Bismillah.module.scss';
+
+export enum BismillahSize {
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
+}
+
+type BismillahProps = {
+  size?: BismillahSize;
+};
+
+const Bismillah = ({ size = BismillahSize.Medium }: BismillahProps) => (
+  <span
+    className={classNames(styles.bismillah, {
+      [styles.bismillahSmall]: size === BismillahSize.Small,
+      [styles.bismillahLarge]: size === BismillahSize.Large,
+    })}
+  >
+    ﷽
+  </span>
+);
 
 export default Bismillah;
