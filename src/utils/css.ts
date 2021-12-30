a76e1319b2f3cc65a02d7ce4a5f73b523c4f6364
osamasayed <osama.sayed.abdelazim@gmail.com>
2021-12-30 08:39:28 +0700

    updates

--- src/utils/css.ts
+++ src/utils/css.ts
@@ -1,7 +1,7 @@
 import { logEvent } from './eventLogger';
 
 /* eslint-disable import/prefer-default-export */
-export const checkForCSSLogicalUnsupported = () => {
+export const logUnsupportedLogicalCSS = () => {
   if (window.CSS && window.CSS.supports) {
     // log when css logical properties are not supported by testing the supportability of "padding-inline" logical property as a sample.
     if (!window.CSS.supports('padding-inline', '10px')) {
