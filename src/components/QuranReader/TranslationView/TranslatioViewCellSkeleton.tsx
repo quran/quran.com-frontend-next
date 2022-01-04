3135f337587045291c17cdc9a271356e5e462ece
Muhammad Muhajir <muhajirframe@gmail.com>
2022-01-04 14:26:09 +0700

    .

--- src/components/QuranReader/TranslationView/TranslatioViewCellSkeleton.tsx
+++ src/components/QuranReader/TranslationView/TranslatioViewCellSkeleton.tsx
@@ -1,25 +1,44 @@
-import { useSelector } from 'react-redux';
+import classNames from 'classnames';
+import { useSelector, shallowEqual } from 'react-redux';
 
 import styles from './TranslationViewSkeleton.module.scss';
 
 import Skeleton from 'src/components/dls/Skeleton/Skeleton';
+import { selectQuranReaderStyles } from 'src/redux/slices/QuranReader/styles';
 import { selectSelectedTranslations } from 'src/redux/slices/QuranReader/translations';
 import { areArraysEqual } from 'src/utils/array';
+import { QuranFont } from 'types/QuranReader';
 
 const TranslationViewCellSkeleton = () => {
   const selectedTranslations = useSelector(selectSelectedTranslations, areArraysEqual);
+  const { quranFont, quranTextFontScale, translationFontScale } = useSelector(
+    selectQuranReaderStyles,
+    shallowEqual,
+  );
+
+  const isTajweedFont = quranFont === QuranFont.Tajweed;
+
   return (
     <div className={styles.cellContainer}>
       <div className={styles.actionsContainers}>
         <Skeleton className={styles.actionContainerLeft} />
         <Skeleton className={styles.actionContainerRight} />
       </div>
-      <Skeleton className={styles.verseContainer} />
-      <div className={styles.translationContainer}>
+      <Skeleton
+        className={classNames(styles.verseContainer, {
+          [styles[`${quranFont}-font-size-${quranTextFontScale}`]]: !isTajweedFont,
+        })}
+      />
+      <div
+        className={classNames(
+          styles.translationContainer,
+          styles[`translation-font-size-${translationFontScale}`],
+        )}
+      >
         {selectedTranslations.map((translation) => (
           <span key={translation}>
-            <Skeleton className={styles.translationText} />
-            <Skeleton className={styles.translationText} />
+            <Skeleton className={classNames(styles.translationText)} />
+            <Skeleton className={classNames(styles.translationText)} />
           </span>
         ))}
       </div>
