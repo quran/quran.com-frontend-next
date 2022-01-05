4a4d88d1c6261ad341c4e3af9c93950705f64223
Muhammad Muhajir <muhajirframe@gmail.com>
2022-01-05 17:59:06 +0700

    get mushaf lines from redux

--- src/components/QuranReader/ReadingView/ReadingViewSkeleton.tsx
+++ src/components/QuranReader/ReadingView/ReadingViewSkeleton.tsx
@@ -7,13 +7,15 @@ import styles from './ReadingViewSkeleton.module.scss';
 import Skeleton from 'src/components/dls/Skeleton/Skeleton';
 import { selectQuranReaderStyles } from 'src/redux/slices/QuranReader/styles';
 import QuranReaderStyles from 'src/redux/types/QuranReaderStyles';
+import { getMushafLinesNumber } from 'src/utils/page';
 
 const ReadingViewSkeleton = () => {
-  const numberOfLines = 15;
-  const { quranFont, quranTextFontScale } = useSelector(
+  const { quranFont, quranTextFontScale, mushafLines } = useSelector(
     selectQuranReaderStyles,
     shallowEqual,
   ) as QuranReaderStyles;
+
+  const numberOfLines = getMushafLinesNumber(mushafLines);
 
   return (
     <div>
