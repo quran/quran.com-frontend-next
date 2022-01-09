699a51e455cfd3a386f921dd058068da36d44fd6
Muhammad Muhajir <muhajirframe@gmail.com>
2022-01-09 16:54:27 +0700

    prototype: verse cell actions left side

--- src/components/QuranReader/TranslationView/TranslationViewCell.tsx
+++ src/components/QuranReader/TranslationView/TranslationViewCell.tsx
@@ -78,17 +78,17 @@ const TranslationViewCell: React.FC<TranslationViewCellProps> = ({
             </div>
           </div>
           <div className={styles.actionContainerRight}>
-            <div className={classNames(styles.actionItem)}>
-              <ShareVerseButton verseKey={verse.verseKey} />
-            </div>
-            <div className={classNames(styles.actionItem)}>
-              <QuranReflectButton verseKey={verse.verseKey} />
-            </div>
             <div className={styles.actionItem}>
               <PlayVerseAudioButton
                 verseKey={verse.verseKey}
                 timestamp={verse.timestamps.timestampFrom}
               />
+            </div>
+            <div className={classNames(styles.actionItem)}>
+              <ShareVerseButton verseKey={verse.verseKey} />
+            </div>
+            <div className={classNames(styles.actionItem)}>
+              <QuranReflectButton verseKey={verse.verseKey} />
             </div>
             <div className={styles.actionItem}>
               <OverflowVerseActionsMenu verse={verse} />
