1b7d5feffa1a36a3bebe922a0ee6c527df48976b
Naveed Ahmad <naveedahmada036@gmail.com>
2022-01-20 11:46:30 +0500

    liniting

--- src/components/chapters/ChapterIcon/index.tsx
+++ src/components/chapters/ChapterIcon/index.tsx
@@ -4,10 +4,6 @@ interface Props {
   id?: string;
 }
 
-const ChapterIcon: React.FC<Props> = ({ id }) => (
-    <span>
-      {id.padStart(3, '0')}
-    </span>
-);
+const ChapterIcon: React.FC<Props> = ({ id }) => <span>{id.padStart(3, '0')}</span>;
 
 export default ChapterIcon;
