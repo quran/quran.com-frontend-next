486a750df7ffd1bc0f80f2613ed62b1d793a97ed
Muhammad Muhajir <muhajirframe@gmail.com>
2022-01-20 20:39:18 +0700

    radio side menu

--- src/components/Navbar/NavigationDrawer/NavigationDrawerBody/index.tsx
+++ src/components/Navbar/NavigationDrawer/NavigationDrawerBody/index.tsx
@@ -12,6 +12,7 @@ import IconProductUpdates from '../../../../../public/icons/product-updates.svg'
 import IconQ from '../../../../../public/icons/Q_simple.svg';
 import QuranReflect from '../../../../../public/icons/QR.svg';
 import IconQuestionMark from '../../../../../public/icons/question-mark.svg';
+import IconRadio2 from '../../../../../public/icons/radio-2.svg';
 import Tarteel from '../../../../../public/icons/tarteel.svg';
 // import MobileApps from '../MobileApps';
 import NavigationDrawerItem from '../NavigationDrawerItem';
@@ -19,7 +20,6 @@ import NavigationDrawerItem from '../NavigationDrawerItem';
 import styles from './NavigationDrawerBody.module.scss';
 
 // import IconDonate from '../../../../../public/icons/donate.svg';
-// import IconRadio2 from '../../../../../public/icons/radio-2.svg';
 // import IconUpdates from '../../../../../public/icons/updates.svg';
 // import IconCollection from '../../../../../public/icons/collection.svg';
 
@@ -47,7 +47,7 @@ const NavigationDrawerBody = () => {
         isExternalLink
       />
       <NavigationDrawerItem title={t('help')} icon={<IconQuestionMark />} href="/support" />
-      {/* <NavigationDrawerItem title="Quran Radio" icon={<IconRadio2 />} /> */}
+      <NavigationDrawerItem href="/radio" title="Quran Radio" icon={<IconRadio2 />} />
       {/* <h3 className={styles.subtitle}>Selected Collections</h3> */}
       {/* <NavigationDrawerItem title="Duaas" icon={<IconCollection />} /> */}
       {/* <NavigationDrawerItem title="Jewels of Quran" icon={<IconCollection />} /> */}
