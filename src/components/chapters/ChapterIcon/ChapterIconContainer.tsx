27b082e301108e8050996bbd8dd532dfbdefea7a
Naveed Ahmad <naveedahmada036@gmail.com>
2022-01-25 02:16:17 +0500

    fixes

--- src/components/chapters/ChapterIcon/ChapterIconContainer.tsx
+++ src/components/chapters/ChapterIcon/ChapterIconContainer.tsx
@@ -12,6 +12,7 @@ export enum ChapterIconsSize {
   Large = 'large',
 }
 
+// TODO: maybe replace `hasSurahPrefix` with `variant` and use it to show v1 or v2 surah name font
 interface Props {
   chapterId: string;
   size?: ChapterIconsSize;
@@ -30,7 +31,7 @@ const IconContainer: React.FC<Props> = ({
     })}
   >
     <ChapterIcon id={chapterId} />
-    {hasSurahPrefix && <ChapterIcon />}
+    {hasSurahPrefix && <ChapterIcon id="surah" />}
   </span>
 );
 
--- src/components/chapters/ChapterIcon/index.tsx
+++ src/components/chapters/ChapterIcon/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 interface Props {
-  id: string;
+  id?: string;
 }
 
 const ChapterIcon: React.FC<Props> = ({ id }) => <span>{id.padStart(3, '0')}</span>;
