5f41f5a8eb43462073fc483d6d7a51450f6bcfce
Muhammad Muhajir <muhajirframe@gmail.com>
2022-02-05 14:53:51 +0700

    chapter header: update play audio mode

--- src/components/QuranReader/PlayChapterAudioButton.tsx
+++ src/components/QuranReader/PlayChapterAudioButton.tsx
@@ -1,13 +1,20 @@
 import useTranslation from 'next-translate/useTranslation';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 
+import PauseIcon from '../../../public/icons/pause.svg';
 import PlayIcon from '../../../public/icons/play-arrow.svg';
+import { triggerPauseAudio, triggerPlayAudio } from '../AudioPlayer/EventTriggers';
 
 import styles from './PlayButton.module.scss';
 
 import Button, { ButtonSize, ButtonType, ButtonVariant } from 'src/components/dls/Button/Button';
 import useGetQueryParamOrReduxValue from 'src/hooks/useGetQueryParamOrReduxValue';
-import { loadAndPlayAudioData } from 'src/redux/slices/AudioPlayer/state';
+import {
+  playFrom,
+  selectAudioData,
+  selectIsPlaying,
+  selectIsRadioMode,
+} from 'src/redux/slices/AudioPlayer/state';
 import { logButtonClick } from 'src/utils/eventLogger';
 import QueryParam from 'types/QueryParam';
 
@@ -17,24 +24,54 @@ interface Props {
 const PlayChapterAudioButton: React.FC<Props> = ({ chapterId }) => {
   const { t } = useTranslation('common');
   const dispatch = useDispatch();
+  const isAudioPlaying = useSelector(selectIsPlaying);
+  const currentAudioData = useSelector(selectAudioData, shallowEqual);
+  const isRadioMode = useSelector(selectIsRadioMode);
+
+  const isPlayingCurrentChapter = isAudioPlaying && currentAudioData?.chapterId === chapterId;
+
   const { value: reciterId }: { value: number } = useGetQueryParamOrReduxValue(QueryParam.Reciter);
   const play = () => {
     logButtonClick('chapter_header_play_audio');
-    dispatch(loadAndPlayAudioData({ chapter: chapterId, reciterId }));
+    if (currentAudioData?.chapterId === chapterId && !isRadioMode) {
+      triggerPlayAudio();
+    } else {
+      dispatch(playFrom({ chapterId, reciterId, timestamp: 0 }));
+    }
   };
+
+  const pause = () => {
+    logButtonClick('chapter_header_pause_audio');
+    triggerPauseAudio();
+  };
+
   return (
     <div className={styles.container}>
-      <Button
-        variant={ButtonVariant.Ghost}
-        type={ButtonType.Success}
-        size={ButtonSize.Small}
-        prefix={<PlayIcon />}
-        onClick={play}
-        hasSidePadding={false}
-        shouldFlipOnRTL={false}
-      >
-        {t('audio.play')}
-      </Button>
+      {isPlayingCurrentChapter ? (
+        <Button
+          variant={ButtonVariant.Ghost}
+          type={ButtonType.Success}
+          size={ButtonSize.Small}
+          prefix={<PauseIcon />}
+          onClick={pause}
+          hasSidePadding={false}
+          shouldFlipOnRTL={false}
+        >
+          {t('audio.player.pause-audio')}
+        </Button>
+      ) : (
+        <Button
+          variant={ButtonVariant.Ghost}
+          type={ButtonType.Success}
+          size={ButtonSize.Small}
+          prefix={<PlayIcon />}
+          onClick={play}
+          hasSidePadding={false}
+          shouldFlipOnRTL={false}
+        >
+          {t('audio.play')}
+        </Button>
+      )}
     </div>
   );
 };
