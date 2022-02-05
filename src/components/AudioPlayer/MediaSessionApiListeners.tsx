b0955a0c088361c5497c315e95ccb3df0b669771
Muhammad Muhajir <muhajirframe@gmail.com>
2022-02-05 13:26:26 +0700

    Implement audio player media session (#1191)
    * Implement audio player media session
    
    * .
    
    * .

--- src/components/AudioPlayer/MediaSessionApiListeners.tsx
+++ src/components/AudioPlayer/MediaSessionApiListeners.tsx
@@ -1,4 +1,12 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
+
+import { shallowEqual, useSelector } from 'react-redux';
+
+import useCurrentStationInfo from '../Radio/useStationInfo';
+
+import { getReciterData } from 'src/api';
+import { selectAudioPlayerState } from 'src/redux/slices/AudioPlayer/state';
+import { getChapterData } from 'src/utils/chapter';
 
 const SEEK_DURATION_SECONDS = 5;
 
@@ -10,6 +18,15 @@ type MediaSessionApiListenersProps = {
   playNextTrack: () => void;
 };
 
+const QURAN_COM_ARTWORK = [
+  { src: '/images/logo/Logo@96x96.png', sizes: '96x96', type: 'image/png' },
+  { src: '/images/logo/Logo@128x128.png', sizes: '128x128', type: 'image/png' },
+  { src: '/images/logo/Logo@192x192.png', sizes: '192x192', type: 'image/png' },
+  { src: '/images/logo/Logo@256x256.png', sizes: '256x256', type: 'image/png' },
+  { src: '/images/logo/Logo@384x384.png', sizes: '384x384', type: 'image/png' },
+  { src: '/images/logo/Logo@512x512.png', sizes: '512x512', type: 'image/png' },
+];
+
 const MediaSessionApiListeners = ({
   play,
   pause,
@@ -17,23 +34,42 @@ const MediaSessionApiListeners = ({
   playPreviousTrack,
   playNextTrack,
 }: MediaSessionApiListenersProps) => {
+  const audioPlayerState = useSelector(selectAudioPlayerState, shallowEqual);
+  const stationInfo = useCurrentStationInfo();
+
+  const { isRadioMode } = audioPlayerState;
+
+  const getRadioMediaMetadata = useCallback(async () => {
+    return new MediaMetadata({
+      title: stationInfo.title,
+      artist: stationInfo.description,
+      album: 'Quran.com',
+      // TODO: replace with station image
+      artwork: QURAN_COM_ARTWORK,
+    });
+  }, [stationInfo.description, stationInfo.title]);
+
+  const getAudioMediaMetadata = useCallback(async () => {
+    const chapterData = getChapterData(audioPlayerState.audioData.chapterId.toString());
+    const reciterData = await getReciterData(audioPlayerState.reciter.id.toString());
+
+    return new MediaMetadata({
+      title: chapterData.transliteratedName,
+      artist: reciterData?.reciter?.name,
+      album: 'Quran.com',
+      // TODO: replace with reciter image
+      artwork: QURAN_COM_ARTWORK,
+    });
+  }, [audioPlayerState.audioData.chapterId, audioPlayerState.reciter.id]);
+
   useEffect(() => {
     if ('mediaSession' in navigator) {
-      navigator.mediaSession.metadata = new MediaMetadata({
-        title: 'Al Baqarah', // Placeholder
-        artist: 'Al-Affasy', // Placeholder
-        album: 'Quran.com',
-        artwork: [
-          { src: '/images/logo/Logo@96x96.png', sizes: '96x96', type: 'image/png' },
-          { src: '/images/logo/Logo@128x128.png', sizes: '128x128', type: 'image/png' },
-          { src: '/images/logo/Logo@192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/images/logo/Logo@256x256.png', sizes: '256x256', type: 'image/png' },
-          { src: '/images/logo/Logo@384x384.png', sizes: '384x384', type: 'image/png' },
-          { src: '/images/logo/Logo@512x512.png', sizes: '512x512', type: 'image/png' },
-        ],
+      const mediaMetaData = isRadioMode ? getRadioMediaMetadata() : getAudioMediaMetadata();
+      mediaMetaData.then((metaData) => {
+        navigator.mediaSession.metadata = metaData;
       });
     }
-  }, []);
+  }, [getAudioMediaMetadata, getRadioMediaMetadata, isRadioMode]);
 
   useEffect(() => {
     if ('mediaSession' in navigator) {
