b7bbf7514a2c437c8c48a0480cb2908325451373
osamasayed <osamasayed@users.noreply.github.com>
2022-02-15 09:55:23 +0700

    Update error revalidation period (#1251)

--- src/utils/staticPageGeneration.ts
+++ src/utils/staticPageGeneration.ts
@@ -1,4 +1,4 @@
-export const REVALIDATION_PERIOD_ON_ERROR_SECONDS = 35;
+export const REVALIDATION_PERIOD_ON_ERROR_SECONDS = 604800; // 7 days. TODO: @osama update the naming + value when we revert this back
 export const ONE_DAY_REVALIDATION_PERIOD_SECONDS = 86400; // 1 day
 export const ONE_WEEK_REVALIDATION_PERIOD_SECONDS = 604800; // 7 days
 export const ONE_MONTH_REVALIDATION_PERIOD_SECONDS = 2592000; // 30  days
