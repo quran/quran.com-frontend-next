d339a6ba05cc6c06a557abc0ec26580c82fb756f
osamasayed <osamasayed@users.noreply.github.com>
2022-02-16 09:24:30 +0700

    Remove Prayer Times (#1256)

--- src/utils/apiPaths.ts
+++ src/utils/apiPaths.ts
@@ -1,12 +1,10 @@
 import { ITEMS_PER_PAGE, makeUrl } from './api';
-import stringify from './qs-stringify';
 
 import {
   getAudioPlayerStateInitialState,
   getReadingPreferencesInitialState,
   getTranslationsInitialState,
 } from 'src/redux/defaultSettings/util';
-import { CalculationMethod, Madhab } from 'src/redux/slices/prayerTimes';
 import { AdvancedCopyRequest, PagesLookUpRequest, SearchRequest } from 'types/ApiRequests';
 import { QuranFont } from 'types/QuranReader';
 
@@ -214,32 +212,3 @@ export const makePageVersesUrl = (
  * @returns {string}
  */
 export const makeFootnoteUrl = (footnoteId: string): string => makeUrl(`/foot_notes/${footnoteId}`);
-
-// TODO: replace this url
-const PRAYER_TIMES_URL =
-  'https://quran-prayer-times-api-abdellatif-io-qurancom.vercel.app/api/prayer-times';
-
-/**
- * Compose the url for prayer times API
- *
- * @param {Object} query
- * @param {number} query.latitude
- * @param {number} query.longtitude
- * @param {CalculationMethod} query.calculationMethod
- * @param {Madhab} query.madhab
- * @returns {string}
- */
-export const makePrayerTimesUrl = (query: {
-  calculationMethod: CalculationMethod;
-  madhab: Madhab;
-  latitude?: number;
-  longtitude?: number;
-}) => {
-  const today = new Date();
-  const date = today.getDate();
-  const month = today.getMonth();
-  const year = today.getFullYear();
-
-  const queryParameters = `?${stringify({ ...query, date, month, year })}`;
-  return `${PRAYER_TIMES_URL}${queryParameters}`;
-};
