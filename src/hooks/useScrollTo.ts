d745600b4b79f5fde0f8636a3ad64f8c7ed0214f
Muhammad Muhajir <muhajirframe@gmail.com>
2022-02-18 13:52:46 +0700

    update doc

--- src/hooks/useScrollTo.ts
+++ src/hooks/useScrollTo.ts
@@ -2,6 +2,16 @@ import { useEffect, useRef } from 'react';
 
 const DEFAULT_SCROLL_MARGIN = 300;
 
+/**
+ *
+ * Scroll to a specific element
+ * How to use: pass the returned `selectedItemRef` to the element you want to scroll to
+ *
+ * @param {Function} scrollAbleParentMapper - A function that returns the scrollable parent of the element.
+ * (element that has overflow: auto/scroll)
+ * @param {number} scrollMargin - The margin to be added to the scrollable parent.
+ * @returns {MutableRefObject} selectedItemRef - React ref
+ */
 const useScrollTo = (
   scrollAbleParentMapper = (node) => node.parentNode,
   scrollMargin: number = DEFAULT_SCROLL_MARGIN,
