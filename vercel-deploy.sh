2dd09add11a480d69919d57e6a50471732fc8a79
osamasayed <osama.sayed.abdelazim@gmail.com>
2022-02-26 17:34:10 +0700

    updates

--- vercel-deploy.sh
+++ vercel-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $VERCEL_ENV == "production" ]]; then
-    yarn node scripts/purge-cloudflare-cache.js $CF_AUTH_KEY $CF_ZONE_ID && yarn build
+    yarn build && yarn node scripts/purge-cloudflare-cache.js $CF_AUTH_KEY $CF_ZONE_ID
 else
     yarn build
 fi
