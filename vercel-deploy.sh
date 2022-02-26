ca8c49b007072712320c00d48cab41f6e6ff7375
osamasayed <osama.sayed.abdelazim@gmail.com>
2022-02-26 16:15:12 +0700

    updates

--- vercel-deploy.sh
+++ vercel-deploy.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo "VERCEL_ENV $VERCEL_ENV"
-echo "CF_AUTH_KEY $CF_AUTH_KEY"
-echo "CF_ZONE_ID $CF_ZONE_ID"
-
 # if [[ $VERCEL_ENV == "production" ]]; then
-if [[ $VERCEL_ENV == "development" ]]; then
+if [[ $VERCEL_ENV == "preview" ]]; then
     yarn node scripts/purge-cloudflare-cache.js $CF_AUTH_KEY $CF_ZONE_ID && yarn build
 else
     yarn build
