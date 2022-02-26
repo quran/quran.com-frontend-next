03347a3e18da1a66c1c4721bba42a3a0716b8f9f
osamasayed <osama.sayed.abdelazim@gmail.com>
2022-02-26 15:31:56 +0700

    test commit

---
+++ vercel-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# if [[ $VERCEL_ENV == "production" ]]; then
+if [[ $VERCEL_ENV == "development" ]]; then
+    yarn node scripts/purge-cloudflare-cache.js $CF_AUTH_KEY $CF_ZONE_ID && yarn build
+else
+    yarn build
+fi
