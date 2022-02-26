38974452c4666eefec9aae425581fe155ee558af
osamasayed <osamasayed@users.noreply.github.com>
2022-02-26 19:25:36 +0700

    Purge CF cache (#1312)
    * Purge CF cache

---
+++ scripts/purge-cloudflare-cache.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+
+const request = require('superagent');
+
+const CF_AUTH_KEY = process.argv[2];
+const CF_ZONE_ID = process.argv[3];
+
+console.log('Cloudflare cache purging process started...');
+request
+  .post(`https://api.cloudflare.com/client/v4/zones/${CF_ZONE_ID}/purge_cache`)
+  .send({ purge_everything: true })
+  .set({
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${CF_AUTH_KEY}`,
+  })
+  .then(() => {
+    console.log('Cloudflare cache purged successfully!');
+  })
+  .catch((error) => {
+    const test = JSON.parse(error.response.text);
+    throw Error(
+      `Cloudflare cache purging error (${test.errors[0].code}) ${test.errors[0].message}`,
+    );
+  });
