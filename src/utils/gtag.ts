c1f7e0880d3c6e5fd8eb04b2760b2210cef65559
Muhammad Muhajir <muhajirframe@gmail.com>
2022-03-07 21:11:00 +0700

    :D

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -2,9 +2,10 @@ const ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 
 // eslint-disable-next-line import/prefer-default-export
 export const pageView = (url: string) => {
-  if (window.gtag)
+  if (window.gtag) {
     window.gtag('config', ANALYTICS_ID, {
       // eslint-disable-next-line @typescript-eslint/naming-convention
       page_path: url,
     });
+  }
 };
