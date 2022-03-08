71d26aa8404e0dde126b21c853b1a22bd92b0706
Muhammad Muhajir <muhajirframe@gmail.com>
2022-03-08 15:20:30 +0700

    .

--- src/components/QuranReader/TranslationView/QuranReflectButton.tsx
+++ src/components/QuranReader/TranslationView/QuranReflectButton.tsx
@@ -65,7 +65,6 @@ const QuranReflectButton = ({
       <ReflectionBody
         initialChapterId={initialChapterId}
         initialVerseNumber={verseNumber}
-        shouldRender={isContentModalOpen}
         scrollToTop={() => {
           // noop
         }}
