b2196a4d7b4a8e411e7ebf528a794dfc81b0b295
Muhammad Muhajir <muhajirframe@gmail.com>
2022-03-09 15:48:31 +0700

    prorotype: translation view cell polish

--- src/components/QuranReader/TranslationView/TranslationViewCell.tsx
+++ src/components/QuranReader/TranslationView/TranslationViewCell.tsx
@@ -62,30 +62,27 @@ const TranslationViewCell: React.FC<TranslationViewCellProps> = ({
         })}
       >
         <div className={styles.actionContainer}>
-          <div className={styles.actionContainerLeft}>
-            <div className={styles.actionItem}>
-              <VerseLink verseKey={verse.verseKey} />
-            </div>
-            <div className={styles.actionItem}>
-              <BookmarkIcon verseKey={verse.verseKey} />
-            </div>
+          <div className={styles.actionItem}>
+            <VerseLink verseKey={verse.verseKey} />
           </div>
-          <div className={styles.actionContainerRight}>
-            <div className={classNames(styles.actionItem)}>
-              <ShareVerseButton verseKey={verse.verseKey} />
-            </div>
-            <div className={classNames(styles.actionItem)}>
-              <QuranReflectButton verseKey={verse.verseKey} />
-            </div>
-            <div className={styles.actionItem}>
-              <PlayVerseAudioButton
-                verseKey={verse.verseKey}
-                timestamp={verse.timestamps.timestampFrom}
-              />
-            </div>
-            <div className={styles.actionItem}>
-              <OverflowVerseActionsMenu verse={verse} isModal isPortalled />
-            </div>
+          <div className={styles.actionItem}>
+            <BookmarkIcon verseKey={verse.verseKey} />
+          </div>
+
+          <div className={classNames(styles.actionItem)}>
+            <ShareVerseButton verseKey={verse.verseKey} />
+          </div>
+          <div className={classNames(styles.actionItem)}>
+            <QuranReflectButton verseKey={verse.verseKey} />
+          </div>
+          <div className={styles.actionItem}>
+            <PlayVerseAudioButton
+              verseKey={verse.verseKey}
+              timestamp={verse.timestamps.timestampFrom}
+            />
+          </div>
+          <div className={styles.actionItem}>
+            <OverflowVerseActionsMenu verse={verse} isModal isPortalled />
           </div>
         </div>
 
