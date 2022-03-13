7d48ad118504de990b5aa219480bd913214a041d
osamasayed <osama.sayed.abdelazim@gmail.com>
2022-03-13 21:53:23 +0700

    Updates

--- next.config.js
+++ next.config.js
@@ -28,7 +28,7 @@ const config = {
     dest: 'public',
     runtimeCaching,
     publicExcludes: [
-      '!fonts/**/!(sura_names)*', // exclude pre-caching all fonts that are not sura_names
+      '!fonts/**/!(sura_names|ProximaVara)*', // exclude pre-caching all fonts that are not sura_names or ProximaVara
       '!icons/**', // exclude all icons
       '!images/**/!(background|homepage)*', // don't pre-cache except background.jpg and homepage.png
     ],
