adcbd2c2231a602608d39c6ec53905f9552bbbd2
osamasayed <osamasayed@users.noreply.github.com>
2022-03-23 19:33:42 +0700

    [Web Vitals] fix Logo doesn't have a discernible name (#1494)

--- src/components/Navbar/Logo/NavbarLogoWrapper.tsx
+++ src/components/Navbar/Logo/NavbarLogoWrapper.tsx
@@ -1,3 +1,5 @@
+import useTranslation from 'next-translate/useTranslation';
+
 import QuranTextLogo from '../../../../public/icons/quran-text-logo.svg';
 
 import styles from './NavbarLogoWrapper.module.scss';
@@ -5,8 +7,9 @@ import styles from './NavbarLogoWrapper.module.scss';
 import Link from 'src/components/dls/Link/Link';
 
 const NavbarLogoWrapper = () => {
+  const { t } = useTranslation('common');
   return (
-    <Link href="/" className={styles.logoWrapper}>
+    <Link href="/" className={styles.logoWrapper} title={t('quran-com')}>
       <QuranTextLogo />
     </Link>
   );
