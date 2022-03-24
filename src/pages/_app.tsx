e5da838c088d339d2cb46229235ffbc3f65d0b63
osamasayed <osama.sayed.abdelazim@gmail.com>
2022-03-24 19:02:11 +0700

    Remove ahrefs validation meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -53,10 +53,6 @@ function MyApp({ Component, pageProps }): JSX.Element {
         <link rel="apple-touch-icon" sizes="192x192" href="/images/logo/Logo@192x192.png" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href={API_HOST} />
-        <meta
-          name="ahrefs-site-verification"
-          content="d9e06610b3345cfc8c1e17a750df73c73ad1934603f0201751d6f88c1b96410c"
-        />
       </Head>
       <FontPreLoader locale={locale} />
       <ReduxProvider locale={locale}>
