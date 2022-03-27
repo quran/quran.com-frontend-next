0df1bf23371d89c20efe1135c6632a077471f627
osamasayed <osamasayed@users.noreply.github.com>
2022-03-27 18:44:09 +0700

    [Web Vitals] Preload homepage's background image (#1520)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import classNames from 'classnames';
 import { NextPage, GetStaticProps } from 'next';
 import useTranslation from 'next-translate/useTranslation';
+import Head from 'next/head';
 
 import styles from './index.module.scss';
 
@@ -30,36 +31,41 @@ type IndexProps = {
 const Index: NextPage<IndexProps> = ({ chaptersData, chaptersResponse: { chapters } }) => {
   const { t, lang } = useTranslation('home');
   return (
-    <DataContext.Provider value={chaptersData}>
-      <NextSeoWrapper
-        title={t('noble-quran')}
-        url={getCanonicalUrl(lang, '')}
-        languageAlternates={getLanguageAlternates('')}
-      />
-      <div className={styles.pageContainer}>
-        <div className={classNames(styles.listContainer, styles.flow)}>
-          <HomePageHero />
-          <div className={styles.flowItem}>
-            <HomePageWelcomeMessage />
-          </div>
-          <div className={classNames(styles.flowItem, styles.fullWidth)}>
-            <RecentReadingSessions />
-          </div>
-          <div className={classNames(styles.flowItem, styles.fullWidth)}>
-            <BookmarksSection />
-          </div>
-          <div className={styles.flowItem}>
-            <ChapterAndJuzListWrapper chapters={chapters} />
-          </div>
-          <div className={styles.flowItem}>
-            <Separator />
-          </div>
-          <div className={styles.flowItem}>
-            <Footer />
+    <>
+      <Head>
+        <link rel="preload" as="image" href="/images/background.jpg" crossOrigin="anonymous" />
+      </Head>
+      <DataContext.Provider value={chaptersData}>
+        <NextSeoWrapper
+          title={t('noble-quran')}
+          url={getCanonicalUrl(lang, '')}
+          languageAlternates={getLanguageAlternates('')}
+        />
+        <div className={styles.pageContainer}>
+          <div className={classNames(styles.listContainer, styles.flow)}>
+            <HomePageHero />
+            <div className={styles.flowItem}>
+              <HomePageWelcomeMessage />
+            </div>
+            <div className={classNames(styles.flowItem, styles.fullWidth)}>
+              <RecentReadingSessions />
+            </div>
+            <div className={classNames(styles.flowItem, styles.fullWidth)}>
+              <BookmarksSection />
+            </div>
+            <div className={styles.flowItem}>
+              <ChapterAndJuzListWrapper chapters={chapters} />
+            </div>
+            <div className={styles.flowItem}>
+              <Separator />
+            </div>
+            <div className={styles.flowItem}>
+              <Footer />
+            </div>
           </div>
         </div>
-      </div>
-    </DataContext.Provider>
+      </DataContext.Provider>
+    </>
   );
 };
 
