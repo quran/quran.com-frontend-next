b863cbe9b83175280216e12d64f85299e1c27b36
Muhammad Muhajir <muhajirframe@gmail.com>
2022-04-04 12:52:37 +0700

    Make generic banner component

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -5,12 +5,18 @@ import CloseIcon from '../../../public/icons/close.svg';
 import MoonIllustrationSVG from '../../../public/images/moon-illustration.svg';
 import Button, { ButtonSize, ButtonType, ButtonVariant } from '../dls/Button/Button';
 
-import styles from './FundraisingStickyHeader.module.scss';
+import styles from './Banner.module.scss';
 
 import { setIsBannerVisible } from 'src/redux/slices/banner';
 
-const FundraisingStickyBanner = () => {
-  const { t } = useTranslation('common');
+type BannerProps = {
+  onClick?: () => void;
+  href?: string;
+  text: string;
+  cta: string;
+};
+
+const Banner = ({ text, href, cta, onClick }: BannerProps) => {
   const dispatch = useDispatch();
   const closeBanner = () => {
     dispatch(setIsBannerVisible(false));
@@ -21,11 +27,18 @@ const FundraisingStickyBanner = () => {
         <div className={styles.illustrationContainer}>
           <MoonIllustrationSVG />
         </div>
-        <div>{t('fundraising-sticky-banner.title')}</div>
+        <div>{text}</div>
       </div>
       <div className={styles.ctaContainer}>
-        <Button className={styles.cta} size={ButtonSize.Small} type={ButtonType.Success}>
-          {t('fundraising-sticky-banner.cta')}
+        <Button
+          isNewTab
+          href={href}
+          onClick={onClick}
+          className={styles.cta}
+          size={ButtonSize.Small}
+          type={ButtonType.Success}
+        >
+          {cta}
         </Button>
       </div>
       <div className={styles.closeButton}>
@@ -37,4 +50,4 @@ const FundraisingStickyBanner = () => {
   );
 };
 
-export default FundraisingStickyBanner;
+export default Banner;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
 
 import classNames from 'classnames';
+import useTranslation from 'next-translate/useTranslation';
 import { useSelector, shallowEqual } from 'react-redux';
 
-import FundraisingStickyBanner from '../Fundaraising/FundraisingStickyBanner';
+import Banner from '../Banner/Banner';
 
 import styles from './Navbar.module.scss';
 import NavbarBody from './NavbarBody';
 
 import { selectIsBannerVisible } from 'src/redux/slices/banner';
 import { selectNavbar } from 'src/redux/slices/navbar';
+import { logButtonClick } from 'src/utils/eventLogger';
 
 const Navbar = () => {
+  const { t } = useTranslation('common');
   const { isVisible: isNavbarVisible } = useSelector(selectNavbar, shallowEqual);
   const isBannerVisible = useSelector(selectIsBannerVisible);
   return (
     <>
       <div className={styles.emptySpacePlaceholder} />
       <nav className={classNames(styles.container, { [styles.hiddenNav]: !isNavbarVisible })}>
-        {isBannerVisible && <FundraisingStickyBanner />}
+        {isBannerVisible && (
+          <Banner
+            cta={t('fundraising-sticky-banner.cta')}
+            text={t('fundraising-sticky-banner.title')}
+            href="https://donate.quran.com"
+            onClick={() => {
+              logButtonClick('banner_cta');
+            }}
+          />
+        )}
         <NavbarBody />
       </nav>
     </>
