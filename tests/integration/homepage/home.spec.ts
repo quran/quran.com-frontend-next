fe90fc8263a81776cb4c715f9d2b215e2f0f4689
osamasayed <osamasayed@users.noreply.github.com>
2022-04-06 12:49:20 +0200

    Updates

--- tests/integration/homepage/home.spec.ts
+++ tests/integration/homepage/home.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
 });
 
 test('Navigation drawer icon opens the drawer when clicked', async ({ page }) => {
