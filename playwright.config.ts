0802eb017b3f86341e9c5bcd26c601daa63dc276
osamasayed <osama.sayed.abdelazim@gmail.com>
2022-04-06 16:38:25 +0200

    Updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,12 +3,6 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
