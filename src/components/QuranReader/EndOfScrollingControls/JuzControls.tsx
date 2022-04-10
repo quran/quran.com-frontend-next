6fb60aa7f7ad4c7bd1149a8b8545670119db2b77
Muhammad Muhajir <muhajirframe@gmail.com>
2022-04-10 13:40:42 +0700

    Add arrow to end of scrolling actions (#1568)
    * .
    
    * .

--- src/components/QuranReader/EndOfScrollingControls/JuzControls.tsx
+++ src/components/QuranReader/EndOfScrollingControls/JuzControls.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import useTranslation from 'next-translate/useTranslation';
 
-import EndOfScrollingButton from './EndOfScrollingButton';
+import ChevronLeftIcon from '../../../../public/icons/chevron-left.svg';
+import ChevronRightIcon from '../../../../public/icons/chevron-right.svg';
 
+import Button, { ButtonType } from 'src/components/dls/Button/Button';
 import useScrollToTop from 'src/hooks/useScrollToTop';
 import { logButtonClick } from 'src/utils/eventLogger';
 import { isFirstJuz, isLastJuz } from 'src/utils/juz';
@@ -21,29 +23,39 @@ const JuzControls: React.FC<Props> = ({ lastVerse }) => {
   return (
     <>
       {!isFirstJuz(juzNumber) && (
-        <EndOfScrollingButton
-          text={t('prev-juz')}
+        <Button
+          type={ButtonType.Secondary}
+          prefix={<ChevronLeftIcon />}
           href={getJuzNavigationUrl(juzNumber - 1)}
           onClick={() => {
             logButtonClick('juz_control_prev_juz');
           }}
-        />
+        >
+          {t('prev-juz')}
+        </Button>
       )}
-      <EndOfScrollingButton
-        text={t('juz-beginning')}
+      <Button
+        type={ButtonType.Secondary}
+        href={getJuzNavigationUrl(juzNumber - 1)}
         onClick={() => {
           logButtonClick('juz_control_scroll_to_beginning');
           scrollToTop();
         }}
-      />
+      >
+        {t('juz-beginning')}
+      </Button>
+
       {!isLastJuz(juzNumber) && (
-        <EndOfScrollingButton
-          text={t('next-juz')}
+        <Button
+          type={ButtonType.Secondary}
+          suffix={<ChevronRightIcon />}
           href={getJuzNavigationUrl(juzNumber + 1)}
           onClick={() => {
             logButtonClick('juz_control_next_juz');
           }}
-        />
+        >
+          {t('next-juz')}
+        </Button>
       )}
     </>
   );
