b2f8a8d9c5e307df643c8adf67786a9a17244106
Muhammad Muhajir <muhajirframe@gmail.com>
2022-04-13 18:20:58 +0700

    Update donation banner (#1578)
    * donation banner always visible
    
    * .
    
    * .
    
    * update banner
    
    * .
    
    * .
    
    * .
    
    * .

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,30 +9,28 @@ import Banner from '../Banner/Banner';
 import styles from './Navbar.module.scss';
 import NavbarBody from './NavbarBody';
 
-import { selectIsBannerVisible } from 'src/redux/slices/banner';
+// import { selectIsBannerVisible } from 'src/redux/slices/banner';
 import { selectNavbar } from 'src/redux/slices/navbar';
 import { logEvent } from 'src/utils/eventLogger';
 
 const Navbar = () => {
   const { t } = useTranslation('common');
   const { isVisible: isNavbarVisible } = useSelector(selectNavbar, shallowEqual);
-  const isBannerVisible = useSelector(selectIsBannerVisible);
+  // const isBannerVisible = useSelector(selectIsBannerVisible);
   return (
     <>
       <div className={styles.emptySpacePlaceholder} />
       <nav className={classNames(styles.container, { [styles.hiddenNav]: !isNavbarVisible })}>
-        {isBannerVisible && (
-          <Banner
-            cta={t('fundraising-sticky-banner.cta')}
-            text={t('fundraising-sticky-banner.title')}
-            href="https://donate.quran.com"
-            onClick={() => {
-              logEvent('donate_button_clicked', {
-                source: 'sticky_banner',
-              });
-            }}
-          />
-        )}
+        <Banner
+          cta={t('fundraising-sticky-banner.cta')}
+          text={t('fundraising-sticky-banner.title')}
+          href="https://donate.quran.com"
+          onClick={() => {
+            logEvent('donate_button_clicked', {
+              source: 'sticky_banner',
+            });
+          }}
+        />
         <NavbarBody />
       </nav>
     </>
