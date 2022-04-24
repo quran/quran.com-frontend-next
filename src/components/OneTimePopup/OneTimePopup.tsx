1003d7d7581e8955db326cc89874e2611fe9482a
Muhammad Muhajir <muhajirframe@gmail.com>
2022-04-24 07:19:38 +0700

    make one time popup

---
+++ src/components/OneTimePopup/OneTimePopup.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+
+import useTranslation from 'next-translate/useTranslation';
+import { useDispatch, useSelector } from 'react-redux';
+
+import CloseIcon from '../../../public/icons/close.svg';
+import Button, { ButtonShape, ButtonType, ButtonVariant } from '../dls/Button/Button';
+import Modal from '../dls/Modal/Modal';
+
+import styles from './OneTimePopup.module.scss';
+
+import { selectIsPopupVisible, setIsPopupVisible } from 'src/redux/slices/popup';
+
+const OneTimePopup = () => {
+  const { t } = useTranslation('common');
+  const isPopupVisible = useSelector(selectIsPopupVisible);
+  const [isDonateButtonLoading, setIsDonateButtonLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  const onDonateButtonClicked = () => {
+    // @ts-ignore
+    window.givingloop('donate');
+    setIsDonateButtonLoading(true);
+    setTimeout(() => {
+      setIsDonateButtonLoading(false);
+
+      // Unfortunately, we need to close this popup. Otherwise the giving loop popup won't be clickable
+      dispatch(setIsPopupVisible(false));
+    }, 5000);
+  };
+
+  if (!isPopupVisible) return null;
+
+  const onCloseButtonClicked = () => {
+    dispatch(setIsPopupVisible(false));
+  };
+
+  return (
+    <Modal isOpen>
+      <div className={styles.container}>
+        <Button
+          className={styles.closeIcon}
+          variant={ButtonVariant.Ghost}
+          shape={ButtonShape.Circle}
+          onClick={onCloseButtonClicked}
+        >
+          <CloseIcon />
+        </Button>
+        <h1 className={styles.title}>{t('popup.title')}</h1>
+        <div className={styles.textsContainer}>
+          <p className={styles.text}>{t('popup.text-1')}</p>
+          <p className={styles.text}>{t('popup.text-2')}</p>
+        </div>
+        <div className={styles.actionsContainer}>
+          <Button
+            className={styles.action}
+            type={ButtonType.Success}
+            onClick={onDonateButtonClicked}
+            isLoading={isDonateButtonLoading}
+          >
+            {t('popup.cta-1')}
+          </Button>
+          <Button
+            href="https://donate.quran.com"
+            isNewTab
+            className={styles.action}
+            type={ButtonType.Success}
+            variant={ButtonVariant.Outlined}
+          >
+            {t('popup.cta-2')}
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default OneTimePopup;
