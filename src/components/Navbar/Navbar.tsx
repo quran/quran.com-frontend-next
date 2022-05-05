c04a0c7b7bc53fa2829d4c3ad1732fe6063c363d
Muhammad Muhajir <muhajirframe@gmail.com>
2022-05-05 20:57:08 +0700

    remove comments

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,34 +1,19 @@
 import React from 'react';
 
 import classNames from 'classnames';
-// import useTranslation from 'next-translate/useTranslation';
 import { useSelector, shallowEqual } from 'react-redux';
-
-// import Banner from '../Banner/Banner';
 
 import styles from './Navbar.module.scss';
 import NavbarBody from './NavbarBody';
 
 import { selectNavbar } from 'src/redux/slices/navbar';
-// import { logEvent } from 'src/utils/eventLogger';
 
 const Navbar = () => {
-  // const { t } = useTranslation('common');
   const { isVisible: isNavbarVisible } = useSelector(selectNavbar, shallowEqual);
   return (
     <>
       <div className={styles.emptySpacePlaceholder} />
       <nav className={classNames(styles.container, { [styles.hiddenNav]: !isNavbarVisible })}>
-        {/* <Banner
-          cta={t('fundraising-sticky-banner.cta')}
-          text={t('fundraising-sticky-banner.title')}
-          href="https://donate.quran.com"
-          onClick={() => {
-            logEvent('donate_button_clicked', {
-              source: 'sticky_banner',
-            });
-          }}
-        /> */}
         <NavbarBody />
       </nav>
     </>
