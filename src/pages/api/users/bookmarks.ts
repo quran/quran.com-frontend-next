37190e88684b288f4e17e72f4836008d83e93bc5
Muhammad Muhajir <muhajirframe@gmail.com>
2022-05-16 20:54:00 +0700

    .

--- src/pages/api/users/bookmarks.ts
+++ src/pages/api/users/bookmarks.ts
@@ -1,19 +1,16 @@
 /* eslint-disable react-func/max-lines-per-function */
-import cookie from 'cookie'
-import type { NextApiResponse } from 'next'
+import cookie from 'cookie';
+import type { NextApiResponse } from 'next';
 
-import withProtect from 'src/middleware/withProtect'
-import withRefreshToken from 'src/middleware/withRefreshToken'
-import {
-  ACCESS_TOKEN_COOKIE_NAME,
-  REFRESH_TOKEN_COOKIE_NAME
-} from 'src/utils/auth/constants'
-import { getAuthApiPath } from 'src/utils/url'
-import NextApiRequestWithTokens from 'types/NextApiRequestWithTokens'
+import withProtect from 'src/middleware/withProtect';
+import withRefreshToken from 'src/middleware/withRefreshToken';
+import { ACCESS_TOKEN_COOKIE_NAME, REFRESH_TOKEN_COOKIE_NAME } from 'src/utils/auth/constants';
+import { getAuthApiPath } from 'src/utils/url';
+import NextApiRequestWithTokens from 'types/NextApiRequestWithTokens';
 
 const getAccessToken = (req) => {
-  return req.accessToken || req.cookies[ACCESS_TOKEN_COOKIE_NAME]
-}
+  return req.accessToken || req.cookies[ACCESS_TOKEN_COOKIE_NAME];
+};
 
 const fetchBookmarks = (accessToken) =>
   fetch(getAuthApiPath('bookmarks'), {
@@ -21,9 +18,9 @@ const fetchBookmarks = (accessToken) =>
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
       // eslint-disable-next-line i18next/no-literal-string
-      Authorization: `bearer ${accessToken}`
-    }
-  })
+      Authorization: `bearer ${accessToken}`,
+    },
+  });
 
 const setAccessToken = (req, res, jsonResponse) => {
   if (req.accessToken && req.refreshToken) {
@@ -36,56 +33,52 @@ const setAccessToken = (req, res, jsonResponse) => {
           secure: process.env.NODE_ENV !== 'development',
           sameSite: true,
           // maxAge: accessTokenExpiresInSeconds, // TODO: update this
-          path: '/'
+          path: '/',
         }),
         cookie.serialize(REFRESH_TOKEN_COOKIE_NAME, req.refreshToken, {
           httpOnly: true,
           secure: process.env.NODE_ENV !== 'development',
           sameSite: true,
           // maxAge: refreshTokenExpiresInSeconds, // TODO: update this
-          path: '/'
-        })
-      ]
-    })
+          path: '/',
+        }),
+      ],
+    });
   }
-}
+};
 
 const getBookmarksByUserId = async (req, res) => {
-  const accessToken = getAccessToken(req)
-  const response = await fetchBookmarks(accessToken)
-  const jsonResponse = await response.json()
+  const accessToken = getAccessToken(req);
+  const response = await fetchBookmarks(accessToken);
+  const jsonResponse = await response.json();
   // TODO: make this a util
-  setAccessToken(req)
-  return res.json({ success: true, data: jsonResponse })
-}
+  setAccessToken(req, res, jsonResponse);
+  return res.json({ success: true, data: jsonResponse });
+};
 
 const fetchAddbookmark = (accessToken, chapterNumber, verseNumber) => {
-  console.log(accessToken, chapterNumber, verseNumber)
+  console.log(accessToken, chapterNumber, verseNumber);
   return fetch(getAuthApiPath('bookmarks'), {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `bearer ${accessToken}`
+      Authorization: `bearer ${accessToken}`,
     },
-    body: JSON.stringify({ chapterNumber, verseNumber })
-  })
-}
+    body: JSON.stringify({ chapterNumber, verseNumber }),
+  });
+};
 
 const addBookmark = async (req, res) => {
-  const accessToken = getAccessToken(req)
-  const { chapterNumber, verseNumber } = JSON.parse(req.body)
-  const jsonResponse = await fetchAddbookmark(
-    accessToken,
-    chapterNumber,
-    verseNumber
-  )
-  setAccessToken(req, res, jsonResponse)
-  return res.json({ success: true })
-}
+  const accessToken = getAccessToken(req);
+  const { chapterNumber, verseNumber } = JSON.parse(req.body);
+  const jsonResponse = await fetchAddbookmark(accessToken, chapterNumber, verseNumber);
+  setAccessToken(req, res, jsonResponse);
+  return res.json({ success: true });
+};
 
 const handler = async (req: NextApiRequestWithTokens, res: NextApiResponse) => {
-  if (req.method === 'GET') return getBookmarksByUserId(req, res)
-  if (req.method === 'POST') return addBookmark(req, res)
-}
+  if (req.method === 'GET') return getBookmarksByUserId(req, res);
+  if (req.method === 'POST') return addBookmark(req, res);
+};
 
-export default withRefreshToken(withProtect(handler))
+export default withRefreshToken(withProtect(handler));
