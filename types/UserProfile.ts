a347b8ded7cc7b64e8f28482e77ae47505543af6
Muhammad Muhajir <muhajirframe@gmail.com>
2022-06-10 15:53:52 +0700

    update typing

--- types/UserProfile.ts
+++ types/UserProfile.ts
@@ -1,11 +1,11 @@
-import { ProfileRequiredFields } from './CompleteSignupRequest';
+import FormField from './FormField';
 
 interface UserProfile {
   firstName: string;
   lastName: string;
   email: string;
   photoUrl?: string;
-  requiredFields: ProfileRequiredFields[];
+  requiredFields: FormField[];
 }
 
 export default UserProfile;
