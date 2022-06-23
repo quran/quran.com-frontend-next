fee2561fc16a203552808bc89b2378a3cb4d0874
Muhammad Muhajir <muhajirframe@gmail.com>
2022-06-23 13:38:39 +0700

    remove logout api (#1702)

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,22 +1,6 @@
-/* eslint-disable import/prefer-default-export */
-import { NextApiResponse } from 'next';
-
 const LOCALE_COOKIE_PERSISTENCE_PERIOD_MS = 86400000000000; // maximum milliseconds-since-the-epoch value https://stackoverflow.com/a/56980560/1931451
 
-/**
- * Set the cookies of the response.
- *
- * @param {NextApiResponse} response
- * @param {number | string | ReadonlyArray<string>} cookies
- * @returns {NextApiResponse}
- */
-export const setResponseCookie = (
-  response: NextApiResponse,
-  cookies: number | string | ReadonlyArray<string>,
-): NextApiResponse => {
-  return response.setHeader('Set-Cookie', cookies);
-};
-
+// eslint-disable-next-line import/prefer-default-export
 export const setLocaleCookie = (newLocale: string) => {
   const date = new Date();
   date.setTime(LOCALE_COOKIE_PERSISTENCE_PERIOD_MS);
