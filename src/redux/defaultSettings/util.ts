75cab72f8fccf4783da8790b0a2ad3f2353f9e50
Ashraf Ali <ashrafali0148@gmail.com>
2022-07-24 19:51:45 +0100

    Import local at build time

--- src/redux/defaultSettings/util.ts
+++ src/redux/defaultSettings/util.ts
@@ -4,14 +4,31 @@
 
 import { RootState } from '../RootState';
 
-import { DefaultSettings } from 'src/redux/defaultSettings/defaultSettings';
-import AudioState from 'src/redux/types/AudioState';
-import QuranReaderStyles from 'src/redux/types/QuranReaderStyles';
-import ReadingPreferences from 'src/redux/types/ReadingPreferences';
-import SliceName from 'src/redux/types/SliceName';
-import TafsirsSettings from 'src/redux/types/TafsirsSettings';
-import Theme from 'src/redux/types/Theme';
-import TranslationsSettings from 'src/redux/types/TranslationsSettings';
+import EnLocal from 'src/redux/defaultSettings/locales/en';
+import ArLocal from 'src/redux/defaultSettings/locales/ar';
+import BnLocal from 'src/redux/defaultSettings/locales/bn';
+import FaLocal from 'src/redux/defaultSettings/locales/fa';
+import FrLocal from 'src/redux/defaultSettings/locales/fr';
+import IdLocal from 'src/redux/defaultSettings/locales/id';
+import ItLocal from 'src/redux/defaultSettings/locales/it';
+import MsLocal from 'src/redux/defaultSettings/locales/ms';
+import NlLocal from 'src/redux/defaultSettings/locales/nl';
+import PtLocal from 'src/redux/defaultSettings/locales/pt';
+import RuLocal from 'src/redux/defaultSettings/locales/ru';
+import SqLocal from 'src/redux/defaultSettings/locales/sq';
+import ThLocal from 'src/redux/defaultSettings/locales/th';
+import TrLocal from 'src/redux/defaultSettings/locales/tr';
+import UrLocal from 'src/redux/defaultSettings/locales/ur';
+import ZhLocal from 'src/redux/defaultSettings/locales/zh';
+
+import { DefaultSettings } from "src/redux/defaultSettings/defaultSettings";
+import AudioState from "src/redux/types/AudioState";
+import QuranReaderStyles from "src/redux/types/QuranReaderStyles";
+import ReadingPreferences from "src/redux/types/ReadingPreferences";
+import SliceName from "src/redux/types/SliceName";
+import TafsirsSettings from "src/redux/types/TafsirsSettings";
+import Theme from "src/redux/types/Theme";
+import TranslationsSettings from "src/redux/types/TranslationsSettings";
 
 /**
  * Get the initial state of the store.
@@ -40,8 +57,26 @@ const DEFAULT_LOCALE = 'en';
  * @param {string} locale
  * @returns {DefaultSettings}
  */
-const importLocaleFile = (locale: string): DefaultSettings =>
-  require(`src/redux/defaultSettings/locales/${locale}`).default;
+const importLocaleFile = (locale: string): DefaultSettings => {
+  switch (locale) {
+    case 'ar': return ArLocal;
+    case 'bn': return BnLocal;
+    case 'fa': return FaLocal;
+    case 'fr': return FrLocal;
+    case 'id': return IdLocal;
+    case 'it': return ItLocal;
+    case 'ms': return MsLocal;
+    case 'nl': return NlLocal;
+    case 'pt': return PtLocal;
+    case 'ru': return RuLocal;
+    case 'sq': return SqLocal;
+    case 'th': return ThLocal;
+    case 'tr': return TrLocal;
+    case 'ur': return UrLocal;
+    case 'zh': return ZhLocal;
+    default: return EnLocal;
+  }
+}
 
 /**
  * Get specific settings by its key for a locale.
