a6f580f2aa2402246b5b665708bbb9899fa0662f
Muhammad Muhajir <muhajirframe@gmail.com>
2022-08-08 14:54:44 +0700

    reciter selection

--- src/components/AudioPlayer/Buttons/SelectReciterMenu.tsx
+++ src/components/AudioPlayer/Buttons/SelectReciterMenu.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useContext } from 'react';
 
+import { useSelector } from '@xstate/react';
 import useTranslation from 'next-translate/useTranslation';
-import { useSelector } from 'react-redux';
 
 import CheckIcon from '../../../../public/icons/check.svg';
 import ChevronLeftIcon from '../../../../public/icons/chevron-left.svg';
@@ -11,44 +11,45 @@ import styles from './SelectReciterMenu.module.scss';
 import DataFetcher from 'src/components/DataFetcher';
 import PopoverMenu from 'src/components/dls/PopoverMenu/PopoverMenu';
 import Spinner from 'src/components/dls/Spinner/Spinner';
-import usePersistPreferenceGroup from 'src/hooks/auth/usePersistPreferenceGroup';
-import useGetQueryParamOrReduxValue from 'src/hooks/useGetQueryParamOrReduxValue';
-import {
-  selectAudioPlayerState,
-  setReciterAndPauseAudio,
-} from 'src/redux/slices/AudioPlayer/state';
 import { makeAvailableRecitersUrl } from 'src/utils/apiPaths';
 import { logButtonClick, logItemSelectionChange, logValueChange } from 'src/utils/eventLogger';
+import { AudioPlayerMachineContext } from 'src/xstate/AudioPlayerMachineContext';
 import { RecitersResponse } from 'types/ApiResponses';
-import PreferenceGroup from 'types/auth/PreferenceGroup';
-import QueryParam from 'types/QueryParam';
 import Reciter from 'types/Reciter';
 
 const DEFAULT_RECITATION_STYLE = 'Murattal';
 
 const SelectReciterMenu = ({ onBack }) => {
   const { lang, t } = useTranslation('common');
-  const { value: selectedReciterId }: { value: number } = useGetQueryParamOrReduxValue(
-    QueryParam.Reciter,
-  );
-  const audioPlayerState = useSelector(selectAudioPlayerState);
-  const {
-    actions: { onSettingsChange },
-    isLoading,
-  } = usePersistPreferenceGroup();
+  // const { value: selectedReciterId }: { value: number } = useGetQueryParamOrReduxValue(
+  //   QueryParam.Reciter,
+  // );
+
+  const audioService = useContext(AudioPlayerMachineContext);
+  const selectedReciterId = useSelector(audioService, (state) => state.context.reciterId);
+
+  // const audioPlayerState = useSelector(selectAudioPlayerState);
+  // const {
+  //   actions: { onSettingsChange },
+  //   isLoading,
+  // } = usePersistPreferenceGroup();
+
+  // TODO: fix db persistence issue
+  const isLoading = false;
 
   const onReciterSelected = useCallback(
     (reciter: Reciter) => {
-      onSettingsChange(
-        'reciter',
-        reciter.id,
-        setReciterAndPauseAudio({ reciter, locale: lang }),
-        setReciterAndPauseAudio({ reciter: audioPlayerState.reciter, locale: lang }),
-        PreferenceGroup.AUDIO,
-        onBack,
-      );
+      audioService.send({ type: 'CHANGE_RECITER', reciterId: reciter.id });
+      // onSettingsChange(
+      //   'reciter',
+      //   reciter.id,
+      //   setReciterAndPauseAudio({ reciter, locale: lang }),
+      //   setReciterAndPauseAudio({ reciter: audioPlayerState.reciter, locale: lang }),
+      //   PreferenceGroup.AUDIO,
+      //   onBack,
+      // );
     },
-    [audioPlayerState, lang, onBack, onSettingsChange],
+    [audioService],
   );
 
   const getItemIcon = useCallback(
