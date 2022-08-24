64c3a9620f4a575a1729fa6929498ff49f03be4a
Muhammad Muhajir <muhajirframe@gmail.com>
2022-08-24 14:29:42 +0700

    update recentReadingSession format to include date

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -42,7 +42,7 @@ import SliceName from './types/SliceName';
 
 const persistConfig = {
   key: 'root',
-  version: 17,
+  version: 22,
   storage,
   migrate: createMigrate(migrations, {
     debug: process.env.NEXT_PUBLIC_VERCEL_ENV === 'development',
