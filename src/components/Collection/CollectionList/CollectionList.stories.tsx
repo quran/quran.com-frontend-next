6f558df5f574d4346fe897acf4e54d351f82efb6
Muhammad Muhajir <muhajirframe@gmail.com>
2022-08-30 11:06:14 +0700

    .

--- src/components/Collection/CollectionList/CollectionList.stories.tsx
+++ src/components/Collection/CollectionList/CollectionList.stories.tsx
@@ -10,18 +10,22 @@ export const Example = () => {
     <CollectionList
       collections={[
         {
+          id: 1,
           name: 'All Saved Verses',
           itemCount: 56,
         },
         {
+          id: 2,
           name: 'Friday Recitations',
           itemCount: 56,
         },
         {
+          id: 3,
           name: 'Woman in Quran',
           itemCount: 56,
         },
         {
+          id: 4,
           name: 'Motivations',
           itemCount: 56,
         },
