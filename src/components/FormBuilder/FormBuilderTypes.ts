7516cf7ae9d0245336d4ba71bd7047c285f671af
Muhammad Muhajir <muhajirframe@gmail.com>
2022-08-30 11:20:18 +0700

    UI: RenameCollectionModal (#1765)
    * wip: RenameCollectionModal
    
    * update: add default value to form builder
    
    * .
    
    * .

--- src/components/FormBuilder/FormBuilderTypes.ts
+++ src/components/FormBuilder/FormBuilderTypes.ts
@@ -3,6 +3,7 @@ import FormField from 'types/FormField';
 
 export type FormBuilderFieldRule = Pick<FieldRule, 'type' | 'value'> & { errorMessage: string };
 export type FormBuilderFormField = Pick<FormField, 'field' | 'type'> & {
+  defaultValue?: any;
   label?: string;
   rules?: FormBuilderFieldRule[];
 };
