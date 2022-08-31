9727d01eddb4d190097caadb1a40c846c926a5d7
Ahmed Riad <ahmed.riad.elashrey@gmail.com>
2022-09-01 02:11:54 +0300

    add vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    include: ['**/src/**/*.test.(js|ts|jsx|tsx)'],
+  },
+});
