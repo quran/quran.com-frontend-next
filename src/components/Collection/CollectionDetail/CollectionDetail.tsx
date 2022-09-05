6a0d3162c76cba45462f9d84d821b9b9ccf8bf23
Muhammad Muhajir <muhajirframe@gmail.com>
2022-09-05 13:46:48 +0700

    add verse detail

--- src/components/Collection/CollectionDetail/CollectionDetail.tsx
+++ src/components/Collection/CollectionDetail/CollectionDetail.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 
 import useTranslation from 'next-translate/useTranslation';
+import { useSelector, shallowEqual } from 'react-redux';
 
 import ChevronDownIcon from '../../../../public/icons/chevron-down.svg';
 import OverflowMenuIcon from '../../../../public/icons/menu_more_horiz.svg';
@@ -8,8 +9,16 @@ import CollectionSorter from '../CollectionSorter/CollectionSorter';
 
 import styles from './CollectionDetail.module.scss';
 
+import DataFetcher from 'src/components/DataFetcher';
 import Collapsible from 'src/components/dls/Collapsible/Collapsible';
+import TafsirVerseText from 'src/components/QuranReader/TafsirView/TafsirVerseText';
+import TranslationText from 'src/components/QuranReader/TranslationView/TranslationText';
 import DataContext from 'src/contexts/DataContext';
+import { selectQuranReaderStyles } from 'src/redux/slices/QuranReader/styles';
+import { selectSelectedTranslations } from 'src/redux/slices/QuranReader/translations';
+import { getDefaultWordFields, getMushafId } from 'src/utils/api';
+import { makeVersesUrl } from 'src/utils/apiPaths';
+import { areArraysEqual } from 'src/utils/array';
 import { getChapterData } from 'src/utils/chapter';
 import { toLocalizedVerseKey } from 'src/utils/locale';
 import { makeVerseKey } from 'src/utils/verse';
@@ -37,6 +46,10 @@ const CollectionDetail = ({
   onSortByChange,
 }: CollectionDetailProps) => {
   const { t, lang } = useTranslation();
+  const quranReaderStyles = useSelector(selectQuranReaderStyles, shallowEqual);
+  const { quranFont, mushafLines } = quranReaderStyles;
+  const { mushaf } = getMushafId(quranFont, mushafLines);
+  const selectedTranslations = useSelector(selectSelectedTranslations, areArraysEqual);
 
   const sortOptions = [
     {
@@ -77,7 +90,46 @@ const CollectionDetail = ({
             >
               {({ isOpen }) => {
                 if (!isOpen) return null;
-                return <div className={styles.item}>aaaaaaa</div>;
+                const chapterId = item.key;
+                const params = {
+                  words: true,
+                  // translation_fields: resource_name,language_id
+                  perPage: 1,
+                  // fields: text_uthmani,chapter_id,hizb_number,text_imlaei_simple
+                  translations: selectedTranslations.join(','),
+                  // reciter: 7
+                  // word_translation_language: en
+                  page: item.verseNumber,
+                  ...getDefaultWordFields(quranReaderStyles.quranFont),
+                  mushaf,
+                };
+
+                return (
+                  <DataFetcher
+                    queryKey={makeVersesUrl(chapterId.toString(), lang, params)}
+                    render={(data) => {
+                      const firstVerse = data.verses?.[0];
+                      return (
+                        <div>
+                          <TafsirVerseText verses={data.verses} />
+                          <div>
+                            {firstVerse.translations.map((translation) => {
+                              return (
+                                <TranslationText
+                                  key={translation.id}
+                                  translationFontScale={quranReaderStyles.translationFontScale}
+                                  text={translation.text}
+                                  languageId={translation.languageId}
+                                  resourceName={translation.resourceName}
+                                />
+                              );
+                            })}
+                          </div>
+                        </div>
+                      );
+                    }}
+                  />
+                );
               }}
             </Collapsible>
           );
