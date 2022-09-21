f7ca25a2dbbca279ecd10eaf48c0f235a5845eff
Muhammad Muhajir <muhajirframe@gmail.com>
2022-09-21 15:54:43 +0700

    fix mobile safari issue

--- src/components/AudioPlayer/AudioPlayer.tsx
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -9,6 +9,7 @@ import dynamic from 'next/dynamic';
 import styles from './AudioPlayer.module.scss';
 
 import Spinner from '@/dls/Spinner/Spinner';
+import { milliSecondsToSeconds } from '@/utils/datetime';
 import { logEvent } from '@/utils/eventLogger';
 import { AudioPlayerMachineContext } from 'src/xstate/AudioPlayerMachineContext';
 
@@ -68,18 +69,24 @@ const AudioPlayer = () => {
     const audioPlayer: HTMLAudioElement = e.target;
     const currentTimestamp = audioPlayer.currentTime;
     const downloadProgress = getAudioPlayerDownloadProgress(audioPlayer);
-    const isWaiting = currentTimestamp > downloadProgress - AUDIO_DURATION_TOLERANCE; // 2s tolerance
-    const isAlmostEnded = currentTimestamp > audioPlayer.duration - AUDIO_DURATION_TOLERANCE; // 2s tolerance
+    const isWaiting = currentTimestamp > downloadProgress - AUDIO_DURATION_TOLERANCE;
 
-    /**
-     * simulate onWaiting event on safari.
-     * If the audio is not in loading state already. And `currentTime` is nearby last timestamp of `buffered`
-     * trigger WAITING event.
-     */
-    if (!isLoading && isWaiting && !isAlmostEnded) {
-      audioService.send({ type: 'WAITING' });
-    } else if (isLoading && !isWaiting) {
-      audioService.send({ type: 'CAN_PLAY' });
+    const audioDataDuration = audioService.getSnapshot().context?.audioData?.duration;
+    if (audioDataDuration) {
+      const isAlmostEnded =
+        currentTimestamp > milliSecondsToSeconds(audioDataDuration) - AUDIO_DURATION_TOLERANCE;
+
+      /**
+       * simulate onWaiting event on safari.
+       * If the audio is not in loading state already. And `currentTime` is nearby last timestamp of `buffered`
+       * trigger WAITING event.
+       */
+
+      if (!isLoading && isWaiting && !isAlmostEnded) {
+        audioService.send({ type: 'WAITING' });
+      } else if (isLoading && !isWaiting) {
+        audioService.send({ type: 'CAN_PLAY' });
+      }
     }
 
     audioService.send({ type: 'UPDATE_TIMING' });
