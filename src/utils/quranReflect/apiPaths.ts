08eb24579740da14ab8cab7f9ca1fa31eab543b5
osamasayed <osamasayed@users.noreply.github.com>
2022-12-20 21:57:23 +0700

    Add QR staging url

--- src/utils/quranReflect/apiPaths.ts
+++ src/utils/quranReflect/apiPaths.ts
@@ -6,12 +6,19 @@ import AyahReflectionsRequestParams from 'types/QuranReflect/AyahReflectionsRequ
 import AyahReflectionsResponse from 'types/QuranReflect/AyahReflectionsResponse';
 import Tab from 'types/QuranReflect/Tab';
 
+const STAGING_API_HOST = 'https://quranreflect.org';
+const PRODUCTION_API_HOST = 'https://quranreflect.com';
+
+// env variables in Vercel can't be dynamic, we have to hardcode the urls here. https://stackoverflow.com/questions/44342226/next-js-error-only-absolute-urls-are-supported
+export const API_HOST =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? PRODUCTION_API_HOST : STAGING_API_HOST;
+
 export const makeQuranReflectApiUrl = (path: string, parameters = {}): string => {
   const params = {
     client_auth_token: process.env.NEXT_PUBLIC_QURAN_REFLECT_TOKEN,
     ...parameters,
   };
-  return `https://quranreflect.com/${path}?${stringify(params)}`;
+  return `${API_HOST}/${path}?${stringify(params)}`;
 };
 
 /* eslint-disable import/prefer-default-export */
