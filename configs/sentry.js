29de341036b6ac297fc72fa1edb04e8b84c466cf
Ahmed Riad <ahmed.riad.elashrey@gmail.com>
2023-01-13 17:44:07 +0300

    move plugin configs to separate files

---
+++ configs/sentry.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
