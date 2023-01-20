85038d9856ee0a5af086dad5007850c7008b62a6
Ahmed Riad <ahmed.riad.elashrey@gmail.com>
2023-01-20 14:51:31 +0300

    Revert "Merge pull request #1874 from quran/next-13"
    This reverts commit 59f5c164025b62651bd022b6677a39a37974ae34, reversing
    changes made to f6f67f7d050ec3edcac897f371c63a2e62b0889d.

--- src/components/DisableAnimationsUntilHydration.tsx
+++ src/components/DisableAnimationsUntilHydration.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import { selectIsPersistGateHydrationComplete } from '@/redux/slices/persistGateHydration';
+import isClient from '@/utils/isClient';
 
 // This utility disables all animations until the redux-persist store hydration is complete.
 // This is done to prevent a bunch of animations from happening on initial page load.
 const DisableAnimationsUntilHydration = () => {
   const isPersistGateHydrationComplete = useSelector(selectIsPersistGateHydrationComplete);
 
-  if (!isPersistGateHydrationComplete) {
+  if (isClient && !isPersistGateHydrationComplete) {
     return (
       <style>
         {`
