41422841206ee7dc4edc726d62de750e3f6d05e4
osamasayed <osamasayed@users.noreply.github.com>
2023-01-20 14:59:36 +0700

    Add repeat counter to repeat settings icon

--- src/components/AudioPlayer/Buttons/RepeatButton.tsx
+++ src/components/AudioPlayer/Buttons/RepeatButton.tsx
@@ -3,8 +3,12 @@ import { useContext, useState } from 'react';
 import { useSelector } from '@xstate/react';
 import useTranslation from 'next-translate/useTranslation';
 
+import RemainingRangeCount from '../RemainingRangeCount';
+
 import RepeatAudioModal from '@/components/AudioPlayer/RepeatAudioModal/RepeatAudioModal';
 import { RepetitionMode } from '@/components/AudioPlayer/RepeatAudioModal/SelectRepetitionMode';
+import Wrapper from '@/components/Wrapper/Wrapper';
+import Badge from '@/dls/Badge/Badge';
 import PopoverMenu from '@/dls/PopoverMenu/PopoverMenu';
 import Spinner, { SpinnerSize } from '@/dls/Spinner/Spinner';
 import RepeatIcon from '@/icons/repeat.svg';
@@ -18,6 +22,8 @@ const RepeatAudioButton = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const currentSurah = useSelector(audioService, (state) => state.context.surah);
   const isLoading = useSelector(audioService, selectIsLoading);
+  const repeatActor = useSelector(audioService, (state) => state.context.repeatActor);
+  const isInRepeatMode = !!repeatActor;
 
   const onButtonClicked = () => {
     logButtonClick('audio_player_repeat');
@@ -29,7 +35,30 @@ const RepeatAudioButton = () => {
       <PopoverMenu.Item
         onClick={onButtonClicked}
         isDisabled={isLoading}
-        icon={isLoading ? <Spinner size={SpinnerSize.Large} /> : <RepeatIcon />}
+        icon={
+          isLoading ? (
+            <Spinner size={SpinnerSize.Large} />
+          ) : (
+            <Wrapper
+              shouldWrap={isInRepeatMode}
+              wrapper={(children) => (
+                <Badge
+                  content={
+                    isInRepeatMode && (
+                      <RemainingRangeCount
+                        rangeActor={repeatActor.getSnapshot().context.rangeCycleActor}
+                      />
+                    )
+                  }
+                >
+                  {children}
+                </Badge>
+              )}
+            >
+              <RepeatIcon />
+            </Wrapper>
+          )
+        }
       >
         {t('audio.player.repeat-settings')}
       </PopoverMenu.Item>
