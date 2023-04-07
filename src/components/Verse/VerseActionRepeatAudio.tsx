e2c59b0e58558604635734d574c2c9489ea7c572
osamasayed <osamasayed@users.noreply.github.com>
2023-04-08 00:52:05 +0200

    Update logging

--- src/components/Verse/VerseActionRepeatAudio.tsx
+++ src/components/Verse/VerseActionRepeatAudio.tsx
@@ -7,15 +7,26 @@ import PopoverMenu from '../dls/PopoverMenu/PopoverMenu';
 
 import RepeatAudioModal from '@/components/AudioPlayer/RepeatAudioModal/RepeatAudioModal';
 import RepeatIcon from '@/icons/repeat.svg';
+import { logButtonClick } from '@/utils/eventLogger';
 import { getChapterNumberFromKey } from '@/utils/verse';
 
 type VerseActionRepeatAudioProps = {
   verseKey: string;
+  isTranslationView: boolean;
 };
-const VerseActionRepeatAudio = ({ verseKey }: VerseActionRepeatAudioProps) => {
+const VerseActionRepeatAudio = ({ verseKey, isTranslationView }: VerseActionRepeatAudioProps) => {
   const { t } = useTranslation('common');
   const [isModalOpen, setIsModalOpen] = useState(false);
   const chapterId = getChapterNumberFromKey(verseKey);
+
+  const onItemClicked = () => {
+    if (isTranslationView) {
+      logButtonClick('translation_view_verse_actions_menu_repeat');
+    } else {
+      logButtonClick('reading_view_verse_actions_menu_repeat');
+    }
+    setIsModalOpen(true);
+  };
 
   return (
     <>
@@ -26,12 +37,7 @@ const VerseActionRepeatAudio = ({ verseKey }: VerseActionRepeatAudioProps) => {
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
       />
-      <PopoverMenu.Item
-        icon={<RepeatIcon />}
-        onClick={() => {
-          setIsModalOpen(true);
-        }}
-      >
+      <PopoverMenu.Item icon={<RepeatIcon />} onClick={onItemClicked}>
         {t('audio.player.repeat-1-verse')}
       </PopoverMenu.Item>
     </>
