e81d92b5a0629f02d6db30bd2b45e6e3ded778fd
Tajmirul <tasmirolislam@gmail.com>
2023-05-01 15:43:21 +0000

    fixing: banner style in dark mood

--- src/components/HomePage/HomePageHero.tsx
+++ src/components/HomePage/HomePageHero.tsx
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
+import { useSelector } from 'react-redux';
 
 import AlQuranulKarimSVG from '../../../public/images/alquranul-karim.svg';
 
@@ -7,17 +8,20 @@ import styles from './HomePageHero.module.scss';
 import QuickLinks from './QuickLinks';
 
 import CommandBarTrigger from '@/components/CommandBar/CommandBarTrigger';
+import { selectTheme } from '@/redux/slices/theme';
 
 const PlayRadioButton = dynamic(() => import('./PlayRadioButton'));
 
 const HomePageHero = () => {
+  const { type } = useSelector(selectTheme);
+
   return (
     <div className={styles.outerContainer}>
       <Head>
         <link rel="preload" as="image" href="/images/background.jpg" />
       </Head>
       <div className={styles.backgroundImage} />
-      <div data-theme="light">
+      <div data-theme={type}>
         <PlayRadioButton />
         <div className={styles.innerContainer}>
           <div className={styles.imageContainer}>
