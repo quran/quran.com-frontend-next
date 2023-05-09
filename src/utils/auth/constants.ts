0fb6ab0168e94703c8b161bef8aef083704b3361
Ahmed Riad <ahmed.riad.elashrey@gmail.com>
2023-05-09 20:26:03 +0300

    add prefix to auth cookies

--- src/utils/auth/constants.ts
+++ src/utils/auth/constants.ts
@@ -1,8 +1,9 @@
 // NOTE: IF THOSE VALUES CHANGE, WE SHOULD CHANGE IT IN OUR AUTH REPO
-export const REFRESH_TOKEN_COOKIE_NAME = 'rt';
-export const ACCESS_TOKEN_COOKIE_NAME = 'at';
+export const REFRESH_TOKEN_COOKIE_NAME = 'rt_staging2';
+export const ACCESS_TOKEN_COOKIE_NAME = 'at_staging2';
+export const USER_ID = 'id_staging2';
+
 export const ACCESS_TOKEN_EXPIRATION_COOKIE_NAME = 'atexp';
-export const USER_ID = 'id';
 export const DEFAULT_PHOTO_URL = `https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y`;
 
 export const AUTH_ONBOARDING_ANNOUNCEMENT_TYPE = 'auth-onboarding';
