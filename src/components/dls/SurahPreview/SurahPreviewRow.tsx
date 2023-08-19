1bf910ce87071149d61463f8fd7ff716e3c07dd1
nzil <anzil@aufaitux.com>
2023-08-19 22:52:24 +0530

    SurahPreview - transiotion added. Right content alignment changed to flex end.

--- src/components/dls/SurahPreview/SurahPreviewRow.tsx
+++ src/components/dls/SurahPreview/SurahPreviewRow.tsx
@@ -5,77 +5,68 @@ import useTranslation from 'next-translate/useTranslation';
 
 import styles from './SurahPreviewRow.module.scss';
 
-import ChapterIconContainer, {
-  ChapterIconsSize,
-} from '@/components/chapters/ChapterIcon/ChapterIconContainer';
+import ChapterIconContainer, { ChapterIconsSize } from '@/components/chapters/ChapterIcon/ChapterIconContainer';
 import Spinner from '@/dls/Spinner/Spinner';
 import { toLocalizedNumber } from '@/utils/locale';
 
 type SurahPreviewRowProps = {
-  surahNumber: number;
-  surahName: string;
-  translatedSurahName: string;
-  description: string;
-  chapterId: number;
-  isMinimalLayout?: boolean;
-  isLoading?: boolean;
+	surahNumber: number;
+	surahName: string;
+	translatedSurahName: string;
+	description: string;
+	chapterId: number;
+	isMinimalLayout?: boolean;
+	isLoading?: boolean;
 };
 const SurahPreviewRow = ({
-  surahName,
-  surahNumber,
-  translatedSurahName,
-  description,
-  chapterId,
-  isMinimalLayout = false,
-  isLoading = false,
+	surahName,
+	surahNumber,
+	translatedSurahName,
+	description,
+	chapterId,
+	isMinimalLayout = false,
+	isLoading = false,
 }: SurahPreviewRowProps) => {
-  const { lang } = useTranslation('home');
-  const localizedSurahNumber = useMemo(
-    () => toLocalizedNumber(surahNumber, lang),
-    [surahNumber, lang],
-  );
+	const { lang } = useTranslation('home');
+	const localizedSurahNumber = useMemo(() => toLocalizedNumber(surahNumber, lang), [surahNumber, lang]);
 
-  if (isMinimalLayout) {
-    return (
-      <div className={styles.container}>
-        <div className={styles.left}>
-          <div className={styles.surahNumber}>
-            <span>{localizedSurahNumber}</span>
-          </div>
-          <ChapterIconContainer
-            chapterId={chapterId.toString()}
-            hasSurahPrefix={false}
-            size={ChapterIconsSize.Large}
-          />
-        </div>
-        <div className={styles.right}>
-          {description && (
-            <div className={classNames(styles.description, styles.largeText)}>{description}</div>
-          )}
-        </div>
-      </div>
-    );
-  }
-  return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <div className={styles.surahNumber}>
-          <span>{localizedSurahNumber}</span>
-        </div>
-        <div className={styles.surahNameContainer}>
-          <div className={styles.surahName}>{surahName}</div>
-          <div className={styles.translatedSurahName}>{translatedSurahName}</div>
-        </div>
-      </div>
-      {isLoading && <Spinner />}
-      <div className={styles.right}>
-        <div>
-          <ChapterIconContainer chapterId={chapterId.toString()} hasSurahPrefix={false} />
-        </div>
-        {description && <div className={styles.description}>{description}</div>}
-      </div>
-    </div>
-  );
+	if (isMinimalLayout) {
+		return (
+			<div className={styles.container}>
+				<div className={styles.left}>
+					<div className={styles.surahNumber}>
+						<span>{localizedSurahNumber}</span>
+					</div>
+					<ChapterIconContainer chapterId={chapterId.toString()} hasSurahPrefix={false} size={ChapterIconsSize.Large} />
+				</div>
+				<div className={styles.right}>
+					{description && (
+						<div className={classNames(styles.surahInfoText, styles.description, styles.largeText)}>{description}</div>
+					)}
+				</div>
+			</div>
+		);
+	}
+	return (
+		<div className={styles.container}>
+			<div className={styles.left}>
+				<div className={styles.surahNumber}>
+					<span>{localizedSurahNumber}</span>
+				</div>
+				<div className={styles.surahNameContainer}>
+					<div className={styles.surahName}>{surahName}</div>
+					<div className={classNames(styles.surahInfoText, styles.translatedSurahName)}>{translatedSurahName}</div>
+				</div>
+			</div>
+			{isLoading && <Spinner />}
+			<div className={styles.right}>
+				<div>
+					<ChapterIconContainer chapterId={chapterId.toString()} hasSurahPrefix={false} />
+				</div>
+				{description && <div className={classNames(styles.surahInfoText, styles.description)}>{description}</div>}
+			</div>
+		</div>
+	);
 };
 
 export default SurahPreviewRow;
