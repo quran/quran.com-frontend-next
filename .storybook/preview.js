dc303bc9c050b33611655ea02883d8ceefa74ed1
Matt <59867217+JealousGx@users.noreply.github.com>
2023-09-22 15:17:08 +0500

    fix: upgrades storybook to v7.4.3

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,35 +1,55 @@
-import ResetCSS from '../src/styles/reset.scss';
-import { TooltipProvider } from '@radix-ui/react-tooltip';
-import Theme from '../src/styles/theme.scss'
-import GlobalFonts from '../src/styles/fonts.scss';
-import GlobalStyles from '../src/styles/global.scss';
-import Styles from './styles.scss';
-import I18nProvider from 'next-translate/I18nProvider';
-import { themes } from '@storybook/theming';
-import { RouterDecorator } from 'storybook-addon-next/dist/routing/decorator';
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+import { themes } from "@storybook/theming";
+import I18nProvider from "next-translate/I18nProvider";
+import GlobalFonts from "../src/styles/fonts.scss";
+import GlobalStyles from "../src/styles/global.scss";
+import ResetCSS from "../src/styles/reset.scss";
+import Theme from "../src/styles/theme.scss";
+import Styles from "./styles.scss";
+// import { RouterDecorator } from "storybook-addon-next/dist/routing/decorator";
 
-import commonEn from '../locales/en/common.json';
-import homeEn from '../locales/en/home.json';
+import commonEn from "../locales/en/common.json";
+import homeEn from "../locales/en/home.json";
 
 const themeDecorator = (Story, context) => {
   const theme = context.globals.theme;
   return (
     <>
-      <link rel="stylesheet" href={GlobalFonts} />
-      <link rel="stylesheet" href={ResetCSS} />
-      <link rel="stylesheet" href={Theme} />
-      <link rel="stylesheet" href={GlobalStyles} />
-      <link rel="stylesheet" href={Styles} />
+      <link
+        rel="stylesheet"
+        href={GlobalFonts}
+      />
+      <link
+        rel="stylesheet"
+        href={ResetCSS}
+      />
+      <link
+        rel="stylesheet"
+        href={Theme}
+      />
+      <link
+        rel="stylesheet"
+        href={GlobalStyles}
+      />
+      <link
+        rel="stylesheet"
+        href={Styles}
+      />
       <div data-theme={theme}>
-        <div style={{
-           backgroundColor: 'var(--color-background-default)',
-           width: '100vw',
-           height: '100vh',
-           padding: '1rem',
-           boxSizing: 'border-box'
-          }}>
+        <div
+          style={{
+            backgroundColor: "var(--color-background-default)",
+            width: "100vw",
+            height: "100vh",
+            padding: "1rem",
+            boxSizing: "border-box",
+          }}
+        >
           <TooltipProvider>
-            <I18nProvider lang='en' namespaces={{ common: commonEn, home: homeEn }}>
+            <I18nProvider
+              lang="en"
+              namespaces={{ common: commonEn, home: homeEn }}
+            >
               <Story />
             </I18nProvider>
           </TooltipProvider>
@@ -39,66 +59,64 @@ const themeDecorator = (Story, context) => {
   );
 };
 
-
-export const decorators = [themeDecorator, RouterDecorator];
+export const decorators = [themeDecorator];
 export const globalTypes = {
   theme: {
-    name: 'Theme',
-    description: 'Global theme for components',
-    defaultValue: 'light',
+    name: "Theme",
+    description: "Global theme for components",
+    defaultValue: "light",
     toolbar: {
-      icon: 'circlehollow',
-      items: ['light', 'dark', 'sepia'],
+      icon: "circlehollow",
+      items: ["light", "dark", "sepia"],
       showName: true,
     },
   },
 };
 
-
 const viewports = {
   mobileS: {
-    name: 'MobileS',
+    name: "MobileS",
     styles: {
       width: `320px`,
-      height: '100%',
+      height: "100%",
     },
   },
   mobileM: {
-    name: 'MobileM',
+    name: "MobileM",
     styles: {
       width: `375px`,
-      height: '100%',
+      height: "100%",
     },
   },
   mobileL: {
-    name: 'MobileL',
+    name: "MobileL",
     styles: {
       width: `425px`,
-      height: '100%',
+      height: "100%",
     },
   },
   tablet: {
-    name: 'Tablet',
+    name: "Tablet",
     styles: {
       width: `768px`,
-      height: '100%',
+      height: "100%",
     },
   },
   desktop: {
-    name: 'Desktop',
+    name: "Desktop",
     styles: {
       width: `1024px`,
-      height: '100%',
+      height: "100%",
     },
   },
 };
 
 export const parameters = {
-  layout: 'fullscreen',
+  layout: "fullscreen",
   viewport: {
     viewports,
   },
   docs: {
     theme: themes.dark,
   },
-}
+};
