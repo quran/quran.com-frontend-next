8704e3b77fcbef601ce15ed3637d8bbc90743930
Osama Sayed <toto777_90@hotmail.com>
2023-11-06 06:03:16 +0800

    Upgrade Sentry version and disable transactions

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -14,6 +14,6 @@ Sentry.init({
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
-  tracesSampleRate: isDev ? 1 : 0.2,
+  tracesSampleRate: isDev ? 1 : 0,
   debug: isDev,
 });
