dbc7a5a258f45e39bd521b99381805b26fa0d7cc
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2023-11-24 02:03:54 +0200

    add infinite scrolling & sync cache in notes page when updating/deleting a note

---
+++ src/components/Notes/NotesPage/NotesList/index.tsx
@@ -0,0 +1,112 @@
+import { useMemo, useState } from 'react';
+
+import { Virtuoso } from 'react-virtuoso';
+
+import NoteModal from '../../NoteModal';
+
+import styles from './NotesList.module.scss';
+
+import Spinner, { SpinnerSize } from '@/dls/Spinner/Spinner';
+import { GetAllNotesResponse } from '@/types/auth/NotesByTypeAndTypeIdResponse';
+
+interface NotesListProps {
+  data: GetAllNotesResponse[];
+  isValidating: boolean;
+  size: number;
+  setSize: (size: number) => void;
+  mutateCache: (data: any) => void;
+}
+
+const NotesList = ({ data, isValidating, size, setSize, mutateCache }: NotesListProps) => {
+  const [selectedNoteId, setSelectedNoteId] = useState<string | null>(null); // for the note modal
+
+  const lastPageData = data[data.length - 1];
+  const { hasNextPage } = lastPageData.pagination;
+  const notes = useMemo(() => {
+    return data ? data.map((response) => response.data).flat() : [];
+  }, [data]);
+  const isLoadingMoreData = notes?.length > 0 && size > 1 && isValidating;
+
+  const loadMore = () => {
+    if (!hasNextPage || isValidating) return;
+    setSize(size + 1);
+  };
+
+  const renderNote = (index: number, note: typeof notes[number]) => {
+    return (
+      // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+      <div
+        className={styles.note}
+        key={note.id}
+        role="button"
+        tabIndex={0}
+        onClick={() => setSelectedNoteId(note.id)}
+      >
+        <h3>{note.title}</h3>
+        <p>{note.body}</p>
+      </div>
+    );
+  };
+
+  const onNoteDeleted = () => {
+    // remove the note from the cache
+    mutateCache((cachedPages: GetAllNotesResponse[]) => {
+      const newPages = cachedPages.map((page) => {
+        return {
+          ...page,
+          data: page.data.filter((note) => note.id !== selectedNoteId),
+        };
+      });
+
+      return newPages;
+    });
+  };
+
+  const onNoteUpdated = (updatedNote) => {
+    // update the note in the cache
+    mutateCache((cachedPages: GetAllNotesResponse[]) => {
+      const newPages = cachedPages.map((page) => {
+        return {
+          ...page,
+          data: page.data.map((note) => {
+            if (note.id === selectedNoteId) {
+              return updatedNote;
+            }
+
+            return note;
+          }),
+        };
+      });
+
+      return newPages;
+    });
+  };
+
+  return (
+    <>
+      {selectedNoteId && (
+        <NoteModal
+          isOpen
+          onClose={() => setSelectedNoteId(null)}
+          noteId={selectedNoteId}
+          onNoteDeleted={onNoteDeleted}
+          onNoteUpdated={onNoteUpdated}
+        />
+      )}
+
+      <Virtuoso
+        data={notes}
+        overscan={10}
+        increaseViewportBy={{ top: 10, bottom: 10 }}
+        className={styles.notesListContainer}
+        endReached={loadMore}
+        itemContent={renderNote}
+        useWindowScroll
+      />
+
+      {isLoadingMoreData && <Spinner size={SpinnerSize.Large} />}
+    </>
+  );
+};
+
+export default NotesList;
