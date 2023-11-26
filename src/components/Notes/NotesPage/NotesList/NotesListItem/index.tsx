2ae9e8523255ff31af49e8d7941fdcc5dea3410a
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2023-11-26 03:33:20 +0200

    resolve comments

--- src/components/Notes/NotesPage/NotesList/NotesListItem/index.tsx
+++ src/components/Notes/NotesPage/NotesList/NotesListItem/index.tsx
@@ -1,33 +1,44 @@
 import React from 'react';
 
+import useTranslation from 'next-translate/useTranslation';
+
 import styles from './NotesListItem.module.scss';
 
 import { Note } from '@/types/auth/Note';
+import { dateToReadableFormat } from '@/utils/datetime';
 import { logButtonClick } from '@/utils/eventLogger';
 
-type Props = {
+interface NotesListItemProps {
   note: Note;
   setSelectedNoteId: (noteId: string) => void;
-};
+}
 
-const NotesListItem: React.FC<Props> = ({ note, setSelectedNoteId }) => {
-  const onNoteContainerClicked = (noteId: string) => {
+const NotesListItem: React.FC<NotesListItemProps> = ({ note, setSelectedNoteId }) => {
+  const { lang } = useTranslation();
+
+  const onNoteClicked = (noteId: string) => {
     logButtonClick('note_list_item', {
       noteId,
     });
     setSelectedNoteId(noteId);
   };
+
   return (
     <div
       className={styles.note}
       key={note.id}
       role="button"
       tabIndex={0}
-      onClick={() => onNoteContainerClicked(note.id)}
-      onKeyDown={() => onNoteContainerClicked(note.id)}
+      onClick={() => onNoteClicked(note.id)}
+      onKeyDown={() => onNoteClicked(note.id)}
     >
       <h3>{note.title}</h3>
       <p>{note.body}</p>
+      <time className={styles.noteDate} dateTime={note.createdAt.toString()}>
+        {dateToReadableFormat(note.createdAt, lang, {
+          year: 'numeric',
+        })}
+      </time>
     </div>
   );
 };
