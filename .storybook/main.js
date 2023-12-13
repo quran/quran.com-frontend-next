6bbf0da09cd2b9b15d9cf7c6a2945545265a5be7
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2023-12-13 10:14:56 +0200

    fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,6 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
     "@storybook/addon-storysource",
-    "@storybook/addon-styling-webpack"
   ],
   framework: {
     name: '@storybook/nextjs',
