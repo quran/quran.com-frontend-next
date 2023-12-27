29898b1f2db8d24a938b8d24b13f8ad29bb88cd7
Hammad1007 <hammadrashid2001@gmail.com>
2023-12-27 18:11:33 +0500

    Install Selenium and make the first demo test

---
+++ selenium/demo.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+# Define Chrome WebDriver options
+chrome_options = Options()
+service = Service('/Users/mac005/Downloads/chromedriver-mac-arm64/chromedriver')
+
+# Initialize Chrome WebDriver
+driver = webdriver.Chrome(service=service, options=chrome_options)
+# Open the Flask application
+driver.get('http://google.com')  
+
+# Close the browser window
+driver.quit()
