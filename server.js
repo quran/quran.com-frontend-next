360f32e197251dc33db1a6999d4fefe33ef8f537
Osama Sayed <toto777_90@hotmail.com>
2024-01-16 01:14:06 +0700

    revert server.js changes

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.prepare().then(() => {
       const parsedUrl = parse(req.url, true);
       handle(req, res, parsedUrl);
     },
-  ).listen(3005, (err) => {
+  ).listen(3000, (err) => {
     if (err) throw err;
-    console.log('> Server started on https://localhost:3005');
+    console.log('> Server started on https://localhost:3000');
   });
 });
