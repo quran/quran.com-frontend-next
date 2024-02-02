ddd13bd41238e670d60bc786ec759121c1bd8b3a
Osama Sayed <toto777_90@hotmail.com>
2024-02-02 22:17:53 +0700

    fix linting error

--- src/components/Navbar/SettingsDrawer/TranslationSelectionBody.tsx
+++ src/components/Navbar/SettingsDrawer/TranslationSelectionBody.tsx
@@ -3,8 +3,8 @@ import { useCallback, useState } from 'react';
 import { Action } from '@reduxjs/toolkit';
 import groupBy from 'lodash/groupBy';
 import omit from 'lodash/omit';
-import { useRouter } from 'next/router';
 import useTranslation from 'next-translate/useTranslation';
+import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 
 import styles from './SearchSelectionBody.module.scss';
