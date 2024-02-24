29b724e64621d36e2e36fb103fb03d32d67d041b
Muhammad Muhajir <muhajirframe@gmail.com>
2024-02-24 18:35:59 +0700

    update

--- src/components/Verse/GeneratedImage.tsx
+++ src/components/Verse/GeneratedImage.tsx
@@ -1,5 +1,6 @@
 import download from 'downloadjs';
 import * as htmlToImage from 'html-to-image';
+import useTranslation from 'next-translate/useTranslation';
 
 import styles from './GeneratedImage.module.scss';
 
@@ -8,6 +9,8 @@ import Button, { ButtonType } from '@/dls/Button/Button';
 import { QuranFont } from '@/types/QuranReader';
 import Verse from '@/types/Verse';
 import { getVerseWords } from '@/utils/verse';
+import { useContext } from 'react';
+import DataContext from '@/contexts/DataContext';
 
 const NODE_ID = 'GENERATAED-IMAGE';
 type Props = {
@@ -15,6 +18,12 @@ type Props = {
 };
 
 const GeneratedImage = ({ verse }: Props) => {
+  const { t } = useTranslation('common');
+  const chaptersData = useContext(DataContext);
+
+  const surah = chaptersData[verse.chapterId];
+
+
   const handleDownloadImage = () => {
     const element = document.getElementById(NODE_ID);
     if (element) {
@@ -46,7 +55,15 @@ const GeneratedImage = ({ verse }: Props) => {
   const translation = <div className={styles.translationText}>{translationText}</div>;
 
   // TODO: include surah name
-  const verseKeyFootnote = <div className={styles.verseKeyFootnote}>Quran {verse.verseKey}</div>;
+  const verseKeyCitation = (
+    <div className={styles.verseKeyCitation}>
+      {t('share-modal.ayah-citation', {
+        surahNumber: verse.chapterId,
+        ayahNumber: verse.verseNumber,
+        surahName: surah.transliteratedName,
+      })}
+    </div>
+  );
 
   return (
     <div>
@@ -56,12 +73,16 @@ const GeneratedImage = ({ verse }: Props) => {
         <div className={styles.content}>
           {arabicText}
           {translation}
-          {verseKeyFootnote}
+          {verseKeyCitation}
         </div>
       </div>
       <div className={styles.actionsContainer}>
-        <Button type={ButtonType.Primary} onClick={handleDownloadImage}>Download</Button>
-        <Button type={ButtonType.Success} onClick={handleDownloadImage}>Share</Button>
+        <Button type={ButtonType.Primary} onClick={handleDownloadImage}>
+          {t('share-modal.download')}
+        </Button>
+        <Button type={ButtonType.Success} onClick={handleDownloadImage}>
+          {t('share-modal.share')}
+        </Button>
       </div>
     </div>
   );
