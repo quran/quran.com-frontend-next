8b853b76174b2493e44798af1b3de93a30c6ba7f
Osama Sayed <toto777_90@hotmail.com>
2024-02-25 19:18:33 +0700

    Add learning plans og image

--- src/pages/learning-plans/index.tsx
+++ src/pages/learning-plans/index.tsx
@@ -3,6 +3,7 @@ import useTranslation from 'next-translate/useTranslation';
 
 import CoursesPageLayout from '@/components/Course/CoursesPageLayout';
 import NextSeoWrapper from '@/components/NextSeoWrapper';
+import { getLearningPlansImageUrl } from '@/lib/og';
 import { getAllChaptersData } from '@/utils/chapter';
 import { getLanguageAlternates } from '@/utils/locale';
 import { getCanonicalUrl, getCoursesNavigationUrl } from '@/utils/navigation';
@@ -17,6 +18,11 @@ const LearningPlansPage: NextPage = () => {
         canonical={getCanonicalUrl(lang, getCoursesNavigationUrl())}
         languageAlternates={getLanguageAlternates(getCoursesNavigationUrl())}
         description={t('learning-plans-meta-desc')}
+        image={getLearningPlansImageUrl({
+          locale: lang,
+        })}
+        imageWidth={1200}
+        imageHeight={630}
       />
       <CoursesPageLayout />
     </>
