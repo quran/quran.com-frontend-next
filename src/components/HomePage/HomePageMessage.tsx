2928d2c2d5a00e62ba5e86ee0cb690108b6d0e61
Osama Sayed <toto777_90@hotmail.com>
2024-02-25 15:17:26 +0700

    Add dismiss button

--- src/components/HomePage/HomePageMessage.tsx
+++ src/components/HomePage/HomePageMessage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import useTranslation from 'next-translate/useTranslation';
+
 import styles from './HomePageMessage.module.scss';
 
 import DonateButton from '@/components/Fundraising/DonateButton';
 import LearnMoreButton from '@/components/Fundraising/DonateButton/LearnMoreButton';
+import Button, { ButtonShape, ButtonSize, ButtonVariant } from '@/dls/Button/Button';
+import CloseIcon from '@/icons/close.svg';
 import DonateButtonClickSource from '@/types/DonateButtonClickSource';
 import DonateButtonType from '@/types/DonateButtonType';
 import LearnMoreClickSource from '@/types/LearnMoreClickSource';
@@ -12,14 +16,28 @@ type HomePageMessageProps = {
   title?: string;
   subtitle?: string;
   body?: React.ReactNode;
+  onClose?: () => void;
 };
 
-const HomePageMessage = ({ title, subtitle, body }: HomePageMessageProps) => {
+const HomePageMessage = ({ title, subtitle, body, onClose }: HomePageMessageProps) => {
+  const { t } = useTranslation('common');
+
   return (
     <div className={styles.container}>
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{subtitle}</p>
       {body}
+      <div className={styles.closeIcon}>
+        <Button
+          size={ButtonSize.Small}
+          shape={ButtonShape.Circle}
+          variant={ButtonVariant.Ghost}
+          onClick={onClose}
+          ariaLabel={t('aria.msg-close')}
+        >
+          <CloseIcon />
+        </Button>
+      </div>
 
       <div className={styles.ctaContainer}>
         <DonateButton
--- src/components/HomePage/HomePageWelcomeMessage.tsx
+++ src/components/HomePage/HomePageWelcomeMessage.tsx
@@ -3,17 +3,35 @@
 import React from 'react';
 
 import useTranslation from 'next-translate/useTranslation';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 
 import HomePageMessage from './HomePageMessage';
 
+import {
+  selectWelcomeMessage,
+  setIsVisible as setIsWelcomeMessageVisible,
+} from '@/redux/slices/welcomeMessage';
+import { logButtonClick } from '@/utils/eventLogger';
+
 const HomePageWelcomeMessage = () => {
   const { t } = useTranslation('common');
+
+  const { isVisible } = useSelector(selectWelcomeMessage, shallowEqual);
+  const dispatch = useDispatch();
+
+  const onClose = () => {
+    logButtonClick('homepage_welcome_message_close');
+    dispatch(setIsWelcomeMessageVisible(false));
+  };
+
+  if (!isVisible) return null;
 
   return (
     <HomePageMessage
       title={t('fundraising.title')}
       subtitle={t('fundraising.description')}
       body={null}
+      onClose={onClose}
     />
   );
 };
