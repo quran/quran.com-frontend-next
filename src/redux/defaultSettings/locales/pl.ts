fb757d0e46ab6de1f49ac7783b91495f466d117e
Bilal Hassona <bilal.hassona@gmail.com>
2024-03-25 04:21:34 +0100

    Fix indent in one file.

--- src/redux/defaultSettings/locales/pl.ts
+++ src/redux/defaultSettings/locales/pl.ts
@@ -3,6 +3,6 @@ import DEFAULT_SETTINGS, { DefaultSettings } from '../defaultSettings';
 const DEFAULT_TRANSLATION = 42; // Józef Bielawski
 
 export default {
-    ...DEFAULT_SETTINGS,
-    translations: { ...DEFAULT_SETTINGS.translations, selectedTranslations: [DEFAULT_TRANSLATION] },
+  ...DEFAULT_SETTINGS,
+  translations: { ...DEFAULT_SETTINGS.translations, selectedTranslations: [DEFAULT_TRANSLATION] },
 } as DefaultSettings;
