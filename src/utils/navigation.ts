a426387cea7515d802a353a10cadd782c683bf8a
Osama Sayed <toto777_90@hotmail.com>
2024-04-08 03:28:12 +0200

    [Quranic Calendar] Update navigation URL

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -318,7 +318,7 @@ export const getReadingGoalProgressNavigationUrl = () => '/reading-goal/progress
 export const getNotesNavigationUrl = () => '/notes-and-reflections';
 
 export const getNotificationSettingsNavigationUrl = () => '/notification-settings';
-export const getQuranicCalendarNavigationUrl = () => '/quranic-calendar';
+export const getQuranicCalendarNavigationUrl = () => '/calendar';
 
 /**
  * Update the browser history with the new url.
