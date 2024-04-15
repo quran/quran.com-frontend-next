b8052d57fc96fe34bc704d1e959321d0f1f5c277
Osama Sayed <toto777_90@hotmail.com>
2024-04-15 10:41:56 +0200

    fix TS issue

--- src/utils/hijri-date.ts
+++ src/utils/hijri-date.ts
@@ -47,11 +47,11 @@ export const getCurrentQuranicCalendarWeek = (currentHijriDate: umalqura.UmAlQur
 export const generateWeeksOfMonths = (
   months: Month[],
 ): {
-  monthsMap: { [key: string]: QuranicCalendarMonthData };
+  monthsDataMap: { [key: string]: QuranicCalendarMonthData };
   currentMonthIndex: number;
 } => {
   const today = umalqura();
-  const monthsMap = {} as {
+  const monthsDataMap = {} as {
     [key: string]: QuranicCalendarMonthData;
   };
   let currentMonthIndex;
@@ -62,11 +62,11 @@ export const generateWeeksOfMonths = (
     const monthYearKey = `${firstDayOfHijriMonth.hy}-${firstDayOfHijriMonth.hm}`;
     // 2. initialize current month details
     // @ts-ignore
-    monthsMap[monthYearKey] = {};
+    monthsDataMap[monthYearKey] = {};
     // @ts-ignore
-    monthsMap[monthYearKey].weeks = {};
+    monthsDataMap[monthYearKey].weeks = {};
     // @ts-ignore
-    monthsMap[monthYearKey].firstDateOfMonth = firstDayOfHijriMonth.date;
+    monthsDataMap[monthYearKey].firstDateOfMonth = firstDayOfHijriMonth.date;
     // filter null days (days before or after start of month) and also filter days before the first day of the month if we are not starting from the first day of the month exactly
     const daysInMonth = firstDayOfHijriMonth.monthArray
       .flat()
@@ -83,7 +83,7 @@ export const generateWeeksOfMonths = (
     // 4. if the month is the current month, set the current month flag
     const isCurrentMonth = today.hy === months[i].year && today.hm === months[i].month;
     // @ts-ignore
-    monthsMap[monthYearKey].isCurrentMonth = isCurrentMonth;
+    monthsDataMap[monthYearKey].isCurrentMonth = isCurrentMonth;
     // 5. save the index of the current month so we can automatically scroll to later
     if (isCurrentMonth) {
       currentMonthIndex = i;
@@ -92,12 +92,12 @@ export const generateWeeksOfMonths = (
     Object.keys(weeksOfMonthDaysMap).forEach((weekKey) => {
       const weekDays = weeksOfMonthDaysMap[weekKey];
       // @ts-ignore
-      monthsMap[monthYearKey].weeks[weekKey] = {
+      monthsDataMap[monthYearKey].weeks[weekKey] = {
         weekDays,
         isCurrentWeek: isCurrentMonth && weekDays[0].weekOfYear === today.weekOfYear,
       };
     });
   }
 
-  return { monthsMap, currentMonthIndex };
+  return { monthsDataMap, currentMonthIndex };
 };
