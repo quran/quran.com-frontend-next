24eff3240927285d5326601537a2bdd0dd0633d1
Osama Sayed <toto777_90@hotmail.com>
2024-04-16 19:38:54 +0200

    Fix issue with wrong url parsing of reflection body

--- src/utils/quranReflect/bodyParser.test.ts
+++ src/utils/quranReflect/bodyParser.test.ts
@@ -16,3 +16,14 @@ it('converts a body containing only 1 hashtag to a link successfully', () => {
     `<a target="_blank" href="${getQuranReflectTagUrl('#Week1')}" class="hashtagStyle">#Week1</a>`,
   );
 });
+
+it('Keeps HTML links as it is', () => {
+  const URL = "<a href='https://bit.ly/QCWK1' target='_blank'>https://bit.ly/QCWK1</a>";
+  const actual = parseReflectionBody(URL, '');
+
+  expect(actual).toBe(URL);
+});
+it('Converts string links to HTML tags correctly', () => {
+  const actual = parseReflectionBody('https://bit.ly/QCWK1', '');
+  expect(actual).toBe("<a href='https://bit.ly/QCWK1' target='_blank'>https://bit.ly/QCWK1</a>");
+});
