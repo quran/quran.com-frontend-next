bad4f6a5b37c1ef36fb9a722bee142ab03188086
Ahmed Hussein <ahmed_hussein.dev@outlook.com>
2024-05-16 13:19:34 +0300

    use isValidPageNumber

--- src/pages/page/[pageId].tsx
+++ src/pages/page/[pageId].tsx
@@ -18,7 +18,7 @@ import {
   ONE_WEEK_REVALIDATION_PERIOD_SECONDS,
   REVALIDATION_PERIOD_ON_ERROR_SECONDS,
 } from '@/utils/staticPageGeneration';
-import { isValidPageId } from '@/utils/validator';
+import { isValidPageNumber } from '@/utils/validator';
 import { VersesResponse } from 'types/ApiResponses';
 import ChaptersData from 'types/ChaptersData';
 import { QuranReaderDataType } from 'types/QuranReader';
@@ -62,22 +62,22 @@ const QuranicPage: NextPage<Props> = ({ hasError, pageVerses }) => {
 // eslint-disable-next-line react-func/max-lines-per-function
 export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
   const pageIdNumber = Number(params.pageId);
-  // we need to validate the pageId first to save calling BE since we haven't set the valid paths inside getStaticPaths to avoid pre-rendering them at build time.
-  if (!isValidPageId(pageIdNumber)) {
-    return {
-      notFound: true,
-    };
-  }
-
   const defaultMushafId = getMushafId(
     getQuranReaderStylesInitialState(locale).quranFont,
     getQuranReaderStylesInitialState(locale).mushafLines,
   ).mushaf;
 
+  // we need to validate the pageId first to save calling BE since we haven't set the valid paths inside getStaticPaths to avoid pre-rendering them at build time.
+  if (!isValidPageNumber(pageIdNumber, defaultMushafId)) {
+    return {
+      notFound: true,
+    };
+  }
+
   // The defaultMushafId is 2 representing the Madinah Mushaf
   // PAGES_MUSHAF_MAP will return the mushaf total number of pages when passed a mushafId
   // Mushaf ID: 2 (Madinah) -> Pages Count: 604 pages
-  const defaultMushafPagesCount = PAGES_MUSHAF_MAP[Number(defaultMushafId)];
+  const defaultMushafPagesCount = PAGES_MUSHAF_MAP[defaultMushafId];
   // In case the requested page/[pageId] is greater than the SSR loaded default mushaf total pages count
   // we set the pageId to the last available page, otherwise we load the passed pageID
   const pageId =
