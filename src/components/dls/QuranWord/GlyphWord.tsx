8c2974fefdb8d7a4fea1a43d4efeec96b9946004
Ahmed Hussein <ahmed_hussein.dev@outlook.com>
2024-05-22 16:52:54 +0300

    Revert "Fix cutting words on firefox"
    This reverts commit 1be29242a4aa297186b373e64ba650c76ed446ac.

--- src/components/dls/QuranWord/GlyphWord.tsx
+++ src/components/dls/QuranWord/GlyphWord.tsx
@@ -56,8 +56,7 @@ const GlyphWord = ({
   return (
     <span
       dangerouslySetInnerHTML={{
-        // The white space will allow firefox browser to show the full word without any word cuts
-        __html: ` ${getWordText(qpcUthmaniHafs, textCodeV1, textCodeV2, font, isFontLoaded)}`,
+        __html: getWordText(qpcUthmaniHafs, textCodeV1, textCodeV2, font, isFontLoaded),
       }}
       data-font-scale={quranTextFontScale}
       data-font={font}
