0d2095cb8eaa9d383bec4e54b78954c269b6e791
Osama Sayed <toto777_90@hotmail.com>
2024-06-10 16:11:27 +0300

    Remove homepage message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,6 @@ import styles from './index.module.scss';
 
 import ChapterAndJuzListWrapper from '@/components/chapters/ChapterAndJuzList';
 import HomePageHero from '@/components/HomePage/HomePageHero';
-import HomePageWelcomeMessage from '@/components/HomePage/HomePageWelcomeMessage';
 import QuranGrowthJourneySection from '@/components/HomePage/QuranGrowthJourneySection';
 import RamadanActivitiesSection from '@/components/HomePage/RamadanActivitiesSection';
 import NextSeoWrapper from '@/components/NextSeoWrapper';
@@ -41,9 +40,6 @@ const Index: NextPage<IndexProps> = ({ chaptersResponse: { chapters } }): JSX.El
       <div className={styles.pageContainer}>
         <div className={styles.flow}>
           <HomePageHero />
-          <div className={classNames(styles.flowItem, styles.fullWidth)}>
-            <HomePageWelcomeMessage />
-          </div>
           <div className={classNames(styles.flowItem, styles.fullWidth)}>
             <RamadanActivitiesSection />
           </div>
