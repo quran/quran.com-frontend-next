58378f4456db142e4c4eaeada1e3e335e285a22d
Hafiz Mohsin Ayoob <mohsinayoob5575@gmail.com>
2024-08-18 20:24:16 +0500

    introduced proxy to backend with redirect to auth

---
+++ src/pages/api/proxy/[...path].ts
@@ -0,0 +1,53 @@
+import { EventEmitter } from 'events';
+
+import { createProxyMiddleware, fixRequestBody } from 'http-proxy-middleware';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+// Define error messages in a constant object
+const ERROR_MESSAGES = {
+  PROXY_ERROR: 'Proxy error',
+  PROXY_HANDLER_ERROR: 'Proxy handler error',
+};
+
+// Increase the max listeners to avoid memory leak
+EventEmitter.defaultMaxListeners = 20;
+
+const apiProxy = createProxyMiddleware<NextApiRequest, NextApiResponse>({
+  target: process.env.NEXT_PUBLIC_AUTH_BASE_URL,
+  changeOrigin: true,
+  pathRewrite: { '^/api/proxy': '' }, // eslint-disable-line @typescript-eslint/naming-convention
+  secure: false, // Disable SSL verification to avoid UNABLE_TO_VERIFY_LEAF_SIGNATURE error
+  logger: console,
+
+  on: {
+    proxyReq: (proxyReq, req) => {
+      // Attach cookies from the request to the proxy request
+      if (req.headers.cookie) {
+        proxyReq.setHeader('Cookie', req.headers.cookie);
+      }
+
+      // Fix the request body if bodyParser is involved
+      fixRequestBody(proxyReq, req);
+    },
+
+    proxyRes: (proxyRes, req, res) => {
+      // Set cookies from the proxy response to the original response
+      const proxyCookies = proxyRes.headers['set-cookie'];
+      if (proxyCookies) {
+        res.setHeader('Set-Cookie', proxyCookies);
+      }
+    },
+
+    error: (err, req, res) => {
+      res.end(() => ({ error: ERROR_MESSAGES.PROXY_ERROR, message: err.message }));
+    },
+  },
+});
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  apiProxy(req, res, (err) => {
+    if (err) {
+      res.status(500).json({ error: ERROR_MESSAGES.PROXY_HANDLER_ERROR, message: err.message });
+    }
+  });
+}
