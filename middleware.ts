8fb98053a99abe93f2f66284c7058dfc881623ba
Ahmed Hussein <ahmedhussein.developer@gmail.com>
2024-10-16 01:46:32 +0300

    remove matcher

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-// run only on homepage
-export const config = {
-  matcher: '/',
-};
-
-const middleware = async (req: NextRequest) => {
-  const { nextUrl: url, geo } = req;
-  const country = geo.country || 'US';
-
-  url.searchParams.set('country', country);
-
-  return NextResponse.rewrite(url);
+const middleware = async (request: NextRequest) => {
+  const country = request.geo?.country || 'US';
+  request.nextUrl.searchParams.set('country', country);
+  return NextResponse.rewrite(request.nextUrl);
 };
 
 export default middleware;
