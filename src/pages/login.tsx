4ebc513cee2f2d912e906cc4387e435fbc9e1647
Osama Sayed <osamasayed@users.noreply.github.com>
2024-10-29 17:13:46 +0800

    QF-982 allow user to play audio on login page (#2225)

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,22 @@
 import { useCallback, useEffect } from 'react';
 
+import { GetStaticProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
 import useTranslation from 'next-translate/useTranslation';
 
 import LoginContainer from '@/components/Login/LoginContainer';
 import PageContainer from '@/components/PageContainer';
 import { ToastStatus, useToast } from '@/dls/Toast/Toast';
+import ChaptersData from '@/types/ChaptersData';
+import { getAllChaptersData } from '@/utils/chapter';
 import { getLoginNavigationUrl } from '@/utils/navigation';
 import AuthError from 'types/AuthError';
 
-const LoginPage = () => {
+interface Props {
+  chaptersData?: ChaptersData;
+}
+
+const LoginPage: NextPage<Props> = () => {
   const toast = useToast();
   const { query, replace } = useRouter();
   const { t } = useTranslation('login');
@@ -39,6 +46,16 @@ const LoginPage = () => {
       <LoginContainer />
     </PageContainer>
   );
+};
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  const allChaptersData = await getAllChaptersData(locale);
+
+  return {
+    props: {
+      chaptersData: allChaptersData,
+    },
+  };
 };
 
 export default LoginPage;
