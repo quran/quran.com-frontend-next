640bfffc88e4945fe97de9d1fe78d86930203e28
Osama Sayed <toto777_90@hotmail.com>
2025-02-22 14:36:49 +0700

    test

--- src/components/Login/SignUpForm/errors.ts
+++ src/components/Login/SignUpForm/errors.ts
@@ -10,7 +10,7 @@ import {
 
 import SignUpRequest from '@/types/auth/SignUpRequest';
 
-type SignUpErrors = Record<keyof SignUpRequest, string>;
+type AuthErrors = Record<keyof SignUpRequest, string>;
 
 export enum ErrorType {
   MISMATCH = 'mismatch',
@@ -21,16 +21,8 @@ export enum ErrorType {
   RESET_PASSWORD = 'reset-password',
 }
 
-const getFormErrors = (t: any, type: ErrorType, apiErrors?: any): { errors: SignUpErrors } => {
-  const baseErrors = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    username: '',
-    password: '',
-    confirmPassword: '',
-    verificationCode: '',
-  };
+const getFormErrors = (t: any, type: ErrorType, apiErrors?: any): { errors: AuthErrors } => {
+  const baseErrors = {};
 
   switch (type) {
     case ErrorType.MISMATCH:
@@ -38,82 +30,87 @@ const getFormErrors = (t: any, type: ErrorType, apiErrors?: any): { errors: Sign
         errors: {
           ...baseErrors,
           confirmPassword: t('errors.confirm'),
-        },
+        } as AuthErrors,
       };
-    case ErrorType.API:
-      return {
-        errors: {
-          ...baseErrors,
-          email: apiErrors?.email
-            ? t(apiErrors.email, {
-                fieldName: t('common:form.email'),
-              })
-            : '',
-          username: apiErrors?.username
-            ? t(apiErrors.username, {
-                fieldName: t('common:form.username'),
-                min: USERNAME_MIN_LENGTH,
-                max: USERNAME_MAX_LENGTH,
-              })
-            : '',
-          password: apiErrors?.password
-            ? t(apiErrors.password, {
-                fieldName: t('common:form.password'),
-                min: PASSWORD_MIN_LENGTH,
-                max: PASSWORD_MAX_LENGTH,
-              })
-            : '',
-          confirmPassword: apiErrors?.confirmPassword
-            ? t(apiErrors.confirmPassword, {
-                fieldName: t('common:form.confirm-password'),
-              })
-            : '',
-          firstName: apiErrors?.firstName
-            ? t(apiErrors.firstName, {
-                fieldName: t('common:form.firstName'),
-                min: NAME_MIN_LENGTH,
-                max: NAME_MAX_LENGTH,
-              })
-            : '',
-          lastName: apiErrors?.lastName
-            ? t(apiErrors.lastName, {
-                fieldName: t('common:form.lastName'),
-                min: NAME_MIN_LENGTH,
-                max: NAME_MAX_LENGTH,
-              })
-            : '',
-        },
-      };
+    case ErrorType.API: {
+      const errors: Partial<AuthErrors> = { ...baseErrors };
+
+      if (apiErrors?.email) {
+        errors.email = t(apiErrors.email, {
+          fieldName: t('common:form.email'),
+        });
+      }
+
+      if (apiErrors?.username) {
+        errors.username = t(apiErrors.username, {
+          fieldName: t('common:form.username'),
+          min: USERNAME_MIN_LENGTH,
+          max: USERNAME_MAX_LENGTH,
+        });
+      }
+
+      if (apiErrors?.password) {
+        errors.password = t(apiErrors.password, {
+          fieldName: t('common:form.password'),
+          min: PASSWORD_MIN_LENGTH,
+          max: PASSWORD_MAX_LENGTH,
+        });
+      }
+
+      if (apiErrors?.confirmPassword) {
+        errors.confirmPassword = t(apiErrors.confirmPassword, {
+          fieldName: t('common:form.confirm-password'),
+        });
+      }
+
+      if (apiErrors?.firstName) {
+        errors.firstName = t(apiErrors.firstName, {
+          fieldName: t('common:form.firstName'),
+          min: NAME_MIN_LENGTH,
+          max: NAME_MAX_LENGTH,
+        });
+      }
+
+      if (apiErrors?.lastName) {
+        errors.lastName = t(apiErrors.lastName, {
+          fieldName: t('common:form.lastName'),
+          min: NAME_MIN_LENGTH,
+          max: NAME_MAX_LENGTH,
+        });
+      }
+
+      return { errors: errors as AuthErrors };
+    }
     case ErrorType.SIGNUP:
       return {
         errors: {
           ...baseErrors,
           password: t('errors.signup-failed'),
-        },
+        } as AuthErrors,
       };
     case ErrorType.FORGOT_PASSWORD:
       return {
         errors: {
           ...baseErrors,
           email: t('errors.forgot-password-failed'),
-        },
+        } as AuthErrors,
       };
     case ErrorType.SIGN_IN:
       return {
         errors: {
           ...baseErrors,
           password: t('errors.signin-failed'),
-        },
+        } as AuthErrors,
       };
     case ErrorType.RESET_PASSWORD:
       return {
         errors: {
           ...baseErrors,
           password: t('errors.reset-password-failed'),
-        },
+        } as AuthErrors,
       };
     default:
-      return { errors: baseErrors };
+      return { errors: baseErrors as AuthErrors };
   }
 };
 
