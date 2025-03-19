0909138df6eec31c5d8c6a704c327d1e95108f12
Ahmed Hussein <ahmedhussein.developer@gmail.com>
2025-03-19 02:48:29 +0200

    update community section (#2360)

--- src/components/Navbar/NavigationDrawer/CommunitySection.tsx
+++ src/components/Navbar/NavigationDrawer/CommunitySection.tsx
@@ -16,7 +16,11 @@ const CommunitySection = () => {
       <div className={styles.flow}>
         <div className={styles.title}>{t('community.header')}</div>
         <div>{t('community.sub-header')}</div>
-        <Link href="https://discord.gg/FxRWSBfWxn" isNewTab className={styles.joinCommunityLink}>
+        <Link
+          href="https://forms.gle/Lb4TFoxkSzjx5XHx6"
+          isNewTab
+          className={styles.joinCommunityLink}
+        >
           <Button href="" type={ButtonType.Success}>
             {t('community.cta')}
           </Button>
