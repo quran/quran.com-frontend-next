a73b05375a248f6229883ae95e0330633d1bffce
Osama Sayed <toto777_90@hotmail.com>
2025-03-26 13:59:26 +0200

    Remove Ramadan section from community

--- src/components/HomePage/CommunitySection/index.tsx
+++ src/components/HomePage/CommunitySection/index.tsx
@@ -8,18 +8,12 @@ import Card from '@/components/HomePage/Card';
 import IconContainer, { IconSize } from '@/dls/IconContainer/IconContainer';
 import QRColoredIcon from '@/icons/qr-colored.svg';
 import QRLogoIcon from '@/icons/qr-logo.svg';
-import TicketIcon from '@/icons/ticket.svg';
 import ArrowIcon from '@/public/icons/arrow.svg';
 import { logButtonClick } from '@/utils/eventLogger';
-import { getRamadanNavigationUrl } from '@/utils/navigation';
 import { getQRNavigationUrl } from '@/utils/quranReflect/navigation';
 
 const CommunitySection = () => {
   const { t } = useTranslation('home');
-
-  const onRamadanCardClicked = () => {
-    logButtonClick('homepage_community_ramadan_card');
-  };
 
   const onQRCardClicked = () => {
     logButtonClick('homepage_community_qr_card');
@@ -31,29 +25,6 @@ const CommunitySection = () => {
         <h1>{t('common:community.title')}</h1>
       </div>
       <div className={styles.cardsContainer}>
-        <Card
-          className={styles.card}
-          link={getRamadanNavigationUrl()}
-          isNewTab
-          linkClassName={styles.link}
-          onClick={onRamadanCardClicked}
-        >
-          <div className={styles.cardContent}>
-            <div className={styles.ramadanTitle}>
-              <div className={styles.titleWithIcon}>
-                <TicketIcon />
-                <p className={styles.title}>{t('ramadan.title')}</p>
-              </div>
-              <IconContainer
-                size={IconSize.Xsmall}
-                icon={<ArrowIcon />}
-                shouldForceSetColors={false}
-                className={styles.arrowIcon}
-              />
-            </div>
-            <p className={styles.cardDescription}>{t('ramadan.header')}</p>
-          </div>
-        </Card>
         <Card
           className={styles.card}
           link={getQRNavigationUrl()}
