1605221a43a06fc67c3e733344d3806630c638b3
Osama Sayed <toto777_90@hotmail.com>
2025-05-19 11:36:24 +0300

    Disable automaticVercelMonitors

--- next.config.js
+++ next.config.js
@@ -192,5 +192,5 @@ module.exports = withSentryConfig(configWithPlugins, {
   // See the following for more information:
   // https://docs.sentry.io/product/crons/
   // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
+  automaticVercelMonitors: false,
 });
