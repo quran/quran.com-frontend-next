8d74f44eb89e922b89dbfbb7e21248ef1c5ea8b1
Hafiz Mohsin Ayoob <41894237+mohsinayoob@users.noreply.github.com>
2025-05-25 18:30:53 +0500

    Protects next.js proxy from unauthorized usage (#2273)
    * chore: protected proxy by origins and signature
    
    * env vars fix
    
    * fix: checking of origin with exact match
    
    * fix: fixed api call in auth to use secure proxy
    
    ---------
    
    Co-authored-by: Osama Sayed <toto777_90@hotmail.com>

--- src/pages/api/proxy/[...path].ts
+++ src/pages/api/proxy/[...path].ts
@@ -3,23 +3,90 @@ import { EventEmitter } from 'events';
 import { createProxyMiddleware, fixRequestBody } from 'http-proxy-middleware';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { X_AUTH_SIGNATURE, X_INTERNAL_CLIENT, X_TIMESTAMP } from '@/api';
 import generateSignature from '@/utils/auth/signature';
+import {
+  X_AUTH_SIGNATURE,
+  X_INTERNAL_CLIENT,
+  X_TIMESTAMP,
+  X_PROXY_SIGNATURE,
+  X_PROXY_TIMESTAMP,
+} from '@/utils/headers';
 
-// Define error messages in a constant object
 const ERROR_MESSAGES = {
   PROXY_ERROR: 'Proxy error',
   PROXY_HANDLER_ERROR: 'Proxy handler error',
+  FORBIDDEN: 'Forbidden',
 };
+
+const ALLOWED_DOMAINS = (process.env.ALLOWED_ORIGINS || '')
+  .split(',')
+  .map((domain) => domain.trim());
 
 // This line increases the default maximum number of event listeners for the EventEmitter to a better number like 20.
 // It is necessary to prevent memory leak warnings when multiple listeners are added,
 // which can occur in a proxy setup like this where multiple requests are handled concurrently.
 EventEmitter.defaultMaxListeners = Number(process.env.PROXY_DEFAULT_MAX_LISTENERS) || 100;
 
-// This file sets up a proxy middleware for API requests. It is needed to forward requests from the frontend
-// to the backend server, allowing for features like cookie handling and request body fixing, which are essential
-// for maintaining session state and ensuring correct request formatting while in a cross domain env.
+const isOriginAllowed = (origin: string | undefined): boolean => {
+  if (!origin) return false;
+  const url = new URL(origin);
+  const { hostname } = url;
+  return ALLOWED_DOMAINS.includes(hostname);
+};
+
+const handleProxyReq = (proxyReq, req, res) => {
+  const origin = req.headers.origin || req.headers.referer || '';
+  if (origin) {
+    if (!isOriginAllowed(origin)) {
+      res.status(403).send({ error: ERROR_MESSAGES.FORBIDDEN });
+      return;
+    }
+  } else if (!verifySignature(req, res)) {
+    return;
+  }
+
+  attachCookies(proxyReq, req);
+  attachSignatureHeaders(proxyReq, req);
+  fixRequestBody(proxyReq, req);
+};
+
+const verifySignature = (req, res) => {
+  const protocol = req.headers['x-forwarded-proto'] || 'http';
+  const requestUrl = `${protocol}://${req.headers.host}/api/proxy${req.url}`;
+  const timestampHeader = req.headers[X_PROXY_TIMESTAMP] as string;
+  const { signature } = generateSignature(
+    req,
+    requestUrl,
+    process.env.PROXY_SIGNATURE_TOKEN as string,
+    timestampHeader,
+  );
+
+  if (req.headers[X_PROXY_SIGNATURE] !== signature) {
+    res.status(403).send({ error: ERROR_MESSAGES.FORBIDDEN });
+    return false;
+  }
+  return true;
+};
+
+const attachCookies = (proxyReq, req) => {
+  if (req.headers.cookie) {
+    proxyReq.setHeader('Cookie', req.headers.cookie);
+  }
+};
+
+const attachSignatureHeaders = (proxyReq, req) => {
+  const requestUrl = `${process.env.API_GATEWAY_URL}${req.url}`;
+  const { signature, timestamp } = generateSignature(
+    req,
+    requestUrl,
+    process.env.SIGNATURE_TOKEN as string,
+  );
+
+  proxyReq.setHeader(X_AUTH_SIGNATURE, signature);
+  proxyReq.setHeader(X_TIMESTAMP, timestamp);
+  proxyReq.setHeader(X_INTERNAL_CLIENT, process.env.INTERNAL_CLIENT_ID);
+};
+
 const apiProxy = createProxyMiddleware<NextApiRequest, NextApiResponse>({
   target: process.env.API_GATEWAY_URL,
   changeOrigin: true,
@@ -28,23 +95,7 @@ const apiProxy = createProxyMiddleware<NextApiRequest, NextApiResponse>({
   logger: console,
 
   on: {
-    proxyReq: (proxyReq, req) => {
-      // Attach cookies from the request to the proxy request
-      if (req.headers.cookie) {
-        proxyReq.setHeader('Cookie', req.headers.cookie);
-      }
-
-      // Generate and attach signature headers
-      const requestUrl = `${process.env.API_GATEWAY_URL}${req.url}`;
-      const { signature, timestamp } = generateSignature(req, requestUrl);
-
-      proxyReq.setHeader(X_AUTH_SIGNATURE, signature);
-      proxyReq.setHeader(X_TIMESTAMP, timestamp);
-      proxyReq.setHeader(X_INTERNAL_CLIENT, process.env.INTERNAL_CLIENT_ID);
-
-      // Fix the request body if bodyParser is involved
-      fixRequestBody(proxyReq, req);
-    },
+    proxyReq: handleProxyReq,
 
     proxyRes: (proxyRes, req, res) => {
       // Set cookies from the proxy response to the original response
