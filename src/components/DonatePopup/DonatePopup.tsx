66324764bad7b7e4e4abacbf7c7270f6f89db3cd
Osama Sayed <toto777_90@hotmail.com>
2025-06-06 11:05:02 +0300

    Revert "refactor: simplify donate popup UI and update hadith reference (#2427)"
    This reverts commit cecf95a8400c9c8f5922b4805714633acac12b9f.

--- src/components/DonatePopup/DonatePopup.tsx
+++ src/components/DonatePopup/DonatePopup.tsx
@@ -51,6 +51,7 @@ const DonatePopup = () => {
           <div className={styles.textsContainer}>
             <p className={styles.text}>{t('popup.subtitle')}</p>
             <p className={styles.text}>{t('popup.text-1')}</p>
+            <p className={styles.text}>{t('popup.text-2')}</p>
           </div>
           <div className={styles.actionsContainer}>
             <DonateButton
@@ -71,6 +72,7 @@ const DonatePopup = () => {
               onAdditionalClick={onPopupClose}
             />
           </div>
+          <div className={styles.text}>{t('popup.footnote')}.</div>
         </div>
       </div>
     </Modal>
