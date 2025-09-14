bd99e4b4dad52303f82ae1dbad75d24917783551
Osama Sayed <osamasayed@users.noreply.github.com>
2025-09-14 21:30:22 +0300

    Update developers page (#2494)

--- src/components/Navbar/NavigationDrawer/CommunitySection.tsx
+++ src/components/Navbar/NavigationDrawer/CommunitySection.tsx
@@ -4,15 +4,11 @@ import styles from './CommunitySection.module.scss';
 
 import Button, { ButtonType } from '@/dls/Button/Button';
 import Link from '@/dls/Link/Link';
-import DiscordIcon from '@/icons/discord-icon.svg';
 
 const CommunitySection = () => {
   const { t } = useTranslation('common');
   return (
     <div className={styles.container}>
-      <div className={styles.platformLogoBackground}>
-        <DiscordIcon />
-      </div>
       <div className={styles.flow}>
         <div className={styles.title}>{t('community.header')}</div>
         <div>{t('community.sub-header')}</div>
