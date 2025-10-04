b2dff1070920db04e2161464879d3c5bd3870fc9
Osama Sayed <toto777_90@hotmail.com>
2025-10-04 21:26:46 +0300

    update developers page

---
+++ src/components/ApiDocsSection/ApiDocsSection.tsx
@@ -0,0 +1,29 @@
+import useTranslation from 'next-translate/useTranslation';
+
+import styles from '../Navbar/NavigationDrawer/CommunitySection.module.scss';
+
+import Button, { ButtonType } from '@/dls/Button/Button';
+import Link from '@/dls/Link/Link';
+
+const ApiDocsSection = () => {
+  const { t } = useTranslation('developers');
+  return (
+    <div className={styles.container}>
+      <div className={styles.flow}>
+        <div className={styles.title}>{t('api-docs.header')}</div>
+        <div>{t('api-docs.sub-header')}</div>
+        <Link
+          href="https://api-docs.quran.foundation"
+          isNewTab
+          className={styles.joinCommunityLink}
+        >
+          <Button href="" type={ButtonType.Success}>
+            {t('api-docs.cta')}
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ApiDocsSection;
