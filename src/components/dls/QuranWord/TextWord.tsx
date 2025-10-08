7d2d214875ff1b883a91ccbfca86212063f9ccb0
zonetecde <zonetecde@gmail.com>
2025-10-08 13:47:15 +0200

    Fix: IndoPak font overlap

--- src/components/dls/QuranWord/TextWord.tsx
+++ src/components/dls/QuranWord/TextWord.tsx
@@ -15,24 +15,30 @@ type MadaniWordTextProps = {
 
 const DEFAULT_FONT_FAMILY = 'UthmanicHafs';
 const INDO_PAK = 'IndoPak';
+const INDO_PAK_STOP_SIGNS_REGEX = /[\u06d6-\u06dc]/;
 
 const UTHMANI_HAFS_FONTS = decamelizeKeys({
   qpcUthmaniHafs: DEFAULT_FONT_FAMILY,
   textIndopak: INDO_PAK,
 });
 
-const TextWord: React.FC<MadaniWordTextProps> = ({ text, font, charType }) => (
-  <span
-    className={classNames(styles.word, {
-      [styles[DEFAULT_FONT_FAMILY]]:
-        charType === CharType.End ||
-        !UTHMANI_HAFS_FONTS[font] ||
-        UTHMANI_HAFS_FONTS[font] === DEFAULT_FONT_FAMILY,
-      [styles[INDO_PAK]]: UTHMANI_HAFS_FONTS[font] === INDO_PAK,
-    })}
-  >
-    {text}
-  </span>
-);
+const TextWord: React.FC<MadaniWordTextProps> = ({ text, font, charType }) => {
+  const mappedFont = UTHMANI_HAFS_FONTS[font];
+  const isIndoPakFont = mappedFont === INDO_PAK;
+  const hasIndoPakStopSign = isIndoPakFont && INDO_PAK_STOP_SIGNS_REGEX.test(text);
+
+  return (
+    <span
+      className={classNames(styles.word, {
+        [styles[DEFAULT_FONT_FAMILY]]:
+          charType === CharType.End || !mappedFont || mappedFont === DEFAULT_FONT_FAMILY,
+        [styles[INDO_PAK]]: isIndoPakFont,
+        [styles.hasIndoPakStopSign]: hasIndoPakStopSign,
+      })}
+    >
+      {text}
+    </span>
+  );
+};
 
 export default TextWord;
