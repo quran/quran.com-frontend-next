d865fa4400d5bc77e21b621097e45617c888529b
Rayane Staszewski <56195432+zonetecde@users.noreply.github.com>
2025-10-09 07:25:12 +0200

    QF-3501 - Fix "Tafsirs" button triggering Surah selection (#2522)
    * Fix "Tafsirs" button triggering Surah selection
    
    * Simplification of bug fixing. preventDefault is sufficient.
    
    * Revert to the old solution: focusing the modal is important.

--- src/components/dls/ContentModal/ContentModal.tsx
+++ src/components/dls/ContentModal/ContentModal.tsx
@@ -1,4 +1,4 @@
-import { ForwardedRef, useImperativeHandle, useRef } from 'react';
+import { ForwardedRef, useCallback, useImperativeHandle, useRef } from 'react';
 
 import * as Dialog from '@radix-ui/react-dialog';
 import classNames from 'classnames';
@@ -60,6 +60,7 @@ const ContentModal = ({
   isBottomSheetOnMobile = true,
 }: ContentModalProps) => {
   const overlayRef = useRef<HTMLDivElement>();
+  const contentRef = useRef<HTMLDivElement>(null);
   const { locale } = useRouter();
   useImperativeHandle(innerRef, () => ({
     scrollToTop: () => {
@@ -95,6 +96,16 @@ const ContentModal = ({
     }
   };
 
+  /**
+   * Prevents Safari from focusing the first focusable element in the modal.
+   * @param {Event} event
+   */
+  const handleOpenAutoFocus = useCallback((event: Event) => {
+    if (event.defaultPrevented) return;
+    event.preventDefault();
+    contentRef.current?.focus({ preventScroll: true });
+  }, []);
+
   return (
     <Dialog.Root open={isOpen}>
       <Dialog.Portal>
@@ -109,6 +120,7 @@ const ContentModal = ({
         >
           <Dialog.Content
             {...(onClick && { onClick })}
+            ref={contentRef}
             className={classNames(styles.contentWrapper, {
               [contentClassName]: contentClassName,
               [styles.small]: size === ContentModalSize.SMALL,
@@ -118,6 +130,7 @@ const ContentModal = ({
             })}
             onEscapeKeyDown={onEscapeKeyDown}
             onPointerDownOutside={onPointerDownOutside}
+            onOpenAutoFocus={handleOpenAutoFocus}
           >
             {hasHeader && (
               <div className={classNames(styles.header, headerClassName)}>
