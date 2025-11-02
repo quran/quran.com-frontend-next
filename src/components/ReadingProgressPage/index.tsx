a4f355e41b22d7d47ff3ee8e343fbe252b5c7092
afifvdin <afifudin.fdn@gmail.com>
2025-11-02 15:07:35 +0700

    Redesign streak progress

--- src/components/ReadingProgressPage/index.tsx
+++ src/components/ReadingProgressPage/index.tsx
@@ -1,7 +1,5 @@
+import Head from 'next/head';
 import useTranslation from 'next-translate/useTranslation';
-
-import DeleteReadingGoalModal from '../ReadingGoal/DeleteReadingGoalModal';
-import UpdateReadingGoalModal from '../ReadingGoal/UpdateReadingGoalModal';
 
 import ProgressPageGoalWidget from './ProgressPageGoalWidget';
 import ProgressPageStreakWidget from './ProgressPageStreakWidget';
@@ -10,17 +8,22 @@ import styles from './ReadingProgressPage.module.scss';
 
 import NextSeoWrapper from '@/components/NextSeoWrapper';
 import PageContainer from '@/components/PageContainer';
+import Button, { ButtonSize, ButtonType, ButtonVariant } from '@/dls/Button/Button';
 import useGetStreakWithMetadata from '@/hooks/auth/useGetStreakWithMetadata';
+import ArrowLeft from '@/icons/arrow-left.svg';
+import Background from '@/icons/background.svg';
 import { getLanguageAlternates } from '@/utils/locale';
-import { getCanonicalUrl, getReadingGoalProgressNavigationUrl } from '@/utils/navigation';
+import {
+  getCanonicalUrl,
+  getReadingGoalNavigationUrl,
+  getReadingGoalProgressNavigationUrl,
+} from '@/utils/navigation';
 
 const ReadingProgressPage = () => {
   const { t, lang } = useTranslation('reading-progress');
-  const { error, goal, weekData, streak, currentActivityDay, isLoading } = useGetStreakWithMetadata(
-    {
-      showDayName: true,
-    },
-  );
+  const { error, goal, weekData, streak, isLoading } = useGetStreakWithMetadata({
+    showDayName: true,
+  });
 
   if (error) return null;
 
@@ -34,10 +37,30 @@ const ReadingProgressPage = () => {
         noindex
       />
 
-      <PageContainer>
-        <div className={styles.contentContainer}>
-          <h1>{t('reading-progress-header')}</h1>
+      <div className={styles.heroContainer}>
+        <Head>
+          <link rel="preload" as="image" href="/images/background.png" />
+        </Head>
+        <div className={styles.heroBackgroundImage}>
+          <Background />
+        </div>
+        <div>
+          <div className={styles.heroInnerContainer}>
+            <Button
+              type={ButtonType.Secondary}
+              size={ButtonSize.Large}
+              variant={ButtonVariant.Compact}
+              href={getReadingGoalNavigationUrl()}
+            >
+              <ArrowLeft />
+            </Button>
+            <h1>{t('reading-progress-header')}</h1>
+          </div>
+        </div>
+      </div>
 
+      <div className={styles.contentContainer}>
+        <PageContainer>
           <div className={styles.widgetsContainer}>
             <ProgressPageStreakWidget
               weekData={weekData}
@@ -46,27 +69,12 @@ const ReadingProgressPage = () => {
               isLoading={isLoading}
             />
 
-            <ProgressPageGoalWidget
-              currentActivityDay={currentActivityDay}
-              goal={goal}
-              isLoading={isLoading}
-            />
+            <ProgressPageGoalWidget goal={goal} isLoading={isLoading} />
           </div>
 
           <ReadingHistory />
-
-          {goal && (
-            <div className={styles.manageGoalSection}>
-              <h1>{t('manage-goal')}</h1>
-
-              <div className={styles.manageGoalContainer}>
-                <DeleteReadingGoalModal />
-                <UpdateReadingGoalModal goal={goal} />
-              </div>
-            </div>
-          )}
-        </div>
-      </PageContainer>
+        </PageContainer>
+      </div>
     </>
   );
 };
