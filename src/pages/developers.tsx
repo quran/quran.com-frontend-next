d385e4545422a3c5cd85fcafea6f9cee557da965
Basit Minhas <basit3407@gmail.com>
2025-12-09 14:36:58 +0500

    Improve developers page layout

--- src/pages/developers.tsx
+++ src/pages/developers.tsx
@@ -9,7 +9,6 @@ import styles from './contentPage.module.scss';
 
 import ApiDocsSection from '@/components/ApiDocsSection/ApiDocsSection';
 import AppPortalSection from '@/components/AppPortalSection';
-import CareersSection from '@/components/CareersSection/CareersSection';
 import CommunitySection from '@/components/Navbar/NavigationDrawer/CommunitySection';
 import LabsSection from '@/components/Navbar/NavigationDrawer/LabsSection';
 import NextSeoWrapper from '@/components/NextSeoWrapper';
@@ -30,120 +29,154 @@ const DevelopersPage: NextPage = () => {
       />
       <PageContainer>
         <div className={styles.contentPage}>
-          <h1>{t('careers.career')}</h1>
-          <CareersSection />
-          <h1>{t('header')}</h1>
-          <CommunitySection />
-          <h1>{t('labs')}</h1>
-          <LabsSection />
-          <h1>{t('app-portal.heading')}</h1>
-          <AppPortalSection />
-          <h1>{t('api-docs.title')}</h1>
-          <ApiDocsSection />
-          <p>{t('main-desc')}</p>
-          <p>
-            <Trans
-              i18nKey="developers:projects.all"
-              components={[
-                <a key={0} href="https://github.com/quran" target="_blank" rel="noreferrer" />,
-              ]}
-            />
-          </p>
-          <div>
-            <p>
+          <div className={styles.pageHero}>
+            <p className={styles.kicker}>{t('common:developers')}</p>
+            <h1 className={styles.heroTitle}>{t('api-docs.header')}</h1>
+            <p className={styles.heroSubtitle}>{t('main-desc')}</p>
+            <div className={styles.heroFooter}>{t('thanks')}</div>
+          </div>
+
+          <div className={styles.sectionGrid}>
+            <section className={styles.featureCard}>
+              <div className={styles.cardHeading}>
+                <span className={styles.pill}>{t('labs')}</span>
+                <p className={styles.cardLead}>{t('labs-description')}</p>
+              </div>
+              <LabsSection />
+            </section>
+
+            <section className={styles.featureCard}>
+              <div className={styles.cardHeading}>
+                <span className={styles.pill}>{t('app-portal.heading')}</span>
+                <p className={styles.cardLead}>{t('app-portal.description')}</p>
+              </div>
+              <AppPortalSection />
+            </section>
+
+            <section className={styles.featureCard}>
+              <div className={styles.cardHeading}>
+                <span className={styles.pill}>{t('api-docs.title')}</span>
+                <p className={styles.cardLead}>{t('api-docs.sub-header')}</p>
+              </div>
+              <ApiDocsSection />
+            </section>
+          </div>
+
+          <section className={styles.supportSection}>
+            <h2 className={styles.sectionTitle}>{t('header')}</h2>
+            <div className={styles.featureCard}>
+              <CommunitySection />
+            </div>
+          </section>
+
+          <section className={styles.projectsSection}>
+            <h2 className={styles.sectionTitle}>{t('common:our-projects')}</h2>
+            <p className={styles.sectionSubtitle}>
               <Trans
-                i18nKey="developers:projects.q-next"
+                i18nKey="developers:projects.all"
                 components={[
-                  <a
-                    key={0}
-                    href="https://github.com/quran/quran.com-frontend-next"
-                    target="_blank"
-                    rel="noreferrer"
-                  />,
+                  <a key={0} href="https://github.com/quran" target="_blank" rel="noreferrer" />,
                 ]}
               />
             </p>
+            <div className={styles.projectGrid}>
+              <p>
+                <Trans
+                  i18nKey="developers:projects.q-next"
+                  components={[
+                    <a
+                      key={0}
+                      href="https://github.com/quran/quran.com-frontend-next"
+                      target="_blank"
+                      rel="noreferrer"
+                    />,
+                  ]}
+                />
+              </p>
+              <p>
+                <Trans
+                  i18nKey="developers:projects.q-api"
+                  components={[
+                    <a
+                      key={0}
+                      href="https://github.com/quran/quran.com-api"
+                      target="_blank"
+                      rel="noreferrer"
+                    />,
+                  ]}
+                />
+              </p>
+              <p>
+                <Trans
+                  i18nKey="developers:projects.q-android"
+                  components={[
+                    <a
+                      key={0}
+                      href="https://github.com/quran/quran_android"
+                      target="_blank"
+                      rel="noreferrer"
+                    />,
+                  ]}
+                />
+              </p>
+              <p>
+                <Trans
+                  i18nKey="developers:projects.q-ios"
+                  components={[
+                    <a
+                      key={0}
+                      href="https://github.com/quran/quran-ios"
+                      target="_blank"
+                      rel="noreferrer"
+                    />,
+                  ]}
+                />
+              </p>
+              <p>
+                <Trans
+                  i18nKey="developers:projects.q-audio"
+                  components={[
+                    <a
+                      key={0}
+                      href="https://github.com/quran/audio.quran.com"
+                      target="_blank"
+                      rel="noreferrer"
+                    />,
+                    <a
+                      key={1}
+                      href="https://github.com/quran/quranicaudio-app"
+                      target="_blank"
+                      rel="noreferrer"
+                    />,
+                  ]}
+                />
+              </p>
+            </div>
+          </section>
+
+          <section className={styles.callout}>
             <p>
               <Trans
-                i18nKey="developers:projects.q-api"
+                i18nKey="developers:issues-guide"
                 components={[
                   <a
                     key={0}
-                    href="https://github.com/quran/quran.com-api"
-                    target="_blank"
-                    rel="noreferrer"
-                  />,
-                ]}
-              />
-            </p>
-            <p>
-              <Trans
-                i18nKey="developers:projects.q-android"
-                components={[
-                  <a
-                    key={0}
-                    href="https://github.com/quran/quran_android"
-                    target="_blank"
-                    rel="noreferrer"
-                  />,
-                ]}
-              />
-            </p>
-            <p>
-              <Trans
-                i18nKey="developers:projects.q-ios"
-                components={[
-                  <a
-                    key={0}
-                    href="https://github.com/quran/quran-ios"
-                    target="_blank"
-                    rel="noreferrer"
-                  />,
-                ]}
-              />
-            </p>
-            <p>
-              <Trans
-                i18nKey="developers:projects.q-audio"
-                components={[
-                  <a
-                    key={0}
-                    href="https://github.com/quran/audio.quran.com"
+                    href="https://github.com/quran/quran.com-frontend-next/pulls"
                     target="_blank"
                     rel="noreferrer"
                   />,
                   <a
                     key={1}
-                    href="https://github.com/quran/quranicaudio-app"
+                    href="https://github.com/quran/quran.com-frontend-next/pulls"
                     target="_blank"
                     rel="noreferrer"
                   />,
                 ]}
               />
             </p>
-          </div>
-          <p>
-            <Trans
-              i18nKey="developers:issues-guide"
-              components={[
-                <a
-                  key={0}
-                  href="https://github.com/quran/quran.com-frontend-next/pulls"
-                  target="_blank"
-                  rel="noreferrer"
-                />,
-                <a
-                  key={1}
-                  href="https://github.com/quran/quran.com-frontend-next/pulls"
-                  target="_blank"
-                  rel="noreferrer"
-                />,
-              ]}
-            />
-          </p>
-          <p>{t('issues-cta')}</p>
-          <p>{t('thanks')}</p>
-          <p>{t('footer')}</p>
+            <p className={styles.sectionSubtitle}>{t('issues-cta')}</p>
+            <p className={styles.footerNote}>{t('footer')}</p>
+          </section>
         </div>
       </PageContainer>
     </>
