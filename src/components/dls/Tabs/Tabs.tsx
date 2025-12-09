3d8073c0a4adf8aa5971ce2141eadebe71f5d90d
afifvdin <afifudin.fdn@gmail.com>
2025-12-09 15:58:55 +0700

    [QF-4113] fix reflection modal/dialog issue

--- src/components/dls/Tabs/Tabs.tsx
+++ src/components/dls/Tabs/Tabs.tsx
@@ -14,22 +14,32 @@ type TabsProps = {
   tabs: Tab[];
   selected: string;
   onSelect?: (value: string) => void;
+  containerClassName?: string;
   className?: string;
   activeClassName?: string;
 };
 
 // TODO: move this to Radix UI Tabs component
-const Tabs = ({ tabs, onSelect, selected, className, activeClassName }: TabsProps) => {
+const Tabs = ({
+  tabs,
+  onSelect,
+  selected,
+  className,
+  activeClassName,
+  containerClassName,
+}: TabsProps) => {
   return (
-    <div className={styles.container} role="tablist">
+    <div
+      className={classNames(styles.container, { [containerClassName]: containerClassName })}
+      role="tablist"
+    >
       {tabs.map((tab) => (
         <div
-          className={classNames(
-            className,
-            styles.tabItem,
-            selected === tab.value && activeClassName,
-            selected === tab.value && !activeClassName && styles.tabItemSelected,
-          )}
+          className={classNames(styles.tabItem, {
+            [className]: className,
+            [activeClassName]: selected === tab.value,
+            [styles.tabItemSelected]: selected === tab.value && !activeClassName,
+          })}
           key={tab.value}
           role="tab"
           tabIndex={0}
