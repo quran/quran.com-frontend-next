cedc65e6145213f2e25a871075de18806c247fd5
al-imam <alimam01828@gmail.com>
2025-12-18 00:49:16 +0600

    feat: add button when ayah already had notes

--- src/components/Notes/modal/NoteModal.tsx
+++ src/components/Notes/modal/NoteModal.tsx
@@ -7,9 +7,13 @@ import styles from './NoteModal.module.scss';
 import ReflectionIntro from '@/components/Notes/modal/ReflectionIntro';
 import Button, { ButtonSize, ButtonVariant } from '@/dls/Button/Button';
 import TextArea from '@/dls/Forms/TextArea';
+import IconContainer, { IconColor, IconSize } from '@/dls/IconContainer/IconContainer';
+import ChevronRightIcon from '@/icons/chevron-right.svg';
+import NotesWithPencilIcon from '@/icons/notes-with-pencil.svg';
 
 const NoteModal = () => {
   const { t } = useTranslation('notes');
+  const count = 5;
 
   const [noteInput, setNoteInput] = useState('');
   const errors: Record<string, string> = {};
@@ -30,6 +34,24 @@ const NoteModal = () => {
 
         {errors.note && <div className={styles.error}>{errors.note}</div>}
       </div>
+
+      {count > 0 && (
+        <button type="button" className={styles.notesButton}>
+          <IconContainer
+            icon={<NotesWithPencilIcon />}
+            color={IconColor.default}
+            size={IconSize.Custom}
+            className={styles.notesButtonPencil}
+          />
+          <span className={styles.notesButtonText}>{t('save-post-to-qr')}</span>
+          <IconContainer
+            icon={<ChevronRightIcon />}
+            color={IconColor.default}
+            size={IconSize.Custom}
+            className={styles.notesButtonChevron}
+          />
+        </button>
+      )}
 
       <div className={styles.actions}>
         <Button
