e73445e39fc63384d2cebc20a3fe70735beaba79
al-imam <alimam01828@gmail.com>
2025-12-28 20:23:08 +0600

    feat: add AddNoteModal and EditNoteModal components for note management
    - Introduced AddNoteModal for creating new notes with validation and toast notifications.
    - Added EditNoteModal for editing existing notes, including error handling and cache invalidation.
    - Implemented utility functions for note response handling and cache management.
    - Enhanced localization support for new modal components and actions.

---
+++ src/components/Notes/modal/EditNoteModal.tsx
@@ -0,0 +1,102 @@
+import { useContext } from 'react';
+
+import classNames from 'classnames';
+import useTranslation from 'next-translate/useTranslation';
+import { useSWRConfig } from 'swr';
+
+import modalStyles from './Modal.module.scss';
+
+import NoteFormModal from '@/components/Notes/modal/NoteFormModal';
+import {
+  getNoteFromResponse,
+  invalidateCache,
+  isNotePublishFailed,
+} from '@/components/Notes/modal/utility';
+import DataContext from '@/contexts/DataContext';
+import IconContainer, { IconSize } from '@/dls/IconContainer/IconContainer';
+import { ToastStatus, useToast } from '@/dls/Toast/Toast';
+import ArrowIcon from '@/icons/arrow.svg';
+import { Note } from '@/types/auth/Note';
+import { updateNote } from '@/utils/auth/api';
+import { verseRangesToVerseKeys } from '@/utils/verseKeys';
+
+interface EditNoteModalProps {
+  note: Note;
+  onMyNotes: () => void;
+  isModalOpen: boolean;
+  onModalClose: () => void;
+}
+
+const EditNoteModal: React.FC<EditNoteModalProps> = ({
+  note,
+  onMyNotes,
+  isModalOpen,
+  onModalClose,
+}) => {
+  const chaptersData = useContext(DataContext);
+  const { t } = useTranslation('notes');
+  const toast = useToast();
+  const { mutate, cache } = useSWRConfig();
+
+  const handleSaveNote = async ({
+    note: noteBody,
+    isPublic,
+  }: {
+    note: string;
+    isPublic: boolean;
+  }) => {
+    try {
+      const data = await updateNote(note.id, noteBody, isPublic);
+
+      if (isNotePublishFailed(data)) {
+        toast(t('notes:update-publish-failed'), { status: ToastStatus.Error });
+      } else {
+        toast(t('notes:update-success'), { status: ToastStatus.Success });
+      }
+
+      invalidateCache({
+        mutate,
+        cache,
+        verseKeys: note.ranges ? verseRangesToVerseKeys(chaptersData, note.ranges) : [],
+        note: getNoteFromResponse(data),
+        invalidateCount: true,
+      });
+    } catch (error) {
+      toast(t('common:error.general'), { status: ToastStatus.Error });
+      throw error;
+    }
+  };
+
+  return (
+    <NoteFormModal
+      key={JSON.stringify(note ?? {})}
+      initialNote={note?.body || ''}
+      ranges={note?.ranges}
+      isModalOpen={isModalOpen}
+      onModalClose={onModalClose}
+      onMyNotes={onMyNotes}
+      onSaveNote={handleSaveNote}
+      showNotesOnVerseButton={false}
+      dataTestId="edit-note-modal-content"
+      header={
+        <button
+          type="button"
+          className={classNames(modalStyles.headerButton, modalStyles.title)}
+          onClick={onMyNotes}
+          data-testid="edit-modal-back-button"
+        >
+          <IconContainer
+            icon={<ArrowIcon />}
+            shouldForceSetColors={false}
+            size={IconSize.Custom}
+            className={modalStyles.arrowIcon}
+          />
+
+          {t('edit-note')}
+        </button>
+      }
+    />
+  );
+};
+
+export default EditNoteModal;
---
+++ src/components/Notes/modal/utility.ts
@@ -0,0 +1,66 @@
+import { ScopedMutator, Cache } from 'swr/dist/types';
+
+import { Note } from '@/types/auth/Note';
+import { makeGetNoteByIdUrl, makeGetNotesByVerseUrl, makeNotesUrl } from '@/utils/auth/apiPaths';
+import { isVerseKeyWithinRanges } from '@/utils/verse';
+
+export const isNotePublishFailed = (data: unknown) => {
+  return data instanceof Object && 'error' in data && data.error === true;
+};
+
+export const getNoteFromResponse = (data: unknown): Note | undefined => {
+  if (typeof data !== 'object' || data === null) {
+    return undefined;
+  }
+
+  const note = data as Note & { note?: Note };
+  if (isNotePublishFailed(note)) return note.note;
+  return note;
+};
+
+export const invalidateCache = ({
+  mutate,
+  cache,
+  verseKeys,
+  note,
+  invalidateCount = false,
+}: {
+  mutate: ScopedMutator<unknown>;
+  cache?: Cache<unknown>;
+  verseKeys?: string[];
+  note?: Note;
+  invalidateCount?: boolean;
+}) => {
+  const cacheKeys = (cache as unknown as { keys: () => string[] })?.keys();
+  const uniqueVerseKeys = verseKeys ? Array.from(new Set(verseKeys)) : [];
+
+  if (invalidateCount) {
+    if (cacheKeys) {
+      const keys = [...cacheKeys].filter((key) => {
+        if (!key.startsWith('countNotes/')) {
+          return false;
+        }
+
+        if (uniqueVerseKeys.length > 0) {
+          const rangeString = key.replace('countNotes/', '');
+          return uniqueVerseKeys.some((verseKey) => isVerseKeyWithinRanges(verseKey, rangeString));
+        }
+
+        return true;
+      });
+
+      keys.forEach((key) => mutate(key, undefined, { revalidate: true }));
+    } else {
+      const countKeys = uniqueVerseKeys.map((key) => `countNotes/${key}-${key}`);
+      countKeys.forEach((key) => mutate(key, undefined, { revalidate: true }));
+    }
+  }
+
+  if (uniqueVerseKeys.length > 0) {
+    const updatedKeys = uniqueVerseKeys.map((key) => makeGetNotesByVerseUrl(key));
+    updatedKeys.forEach((key) => mutate(key, undefined, { revalidate: true }));
+  }
+
+  if (note) mutate(makeGetNoteByIdUrl(note.id), undefined, { revalidate: true });
+  mutate(makeNotesUrl(), undefined, { revalidate: true });
+};
