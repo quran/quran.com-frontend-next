quran/quran.com-frontend-next

commit afc6fbcb571aba66bda35e6e03f8c1e8d6eabad4

[QF-4113] fix reflection modal/dialog issue (#2646)
* [QF-4113] fix reflection modal/dialog issue

* checkpoint

* [QF-4113] remove important

* [QF-4113] fix padding

* [QF-4113] fix classname priority

* fix reflection page

* restore overflow

* fix layout

* [QF-4113] fix css

src/components/QuranReader/TranslationView/BottomActionsModals.module.scss
@@ -1,0 +1,30 @@
+@use 'src/styles/breakpoints';
+
+.reflectionOverlay .reflectionContentModal {
+  padding: 0;
+  .reflectionHeader {
+    border-radius: var(--border-radius-rounded);
+    padding-inline: var(--spacing-medium-px);
+    padding-block: var(--spacing-small-px);
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+    align-items: center;
+
+    @include breakpoints.tablet {
+      padding-inline: var(--spacing-large-px);
+      padding-block: var(--spacing-medium-px);
+    }
+
+    .reflectionHeaderCloseIcon {
+      position: relative;
+      inset-block: unset;
+      inset-inline: unset;
+      transform: unset;
+    }
+  }
+  // Remove default padding to allow full-width tabs in reflection modal
+  .reflectionInnerContentModal {
+    padding-inline: 0;
+  }
+}

src/components/dls/ContentModal/ContentModal.module.scss
@@ -170,7 +170,6 @@ $translateYDistance: 200px;
     border: none;
     padding-block: 0;
   }
-  border-block-end: 1px solid var(--color-borders-hairline);
 }
 
 .content {

src/components/QuranReader/ReflectionView/ReflectionBodyContainer/ReflectionBody/ReflectionBody.module.scss
@@ -1,3 +1,5 @@
+@use 'src/styles/breakpoints';
+
 .separatorContainer {
   margin-block: var(--spacing-large);
 }

src/components/QuranReader/ReflectionView/ReflectionBodyContainer/ReflectionBody/index.tsx
@@ -85,9 +85,7 @@ const ReflectionBody: React.FC<Props> = ({
 
   return (
     <div className={styles.container}>
-      {isModal ? (
-        <div className={styles.separatorContainer} />
-      ) : (
+      {!isModal && (
         <>
           <VerseAndTranslation
             from={Number(selectedVerseNumber)}

src/components/QuranReader/ReflectionView/ReflectionBodyContainer/ReflectionBodyContainer.module.scss
@@ -1,3 +1,45 @@
+@use 'src/styles/breakpoints';
+
+.reflectionDataContainer {
+  padding-block-start: var(--spacing-small-px);
+  padding-inline: var(--spacing-medium-px);
+  @include breakpoints.tablet {
+    padding-block-start: var(--spacing-medium-px);
+    padding-inline: var(--spacing-large-px);
+  }
+}
+
+.tabsContainerWrapper .tabsContainer {
+  border-block-end: unset;
+
+  .tab {
+    padding-inline: var(--spacing-xxlarge-px);
+    padding-block: var(--spacing-medium-px);
+    inline-size: 100%;
+    font-weight: var(--font-weight-semibold);
+    color: var(--color-text-faded-new);
+    border-block-end: var(--spacing-micro-px) solid var(--color-borders-hairline);
+
+    svg {
+      inline-size: var(--spacing-medium-plus-px);
+      block-size: var(--spacing-medium-plus-px);
+      @include breakpoints.tablet {
+        inline-size: var(--spacing-medium2-px);
+        block-size: var(--spacing-medium2-px);
+      }
+    }
+  }
+
+  .tab + .tab {
+    margin-inline-start: unset;
+  }
+
+  .tabActive {
+    color: var(--color-success-medium);
+    border-block-end-color: var(--color-success-medium);
+  }
+}
+
 .titleContainer {
   display: flex;
   align-items: center;
@@ -5,17 +47,9 @@
   gap: var(--spacing-micro-px);
 }
 
-.tab {
-  inline-size: 100%;
-  font-weight: var(--font-weight-semibold);
-  color: var(--color-text-faded-new);
-  svg {
-    inline-size: var(--spacing-medium2);
-    block-size: var(--spacing-medium2);
+.modalTitleContainer {
+  font-size: var(--font-size-xsmall-px);
+  @include breakpoints.tablet {
+    font-size: var(--font-size-normal-px);
   }
-}
-
-.tabActive {
-  color: var(--color-success-medium);
-  border-block-end: 2px solid var(--color-success-medium);
 }

src/components/QuranReader/ReflectionView/ReflectionBodyContainer/ReflectionSurahAndAyahSelection/ReflectionSurahAndAyahSelection.module.scss
@@ -1,4 +1,4 @@
-@use "src/styles/breakpoints";
+@use 'src/styles/breakpoints';
 
 .surahSelectionContainer {
   @include breakpoints.tablet {

src/components/QuranReader/ReflectionView/ReflectionBodyContainer/ReflectionSurahAndAyahSelection/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import useTranslation from 'next-translate/useTranslation';
 
-import styles from './ReflectionSurahAndAyahSelection.module.scss';
-
 import SurahAndAyahSelection from '@/components/QuranReader/TafsirView/SurahAndAyahSelection';
 import { logItemSelectionChange } from '@/utils/eventLogger';
 import { fakeNavigate, getReflectionNavigationUrl } from '@/utils/navigation';
@@ -44,14 +42,12 @@ const ReflectionSurahAndAyahSelection: React.FC<Props> = ({
   };
 
   return (
-    <div className={styles.surahSelectionContainer}>
-      <SurahAndAyahSelection
-        selectedChapterId={selectedChapterId}
-        selectedVerseNumber={selectedVerseNumber}
-        onChapterIdChange={onChapterIdChange}
-        onVerseNumberChange={onVerseNumberChange}
-      />
-    </div>
+    <SurahAndAyahSelection
+      selectedChapterId={selectedChapterId}
+      selectedVerseNumber={selectedVerseNumber}
+      onChapterIdChange={onChapterIdChange}
+      onVerseNumberChange={onVerseNumberChange}
+    />
   );
 };
 

src/components/QuranReader/ReflectionView/ReflectionBodyContainer/helpers.tsx
@@ -1,3 +1,5 @@
+import classNames from 'classnames';
+
 import styles from './ReflectionBodyContainer.module.scss';
 
 import { Tab } from '@/dls/Tabs/Tabs';
@@ -8,10 +10,10 @@ import { isLoggedIn } from '@/utils/auth/login';
 import { logPostView } from '@/utils/quranReflect/apiPaths';
 import ContentType from 'types/QuranReflect/ContentType';
 
-export const getReflectionTabs = (t: (key: string) => string): Tab[] => [
+export const getReflectionTabs = (t: (key: string) => string, isModal: boolean): Tab[] => [
   {
     title: (
-      <div className={styles.titleContainer}>
+      <div className={classNames(styles.titleContainer, { [styles.modalTitleContainer]: isModal })}>
         <ChatIcon />
         {t('common:reflections')}
       </div>
@@ -20,7 +22,7 @@ export const getReflectionTabs = (t: (key: string) => string): Tab[] => [
   },
   {
     title: (
-      <div className={styles.titleContainer}>
+      <div className={classNames(styles.titleContainer, { [styles.modalTitleContainer]: isModal })}>
         <LearningPlanIcon />
         {t('common:lessons')}
       </div>

commit 20b3531578dee74390bce55c9ba886267a8498e7

[QF-4339] Fix Surah Info Modal Spacing & Colors  (#2726)
* fix: modal color and spacing

* fix: update line-height variable in SurahInfoModal styles

* fix: standardize spacing variables in SurahInfoModal styles

* fix(ui): resolve regression introduced in #2646

https://github.com/quran/quran.com-frontend-next/pull/2646/changes#diff-30031f30262b9b8e69ab331c4c3be6ad7f7a48b109d15c916ec9918b8071b7c8

src/components/QuranReader/TranslationView/BottomActionsModals.module.scss
@@ -10,6 +10,7 @@
     flex-direction: row-reverse;
     justify-content: space-between;
     align-items: center;
+    border-block-end: none;
 
     @include breakpoints.tablet {
       padding-inline: var(--spacing-large-px);

src/components/chapters/ChapterHeader/ChapterHeader.module.scss
@@ -246,9 +246,30 @@ $bismillahSvgInlineSizeMobile: 148px;
   min-block-size: calc(3.75 * var(--spacing-medium));
 }
 
-.surahInfoHeader {
-  font-size: var(--font-size-large2);
+.surahInfoTitle {
   font-weight: var(--font-weight-bold);
+  color: var(--color-text-faded-new);
+  font-size: var(--font-size-large2);
+}
+
+.surahInfoContent {
+  @include breakpoints.smallerThanTablet {
+    overflow-y: auto;
+  }
+}
+
+// Need div to get higher specificity
+div {
+  &.surahInfoHeader {
+    @include breakpoints.smallerThanTablet {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      block-size: calc(2 * var(--spacing-mega));
+      padding-block: 0;
+    }
+  }
 }
 
 .chapterIconContainer {

src/components/chapters/ChapterHeader/components/SurahInfoButton.tsx
@@ -70,7 +70,9 @@ const SurahInfoButton: React.FC<SurahInfoButtonProps> = ({ chapterId, className 
           onClose={handleClose}
           onEscapeKeyDown={handleClose}
           hasCloseButton
-          header={<div className={styles.surahInfoHeader}>{t('surah-info')}</div>}
+          header={<div className={styles.surahInfoTitle}>{t('surah-info')}</div>}
+          headerClassName={styles.surahInfoHeader}
+          contentClassName={styles.surahInfoContent}
           size={ContentModalSize.MEDIUM}
         >
           <SurahInfoModal chapterId={chapterId} />

src/components/chapters/Info/SurahInfoModal.module.scss
@@ -2,6 +2,7 @@
 
 $calligraphy-offset: -0.875rem;
 $container-min-height: 200px;
+$line-height-relaxed: 1.6;
 
 .headerContainer {
   display: flex;
@@ -10,10 +11,15 @@ $container-min-height: 200px;
   padding-block: var(--spacing-mega);
   margin-block-end: var(--spacing-mega);
   border-block-end: 1px solid var(--color-borders-hairline);
+
+  @include breakpoints.smallerThanTablet {
+    padding-block-start: calc(2 * var(--spacing-small));
+    margin-block-end: calc(2 * var(--spacing-small));
+  }
 }
 
 .calligraphyContainer {
-  max-width: max-content;
+  max-inline-size: max-content;
 
   // The calligraphy SVG has a lot of whitespace on the left side in LTR, so we shift it left to visually center it.
   // If we applied this transform in RTL, it would shift the calligraphy further to the right, increasing whitespace on the right
@@ -26,10 +32,12 @@ $container-min-height: 200px;
 .surahName {
   font-size: var(--font-size-large2);
   font-weight: var(--font-weight-bold);
+  color: var(--color-text-default-new);
 }
 
 .detailsContainer {
   display: flex;
+  color: var(--color-text-default-new);
   font-size: var(--font-size-normal);
   gap: var(--spacing-medium);
 }
@@ -43,22 +51,31 @@ $container-min-height: 200px;
 
 .detailHeader {
   font-weight: var(--font-weight-bold);
-  color: var(--color-text-secondary);
 }
 
 .descriptionContainer {
+  color: var(--color-text-default-new);
+
+  p,
+  h2,
+  h3,
+  li,
+  em {
+    line-height: $line-height-relaxed;
+  }
+
   @include breakpoints.smallerThanTablet {
     margin-block-end: var(--spacing-max);
   }
 
   p {
-    margin-block-end: var(--spacing-small);
+    margin-block-end: var(--spacing-medium3-px);
   }
 
   h2,
   h3 {
     font-weight: var(--font-weight-bold);
-    margin-block-end: var(--spacing-small);
+    margin-block-end: var(--spacing-medium3-px);
   }
 
   em {
@@ -74,7 +91,6 @@ $container-min-height: 200px;
 
   li {
     display: list-item;
-    line-height: normal;
     margin-block-end: var(--spacing-xxsmall);
   }
 }
@@ -84,7 +100,7 @@ $container-min-height: 200px;
   display: flex;
   justify-content: center;
   align-items: center;
-  min-height: $container-min-height;
+  min-block-size: $container-min-height;
   padding-block: var(--spacing-large);
 }
 

src/components/chapters/Info/SurahInfoPage.tsx
@@ -28,7 +28,9 @@ const SurahInfoPage: React.FC<SurahInfoPageProps> = ({ chapterInfo, chapter }) =
       isFakeSEOFriendlyMode
       onClose={handleClose}
       hasCloseButton
-      header={<div className={styles.surahInfoHeader}>{t('surah-info')}</div>}
+      header={<div className={styles.surahInfoTitle}>{t('surah-info')}</div>}
+      headerClassName={styles.surahInfoHeader}
+      contentClassName={styles.surahInfoContent}
       size={ContentModalSize.MEDIUM}
     >
       <SurahInfoContent

src/components/dls/ContentModal/ContentModal.module.scss
@@ -162,6 +162,7 @@ $translateYDistance: 200px;
   z-index: 1;
   padding-inline: var(--spacing-small);
   padding-inline-end: var(--spacing-xlarge-px);
+  border-block-end: var(--spacing-hairline-px) solid var(--color-borders-hairline);
 
   @include breakpoints.tablet {
     position: relative;