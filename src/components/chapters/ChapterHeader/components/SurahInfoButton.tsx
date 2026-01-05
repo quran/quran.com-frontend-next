quran/quran.com-frontend-next

commit afc6fbcb571aba66bda35e6e03f8c1e8d6eabad4

[QF-4113] fix reflection modal/dialog issue (#2646)
* [QF-4113] fix reflection modal/dialog issue

* checkpoint

* [QF-4113] remove important

* [QF-4113] fix padding

* [QF-4113] fix classname priority

* fix reflection page

* restore overflow

* fix layout

* [QF-4113] fix css

src/components/dls/ContentModal/ContentModal.module.scss
@@ -170,7 +170,6 @@ $translateYDistance: 200px;
     border: none;
     padding-block: 0;
   }
-  border-block-end: 1px solid var(--color-borders-hairline);
 }
 
 .content {

src/components/dls/ContentModal/ContentModal.tsx
@@ -28,8 +28,9 @@ type ContentModalProps = {
   header?: React.ReactNode;
   innerRef?: ForwardedRef<ContentModalHandles>;
   onClick?: (e: React.MouseEvent) => void;
-  contentClassName?: string;
   overlayClassName?: string;
+  contentClassName?: string;
+  innerContentClassName?: string;
   closeIconClassName?: string;
   headerClassName?: string;
   size?: ContentModalSize;
@@ -49,8 +50,9 @@ const ContentModal = ({
   children,
   header,
   innerRef,
-  contentClassName,
   overlayClassName,
+  contentClassName,
+  innerContentClassName,
   closeIconClassName,
   headerClassName,
   size = ContentModalSize.MEDIUM,
@@ -123,8 +125,7 @@ const ContentModal = ({
           <Dialog.Content
             {...(onClick && { onClick })}
             ref={contentRef}
-            className={classNames(styles.contentWrapper, {
-              [contentClassName]: contentClassName,
+            className={classNames(styles.contentWrapper, contentClassName, {
               [styles.small]: size === ContentModalSize.SMALL,
               [styles.medium]: size === ContentModalSize.MEDIUM,
               [styles.autoHeight]: !isFixedHeight,
@@ -151,7 +152,10 @@ const ContentModal = ({
               </div>
             )}
 
-            <div className={styles.content} data-testid="modal-content">
+            <div
+              className={classNames(styles.content, innerContentClassName)}
+              data-testid="modal-content"
+            >
               {children}
             </div>
           </Dialog.Content>

src/components/QuranReader/ReflectionView/ReflectionBodyContainer/ReflectionBody/ReflectionBody.module.scss
@@ -1,3 +1,5 @@
+@use 'src/styles/breakpoints';
+
 .separatorContainer {
   margin-block: var(--spacing-large);
 }

src/components/QuranReader/ReflectionView/ReflectionBodyContainer/ReflectionBody/index.tsx
@@ -85,9 +85,7 @@ const ReflectionBody: React.FC<Props> = ({
 
   return (
     <div className={styles.container}>
-      {isModal ? (
-        <div className={styles.separatorContainer} />
-      ) : (
+      {!isModal && (
         <>
           <VerseAndTranslation
             from={Number(selectedVerseNumber)}

src/components/QuranReader/ReflectionView/ReflectionBodyContainer/ReflectionBodyContainer.module.scss
@@ -1,3 +1,45 @@
+@use 'src/styles/breakpoints';
+
+.reflectionDataContainer {
+  padding-block-start: var(--spacing-small-px);
+  padding-inline: var(--spacing-medium-px);
+  @include breakpoints.tablet {
+    padding-block-start: var(--spacing-medium-px);
+    padding-inline: var(--spacing-large-px);
+  }
+}
+
+.tabsContainerWrapper .tabsContainer {
+  border-block-end: unset;
+
+  .tab {
+    padding-inline: var(--spacing-xxlarge-px);
+    padding-block: var(--spacing-medium-px);
+    inline-size: 100%;
+    font-weight: var(--font-weight-semibold);
+    color: var(--color-text-faded-new);
+    border-block-end: var(--spacing-micro-px) solid var(--color-borders-hairline);
+
+    svg {
+      inline-size: var(--spacing-medium-plus-px);
+      block-size: var(--spacing-medium-plus-px);
+      @include breakpoints.tablet {
+        inline-size: var(--spacing-medium2-px);
+        block-size: var(--spacing-medium2-px);
+      }
+    }
+  }
+
+  .tab + .tab {
+    margin-inline-start: unset;
+  }
+
+  .tabActive {
+    color: var(--color-success-medium);
+    border-block-end-color: var(--color-success-medium);
+  }
+}
+
 .titleContainer {
   display: flex;
   align-items: center;
@@ -5,17 +47,9 @@
   gap: var(--spacing-micro-px);
 }
 
-.tab {
-  inline-size: 100%;
-  font-weight: var(--font-weight-semibold);
-  color: var(--color-text-faded-new);
-  svg {
-    inline-size: var(--spacing-medium2);
-    block-size: var(--spacing-medium2);
+.modalTitleContainer {
+  font-size: var(--font-size-xsmall-px);
+  @include breakpoints.tablet {
+    font-size: var(--font-size-normal-px);
   }
-}
-
-.tabActive {
-  color: var(--color-success-medium);
-  border-block-end: 2px solid var(--color-success-medium);
 }

src/components/QuranReader/ReflectionView/ReflectionBodyContainer/ReflectionSurahAndAyahSelection/ReflectionSurahAndAyahSelection.module.scss
@@ -1,4 +1,4 @@
-@use "src/styles/breakpoints";
+@use 'src/styles/breakpoints';
 
 .surahSelectionContainer {
   @include breakpoints.tablet {

src/components/QuranReader/ReflectionView/ReflectionBodyContainer/ReflectionSurahAndAyahSelection/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import useTranslation from 'next-translate/useTranslation';
 
-import styles from './ReflectionSurahAndAyahSelection.module.scss';
-
 import SurahAndAyahSelection from '@/components/QuranReader/TafsirView/SurahAndAyahSelection';
 import { logItemSelectionChange } from '@/utils/eventLogger';
 import { fakeNavigate, getReflectionNavigationUrl } from '@/utils/navigation';
@@ -44,14 +42,12 @@ const ReflectionSurahAndAyahSelection: React.FC<Props> = ({
   };
 
   return (
-    <div className={styles.surahSelectionContainer}>
-      <SurahAndAyahSelection
-        selectedChapterId={selectedChapterId}
-        selectedVerseNumber={selectedVerseNumber}
-        onChapterIdChange={onChapterIdChange}
-        onVerseNumberChange={onVerseNumberChange}
-      />
-    </div>
+    <SurahAndAyahSelection
+      selectedChapterId={selectedChapterId}
+      selectedVerseNumber={selectedVerseNumber}
+      onChapterIdChange={onChapterIdChange}
+      onVerseNumberChange={onVerseNumberChange}
+    />
   );
 };
 

src/components/QuranReader/ReflectionView/ReflectionBodyContainer/helpers.tsx
@@ -1,3 +1,5 @@
+import classNames from 'classnames';
+
 import styles from './ReflectionBodyContainer.module.scss';
 
 import { Tab } from '@/dls/Tabs/Tabs';
@@ -8,10 +10,10 @@ import { isLoggedIn } from '@/utils/auth/login';
 import { logPostView } from '@/utils/quranReflect/apiPaths';
 import ContentType from 'types/QuranReflect/ContentType';
 
-export const getReflectionTabs = (t: (key: string) => string): Tab[] => [
+export const getReflectionTabs = (t: (key: string) => string, isModal: boolean): Tab[] => [
   {
     title: (
-      <div className={styles.titleContainer}>
+      <div className={classNames(styles.titleContainer, { [styles.modalTitleContainer]: isModal })}>
         <ChatIcon />
         {t('common:reflections')}
       </div>
@@ -20,7 +22,7 @@ export const getReflectionTabs = (t: (key: string) => string): Tab[] => [
   },
   {
     title: (
-      <div className={styles.titleContainer}>
+      <div className={classNames(styles.titleContainer, { [styles.modalTitleContainer]: isModal })}>
         <LearningPlanIcon />
         {t('common:lessons')}
       </div>

commit 09f658ca38e1eab02688ffedfe72134f87c462d3

[QF-4338] Make Surah Info Header Sticky (#2733)
* fix: modal color and spacing

* feat: add drawer behavior on desktop for surah info modal

- fixed header should be sticky
- modal should be behave like a drawer on desktop

* fix: update line-height variable in SurahInfoModal styles

* fix: code comment scss styling improvement

* fix: added text to verify header is visible after scrolling

- Updated EditNoteModal to use a more specific key format based on note ID and updatedAt timestamp for better React reconciliation.
- Enhanced utility functions for handling API responses, including improved type safety and detailed documentation for note publish failure checks and cache invalidation processes.
- Added new functions to streamline cache invalidation for notes and their counts, improving performance and reliability.

* fix: standardize spacing variables in SurahInfoModal styles

* fix(ui): resolve regression introduced in #2646

https://github.com/quran/quran.com-frontend-next/pull/2646/changes#diff-30031f30262b9b8e69ab331c4c3be6ad7f7a48b109d15c916ec9918b8071b7c8

src/components/Course/CourseDetails/Tabs/MainDetails/MainDetails.module.scss
@@ -1,7 +1,7 @@
 .htmlDescription {
-    p + p,
-    ul + p,
-    ol + p {
-        margin-top: var(--spacing-large);
-    }
+  p + p,
+  ul + p,
+  ol + p {
+    margin-block-start: var(--spacing-large);
+  }
 }

src/components/Course/LessonView/Lesson.module.scss
@@ -62,11 +62,11 @@
 }
 
 .htmlContent {
-    p + p,
-    ul + p,
-    ol + p {
-        margin-top: var(--spacing-large);
-    }
+  p + p,
+  ul + p,
+  ol + p {
+    margin-block-start: var(--spacing-large);
+  }
 }
 
 .headerButtonsContainer {

src/components/Profile/DeleteAccountButton.module.scss
@@ -1,8 +1,8 @@
 .inputContainer {
-  width: 100%;
+  inline-size: 100%;
 }
 .deleteButton {
-  width: 100%;
+  inline-size: 100%;
 }
 
 .confirmationText {

src/components/Profile/EmailNotificationSettingsForm.module.scss
@@ -9,8 +9,8 @@
     margin: 0;
     outline: var(--spacing-hairline-px) solid var(--color-daily-progress);
     border: none;
-    height: var(--spacing-medium2-px);
-    width: var(--spacing-medium2-px);
+    block-size: var(--spacing-medium2-px);
+    inline-size: var(--spacing-medium2-px);
     border-radius: var(--border-radius-xsmall2-px);
 
     &[data-state='checked'] {
@@ -23,8 +23,8 @@
     .indicator {
       background-color: var(--color-daily-progress);
       border-radius: var(--border-radius-xsmall2-px);
-      height: var(--spacing-medium2-px);
-      width: var(--spacing-medium2-px);
+      block-size: var(--spacing-medium2-px);
+      inline-size: var(--spacing-medium2-px);
     }
   }
 }

src/components/Profile/EmailNotificationSettingsSkeleton.module.scss
@@ -11,8 +11,8 @@
 }
 
 .checkbox {
-  width: var(--spacing-medium2-px);
-  height: var(--spacing-medium2-px);
+  inline-size: var(--spacing-medium2-px);
+  block-size: var(--spacing-medium2-px);
   border-radius: var(--border-radius-xsmall2-px);
   flex-shrink: 0;
 }
@@ -25,14 +25,14 @@
 }
 
 .title {
-  height: var(--spacing-small-px);
-  width: 40%;
+  block-size: var(--spacing-small-px);
+  inline-size: 40%;
   border-radius: var(--border-radius-xsmall-px);
 }
 
 .description {
-  height: var(--font-size-xsmall-px);
-  width: 80%;
+  block-size: var(--font-size-xsmall-px);
+  inline-size: 80%;
   border-radius: var(--border-radius-xsmall-px);
 }
 
@@ -41,7 +41,7 @@
 }
 
 .button {
-  height: var(--spacing-medium3-px);
-  width: calc(var(--spacing-mega-px) + var(--spacing-medium2-px));
+  block-size: var(--spacing-medium3-px);
+  inline-size: calc(var(--spacing-mega-px) + var(--spacing-medium2-px));
   border-radius: var(--border-radius-small-px);
 }

src/components/Profile/SharedProfileStyles.module.scss
@@ -16,7 +16,7 @@
   }
 
   input {
-    height: auto;
+    block-size: auto;
   }
 }
 
@@ -34,10 +34,10 @@
     grid-template-columns: repeat(2, 1fr);
   }
   .formInputContainer {
-    width: 100%;
+    inline-size: 100%;
     .formInput {
       @include input-base-styles;
-      width: unset;
+      inline-size: unset;
     }
   }
 }
@@ -47,11 +47,11 @@
   flex-direction: column;
   gap: var(--spacing-medium-px);
   @include breakpoints.tablet {
-    max-width: calc(50% - calc(var(--spacing-medium-px) / 2));
+    max-inline-size: calc(50% - calc(var(--spacing-medium-px) / 2));
   }
   .passwordFormInput {
     @include input-base-styles;
-    height: calc(2 * var(--spacing-medium2-px));
+    block-size: calc(2 * var(--spacing-medium2-px));
   }
 }
 

src/components/chapters/ChapterHeader/components/SurahInfoButton.tsx
@@ -7,6 +7,7 @@ import useTranslation from 'next-translate/useTranslation';
 import styles from '../ChapterHeader.module.scss';
 
 import SurahInfoModal from '@/components/chapters/Info/SurahInfoModal';
+import surahInfoStyles from '@/components/chapters/Info/SurahInfoModal.module.scss';
 import ContentModal, { ContentModalSize } from '@/components/dls/ContentModal/ContentModal';
 import InfoIcon from '@/icons/info.svg';
 import { logButtonClick } from '@/utils/eventLogger';
@@ -72,7 +73,12 @@ const SurahInfoButton: React.FC<SurahInfoButtonProps> = ({ chapterId, className 
           hasCloseButton
           header={<div className={styles.surahInfoTitle}>{t('surah-info')}</div>}
           headerClassName={styles.surahInfoHeader}
-          contentClassName={styles.surahInfoContent}
+          contentClassName={classNames(
+            styles.surahInfoContent,
+            surahInfoStyles.bottomSheetOnDesktopContent,
+          )}
+          overlayClassName={surahInfoStyles.bottomSheetOnDesktopOverlay}
+          innerContentClassName={surahInfoStyles.bottomSheetOnDesktopInnerContent}
           size={ContentModalSize.MEDIUM}
         >
           <SurahInfoModal chapterId={chapterId} />

src/components/chapters/Info/SurahInfoContent.tsx
@@ -23,40 +23,45 @@ const SurahInfoContent: React.FC<SurahInfoContentProps> = ({ chapterId, chapterI
 
   return (
     <>
-      <div className={styles.headerContainer}>
-        <div className={styles.calligraphyContainer}>
-          <ChapterIconContainer
-            chapterId={chapterId}
-            size={ChapterIconsSize.XMega}
-            hasSurahPrefix={false}
-          />
+      <div className={styles.bottomSheetOnDesktopHeaderSeparator} />
+      <div className={styles.surahInfoModalContent} data-testid="surah-info-content">
+        <div className={styles.headerContainer}>
+          <div className={styles.calligraphyContainer}>
+            <ChapterIconContainer
+              chapterId={chapterId}
+              size={ChapterIconsSize.XMega}
+              hasSurahPrefix={false}
+            />
+          </div>
+
+          {!shouldHideTransliteration && (
+            <h2 className={styles.surahName} data-testid="surah-name">
+              {t('common:surah')} {chapter.transliteratedName}
+            </h2>
+          )}
+
+          <div className={styles.detailsContainer}>
+            <div className={styles.detailItem}>
+              <p className={styles.detailHeader}>{t('common:ayahs')}:</p>
+              <p data-testid="surah-number-of-ayahs">
+                {toLocalizedNumber(chapter.versesCount, lang)}
+              </p>
+            </div>
+            <div className={styles.detailItem}>
+              <p className={styles.detailHeader}>{t('surah-info:revelation-place')}:</p>
+              <p data-testid="surah-revelation-place">
+                {t(`surah-info:${chapter.revelationPlace}`)}
+              </p>
+            </div>
+          </div>
         </div>
 
-        {!shouldHideTransliteration && (
-          <h2 className={styles.surahName} data-testid="surah-name">
-            {t('common:surah')} {chapter.transliteratedName}
-          </h2>
-        )}
-
-        <div className={styles.detailsContainer}>
-          <div className={styles.detailItem}>
-            <p className={styles.detailHeader}>{t('common:ayahs')}:</p>
-            <p data-testid="surah-number-of-ayahs">
-              {toLocalizedNumber(chapter.versesCount, lang)}
-            </p>
-          </div>
-          <div className={styles.detailItem}>
-            <p className={styles.detailHeader}>{t('surah-info:revelation-place')}:</p>
-            <p data-testid="surah-revelation-place">{t(`surah-info:${chapter.revelationPlace}`)}</p>
-          </div>
-        </div>
+        <div
+          className={styles.descriptionContainer}
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{ __html: chapterInfo.text }}
+        />
       </div>
-
-      <div
-        className={styles.descriptionContainer}
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{ __html: chapterInfo.text }}
-      />
     </>
   );
 };

src/components/chapters/Info/SurahInfoModal.module.scss
@@ -8,12 +8,12 @@ $line-height-relaxed: 1.6;
   display: flex;
   flex-direction: column;
   gap: var(--spacing-xsmall);
-  padding-block: var(--spacing-mega);
+  padding-block-start: calc(2 * var(--spacing-small));
+  padding-block-end: var(--spacing-mega);
   margin-block-end: var(--spacing-mega);
-  border-block-end: 1px solid var(--color-borders-hairline);
+  border-block-end: var(--spacing-hairline-px) solid var(--color-borders-hairline);
 
   @include breakpoints.smallerThanTablet {
-    padding-block-start: calc(2 * var(--spacing-small));
     margin-block-end: calc(2 * var(--spacing-small));
   }
 }
@@ -64,6 +64,14 @@ $line-height-relaxed: 1.6;
     line-height: $line-height-relaxed;
   }
 
+  @include breakpoints.tablet {
+    /**
+    * Adds a small inline-end padding to create visual spacing
+    * between the content and the scrollbar.
+    */
+    padding-inline-end: var(--spacing-xxsmall);
+  }
+
   @include breakpoints.smallerThanTablet {
     margin-block-end: var(--spacing-max);
   }
@@ -93,6 +101,10 @@ $line-height-relaxed: 1.6;
     display: list-item;
     margin-block-end: var(--spacing-xxsmall);
   }
+
+  & > *:last-child {
+    margin-block-end: 0;
+  }
 }
 
 .loadingContainer,
@@ -106,4 +118,63 @@ $line-height-relaxed: 1.6;
 
 .errorContainer {
   color: var(--color-text-error);
+}
+
+div {
+  &.bottomSheetOnDesktopOverlay {
+    @include breakpoints.tablet {
+      overflow-y: hidden;
+      align-items: flex-end;
+      justify-items: center;
+      padding-block-start: 0;
+      padding-block-end: 0;
+    }
+  }
+}
+
+div {
+  &.bottomSheetOnDesktopContent {
+    @include breakpoints.tablet {
+      border-radius: var(--border-radius-rounded) var(--border-radius-rounded) 0 0;
+      block-size: 95vh;
+      display: flex;
+      flex-direction: column;
+      overflow: hidden;
+      margin-block-end: 0;
+      padding-block-end: 0;
+    }
+  }
+}
+
+div {
+  &.surahInfoModalContent {
+    @include breakpoints.tablet {
+      flex: 1;
+      min-block-size: 0;
+      overflow-y: auto;
+      padding-block-end: calc(1.5 * var(--spacing-mega));
+    }
+  }
+}
+
+.bottomSheetOnDesktopHeaderSeparator {
+  display: none;
+  @include breakpoints.tablet {
+    display: block;
+    padding-block-start: var(--spacing-medium3-px);
+    margin-block-end: var(--spacing-micro);
+    border-block-end: var(--spacing-hairline-px) solid var(--color-borders-hairline);
+  }
+}
+
+.bottomSheetOnDesktopInnerContent {
+  @include breakpoints.tablet {
+    /**
+    * Acts as a non-scrollable flex container so that
+    * scrollability is delegated to its inner content.
+    */
+    min-block-size: 0;
+    display: flex;
+    flex-direction: column;
+  }
 }

src/components/chapters/Info/SurahInfoModal.tsx
@@ -32,7 +32,7 @@ const SurahInfoModal: React.FC<SurahInfoModalProps> = ({ chapterId }) => {
   const chapterInfo = chapterInfoResponse?.chapterInfo;
 
   return (
-    <div>
+    <>
       {isLoading && (
         <div className={styles.loadingContainer}>
           <Spinner />
@@ -48,7 +48,7 @@ const SurahInfoModal: React.FC<SurahInfoModalProps> = ({ chapterId }) => {
       {chapterInfo && (
         <SurahInfoContent chapterId={chapterId} chapter={chapter} chapterInfo={chapterInfo} />
       )}
-    </div>
+    </>
   );
 };
 

src/components/chapters/Info/SurahInfoPage.tsx
@@ -1,7 +1,10 @@
 import React, { useCallback } from 'react';
 
+import classNames from 'classnames';
 import { useRouter } from 'next/router';
 import useTranslation from 'next-translate/useTranslation';
+
+import surahInfoStyles from './SurahInfoModal.module.scss';
 
 import styles from '@/components/chapters/ChapterHeader/ChapterHeader.module.scss';
 import SurahInfoContent from '@/components/chapters/Info/SurahInfoContent';
@@ -30,8 +33,13 @@ const SurahInfoPage: React.FC<SurahInfoPageProps> = ({ chapterInfo, chapter }) =
       hasCloseButton
       header={<div className={styles.surahInfoTitle}>{t('surah-info')}</div>}
       headerClassName={styles.surahInfoHeader}
-      contentClassName={styles.surahInfoContent}
+      contentClassName={classNames(
+        styles.surahInfoContent,
+        surahInfoStyles.bottomSheetOnDesktopContent,
+      )}
       size={ContentModalSize.MEDIUM}
+      overlayClassName={surahInfoStyles.bottomSheetOnDesktopOverlay}
+      innerContentClassName={surahInfoStyles.bottomSheetOnDesktopInnerContent}
     >
       <SurahInfoContent
         chapterId={String(chapter.id)}

src/components/dls/ContentModal/ContentModal.module.scss
@@ -154,6 +154,7 @@ $translateYDistance: 200px;
     inset-inline-end: var(--spacing-mega);
   }
 }
+
 .header {
   position: sticky;
   inset-block-start: 0;

src/components/dls/ContentModal/ContentModal.tsx
@@ -177,6 +177,7 @@ const ContentModal = ({
             onEscapeKeyDown={onEscapeKeyDown}
             onPointerDownOutside={onPointerDownOutside}
             onOpenAutoFocus={handleOpenAutoFocus}
+            data-testid="root-dialog"
           >
             {hasHeader && (
               <div className={classNames(styles.header, headerClassName)}>

tests/integration/content-display-and-reading/surah-reader/surah-header-and-navigation.spec.ts
@@ -44,6 +44,7 @@ test.describe('Surah Header Display', () => {
   );
 });
 
+// eslint-disable-next-line react-func/max-lines-per-function
 test.describe('Surah Navigation', () => {
   test(
     'Surah info button navigates to surah information page',
@@ -64,22 +65,49 @@ test.describe('Surah Navigation', () => {
     'Clicking surah info button opens modal with surah information',
     { tag: ['@slow', '@surah', '@modal', '@info'] },
     async ({ page }) => {
-      // Pre-calculate the surah info button locator
+      // Open the surah info modal
       const infoButton = page.getByTestId(TestId.SURAH_INFO_BUTTON);
-
-      // Click on the info button
       await infoButton.click();
 
-      // Verify modal is open and contains surah info content
-      await expect(page.getByTestId(TestId.MODAL_CONTENT)).toBeVisible();
-      await expect(page.getByText('Surah Info')).toBeVisible(); // Modal header
-
-      // Verify surah info content is inside the modal
+      // Get the modal content
       const modalContent = page.getByTestId(TestId.MODAL_CONTENT);
-      await expect(modalContent.getByTestId(TestId.SURAH_NAME)).toBeVisible();
-      await expect(modalContent.getByTestId(TestId.SURAH_NAME)).toContainText('Surah Al-Fatihah');
-      await expect(modalContent.getByTestId(TestId.SURAH_REVELATION_PLACE)).toContainText('Mecca');
-      await expect(modalContent.getByTestId(TestId.SURAH_NUMBER_OF_AYAHS)).toContainText('7');
+      await expect(modalContent).toBeVisible();
+
+      // Verify the modal header is visible
+      const modalHeader = page.getByText('Surah Info');
+      await expect(modalHeader).toBeVisible();
+
+      // Verify the surah name is visible
+      const surahName = modalContent.getByTestId(TestId.SURAH_NAME);
+      await expect(surahName).toBeVisible();
+      await expect(surahName).toContainText(`Surah ${chapter.transliteratedName}`);
+
+      // Verify the revelation place is visible
+      const revelationPlace = modalContent.getByTestId(TestId.SURAH_REVELATION_PLACE);
+      await expect(revelationPlace).toBeVisible();
+      await expect(revelationPlace).toContainText('Mecca');
+
+      // Verify the number of ayahs is visible
+      const numberOfAyahs = modalContent.getByTestId(TestId.SURAH_NUMBER_OF_AYAHS);
+      await expect(numberOfAyahs).toBeVisible();
+      await expect(numberOfAyahs).toContainText(chapter.versesCount.toString());
+
+      // Verify the surah info header is sticky while scrolling
+      const dialogContent = page.getByTestId(TestId.ROOT_DIALOG);
+      const surahInfoContent = page.getByTestId(TestId.SURAH_INFO_CONTENT);
+
+      // Find which element is scrollable and scroll it
+      const dialogScrollable = await dialogContent.evaluate(
+        (el) => el.scrollHeight > el.clientHeight,
+      );
+      const scrollableElement = dialogScrollable ? dialogContent : surahInfoContent;
+
+      await scrollableElement.evaluate((el) => {
+        // eslint-disable-next-line no-param-reassign
+        el.scrollTop = el.scrollHeight;
+      });
+
+      await expect(modalHeader).toBeVisible();
     },
   );
 });

tests/test-ids.ts
@@ -52,6 +52,7 @@ export enum TestId {
   LIGHT_BUTTON = 'light-button',
   LINES = 'lines',
   LISTEN_BUTTON = 'listen-button',
+  ROOT_DIALOG = 'root-dialog',
   MODAL_CONTENT = 'modal-content',
   MORE_RESULTS = 'more-results',
   NAVBAR = 'navbar',
@@ -89,6 +90,7 @@ export enum TestId {
   SIDEBAR_NAVIGATION = 'sidebar-navigation',
   SIGNUP_BUTTON = 'signup-button',
   SURAH_INFO_BUTTON = 'surah-info-button',
+  SURAH_INFO_CONTENT = 'surah-info-content',
   SURAH_NAME = 'surah-name',
   SURAH_NUMBER_OF_AYAHS = 'surah-number-of-ayahs',
   SURAH_REVELATION_PLACE = 'surah-revelation-place',