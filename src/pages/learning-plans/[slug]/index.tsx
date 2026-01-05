ddc2c138fa3294c516f93270e37b57c6a439b324
Mustafa J <mustafa.0x@gmail.com>
2026-01-04 19:50:04 -0800

    LPs: isr

--- src/pages/learning-plans/[slug]/index.tsx
+++ src/pages/learning-plans/[slug]/index.tsx
@@ -5,23 +5,20 @@ import useTranslation from 'next-translate/useTranslation';
 
 import styles from './courses.module.scss';
 
+import { fetcher } from '@/api';
 import CourseDetails from '@/components/Course/CourseDetails';
-import DataFetcher from '@/components/DataFetcher';
 import NextSeoWrapper from '@/components/NextSeoWrapper';
 import PageContainer from '@/components/PageContainer';
 import Spinner from '@/dls/Spinner/Spinner';
 import { logErrorToSentry } from '@/lib/sentry';
 import layoutStyles from '@/pages/index.module.scss';
 import { Course } from '@/types/auth/Course';
-import { getCourse, privateFetcher } from '@/utils/auth/api';
 import { makeGetCourseUrl } from '@/utils/auth/apiPaths';
 import { getAllChaptersData } from '@/utils/chapter';
 import { getLanguageAlternates } from '@/utils/locale';
 import { getCanonicalUrl, getCourseNavigationUrl } from '@/utils/navigation';
-import {
-  ONE_WEEK_REVALIDATION_PERIOD_SECONDS,
-  REVALIDATION_PERIOD_ON_ERROR_SECONDS,
-} from '@/utils/staticPageGeneration';
+import { REVALIDATION_PERIOD_ON_ERROR_SECONDS } from '@/utils/staticPageGeneration';
+import { getBasePath } from '@/utils/url';
 
 const Loading = () => (
   <div className={layoutStyles.loadingContainer}>
@@ -38,8 +35,9 @@ interface Props {
 const LearningPlanPage: NextPage<Props> = ({ course }) => {
   const { lang, t } = useTranslation('learn');
   const router = useRouter();
-  const { slug } = router.query;
-
+  if (router.isFallback) {
+    return <Loading />;
+  }
   const url = getCourseNavigationUrl(course.slug);
 
   return (
@@ -53,18 +51,7 @@ const LearningPlanPage: NextPage<Props> = ({ course }) => {
       <div className={layoutStyles.pageContainer}>
         <div className={styles.container}>
           <PageContainer>
-            <DataFetcher
-              loading={Loading}
-              queryKey={makeGetCourseUrl(slug as string)}
-              fetcher={privateFetcher}
-              render={
-                ((courseDetailsResponse: Course) => (
-                  <>
-                    <CourseDetails course={courseDetailsResponse} />
-                  </>
-                )) as any
-              }
-            />
+            <CourseDetails course={course} />
           </PageContainer>
         </div>
       </div>
@@ -77,16 +64,21 @@ export const getStaticPaths: GetStaticPaths = async () => ({
   fallback: 'blocking', // will server-render pages on-demand if the path doesn't exist.
 });
 
+// eslint-disable-next-line react-func/max-lines-per-function
 export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
   try {
-    const course = await getCourse(params.slug as string);
+    const course = await fetcher<Course>(makeGetCourseUrl(params.slug as string), {
+      headers: {
+        origin: getBasePath(),
+      },
+    });
     const chaptersData = await getAllChaptersData(locale);
     return {
       props: {
         course,
         chaptersData,
       },
-      revalidate: ONE_WEEK_REVALIDATION_PERIOD_SECONDS,
+      revalidate: REVALIDATION_PERIOD_ON_ERROR_SECONDS,
     };
   } catch (error) {
     logErrorToSentry(error, {
