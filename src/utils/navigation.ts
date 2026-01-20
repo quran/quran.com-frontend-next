48541bcb59ac23c7e97c2401ee248d9f58415a3b
afifvdin <afifudin.fdn@gmail.com>
2026-01-20 12:28:42 +0700

    Remove unused

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -65,7 +65,6 @@ export const DEVELOPERS_URL = '/developers';
 export const PRODUCT_UPDATES_URL = '/product-updates';
 export const SUPPORT_URL = '/support';
 export const RAMADAN_2026_URL = '/ramadan2026';
-export const RAMADAN_CHALLENGE_URL = '/ramadanchallenge';
 
 /**
  * Get the href link to a verse.
