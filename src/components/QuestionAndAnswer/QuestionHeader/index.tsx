02b99bedc61e26b705dd0d6172aac030446067ea
al-imam <alimam01828@gmail.com>
2026-01-22 10:22:41 +0600

    feat: Integrate Markdown editor in QuestionHeader for enhanced content display
    - Wrapped the body content in a MilkdownProvider to enable Markdown editing capabilities.
    - Replaced plain text display of the body with a MarkdownEditor set to non-editable mode for improved presentation.

--- src/components/QuestionAndAnswer/QuestionHeader/index.tsx
+++ src/components/QuestionAndAnswer/QuestionHeader/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { MilkdownProvider } from '@milkdown/react';
 import classNames from 'classnames';
 
 import styles from './QuestionHeader.module.scss';
 
+import MarkdownEditor from '@/components/MarkdownEditor';
 import QuestionAndAnswerPill from '@/components/QuestionAndAnswer/Pill';
 import { Question } from '@/types/QuestionsAndAnswers/Question';
 
@@ -15,7 +17,9 @@ const QuestionHeader: React.FC<Props> = ({ body, type, isPage = false }) => {
   return (
     <div className={styles.header}>
       <div dir="auto" className={classNames({ [styles.summary]: isPage })}>
-        {body}
+        <MilkdownProvider>
+          <MarkdownEditor isEditable={false} defaultValue={body} />
+        </MilkdownProvider>
       </div>
       <div className={styles.pillContainer}>
         <QuestionAndAnswerPill type={type} isButton={false} />
