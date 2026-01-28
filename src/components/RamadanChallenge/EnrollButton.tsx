120bb4e0d500e47aa18d41c62b14ac836e916272
afifvdin <afifudin.fdn@gmail.com>
2026-01-29 00:56:18 +0700

    fix ramadan enrollment button

--- src/components/RamadanChallenge/EnrollButton.tsx
+++ src/components/RamadanChallenge/EnrollButton.tsx
@@ -1,49 +1,81 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable i18next/no-literal-string */
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import { useRouter } from 'next/router';
+import { ChannelTypeEnum } from '@novu/headless';
 import useTranslation from 'next-translate/useTranslation';
+
+import { useHeadlessService } from '../Notifications/hooks/useHeadlessService';
 
 import Button, { ButtonVariant } from '@/dls/Button/Button';
 import Spinner from '@/dls/Spinner/Spinner';
 import { ToastStatus, useToast } from '@/dls/Toast/Toast';
-import useRamadanChallengeStatus from '@/hooks/useGetRamadanChallengeStatus';
+import useEmailNotificationSettings from '@/hooks/auth/useEmailNotificationSettings';
 import { logErrorToSentry } from '@/lib/sentry';
 import styles from '@/pages/contentPage.module.scss';
 import { addReadingGoal } from '@/utils/auth/api';
-import { isLoggedIn } from '@/utils/auth/login';
 import { logButtonClick } from '@/utils/eventLogger';
-import { getLoginNavigationUrl } from '@/utils/navigation';
 import { GoalCategory } from 'types/auth/Goal';
+
+const DAILY_RAMADAN_CHALLENGE_TAG = 'DAILY-RAMADAN-CHALLENGE';
 
 interface Props {
   section: string;
+  isEnrolled: boolean;
+  mutate: () => Promise<void>;
+  isLoading: boolean;
+  ctaText;
 }
 
-const EnrollButton = ({ section }: Props) => {
-  const { isEnrolled, mutate, isLoading } = useRamadanChallengeStatus();
+const EnrollButton = ({ section, isEnrolled, mutate, isLoading, ctaText }: Props) => {
+  const { headlessService } = useHeadlessService();
+  const { preferences, isSaving } = useEmailNotificationSettings();
   const { t } = useTranslation('ramadan-activities');
-  const router = useRouter();
   const toast = useToast();
+  const [notificationEnabled, setNotificationEnabled] = useState(false);
   const [isEnrollLoading, setIsEnrollLoading] = useState(false);
 
-  const onButtonClicked = () => {
-    logButtonClick(`ramadan_challenge_${section}`);
+  const ramadanChallengePreference = preferences.find((preference) =>
+    preference.template.tags.includes(DAILY_RAMADAN_CHALLENGE_TAG),
+  );
+
+  const isDisabled = (isEnrolled || isEnrollLoading || isLoading) && notificationEnabled;
+
+  const isSubscribed = isDisabled && notificationEnabled;
+
+  const handleEnableNotification = async () => {
+    // eslint-disable-next-line no-underscore-dangle
+    if (isSaving || notificationEnabled || !ramadanChallengePreference?.template?._id) {
+      return;
+    }
+    setIsEnrollLoading(true);
+    logButtonClick(`ramadan_challenge_enable_notification_${section}`);
+    headlessService.updateUserPreferences({
+      // eslint-disable-next-line no-underscore-dangle
+      templateId: ramadanChallengePreference?.template._id,
+      checked: true,
+      channelType: ChannelTypeEnum.EMAIL,
+      listener: () => {},
+      onSuccess: () => {
+        toast('Enrolled successfully!.', {
+          status: ToastStatus.Success,
+        });
+        setIsEnrollLoading(false);
+        setNotificationEnabled(true);
+      },
+      onError: (err) => {
+        logErrorToSentry(err, {
+          transactionName: 'ramadan_challenge_enable_notification',
+          metadata: { section },
+        });
+        toast('Failed to enroll, please try again later.', { status: ToastStatus.Error });
+        setIsEnrollLoading(false);
+      },
+    });
   };
 
   const handleEnroll = async () => {
-    onButtonClicked();
-
-    if (!isLoggedIn()) {
-      router.push(getLoginNavigationUrl(router.asPath));
-      return;
-    }
-
-    if (isEnrolled) {
-      return;
-    }
-
+    logButtonClick(`ramadan_challenge_enroll${section}`);
     setIsEnrollLoading(true);
     try {
       await addReadingGoal({ category: GoalCategory.RAMADAN_CHALLENGE });
@@ -51,6 +83,7 @@ const EnrollButton = ({ section }: Props) => {
       toast('Enrolled successfully! You may see a welcome email in your inbox.', {
         status: ToastStatus.Success,
       });
+      setNotificationEnabled(true);
     } catch (error) {
       logErrorToSentry(error, {
         transactionName: 'ramadan_challenge_enroll',
@@ -62,42 +95,40 @@ const EnrollButton = ({ section }: Props) => {
     }
   };
 
-  const isDisabled = isEnrolled || isEnrollLoading || isLoading;
+  const handleButton = async () => {
+    if (isEnrolled) {
+      await handleEnableNotification();
+      return;
+    }
 
-  const getButtonText = () => {
-    if (isEnrolled) return 'Subscribed!';
-    return 'Join the Surah Al-Mulk Challenge';
+    if (isDisabled) {
+      return;
+    }
+
+    await handleEnroll();
   };
+
+  useEffect(() => {
+    if (ramadanChallengePreference) {
+      setNotificationEnabled(ramadanChallengePreference.preference.channels.email);
+    }
+  }, [ramadanChallengePreference]);
 
   if (isLoading) {
     return <Spinner />;
   }
 
-  if (!isLoggedIn()) {
-    return (
-      <Button
-        href={getLoginNavigationUrl(router.asPath)}
-        variant={ButtonVariant.Shadow}
-        className={styles.button}
-        isLoading={isEnrollLoading}
-        isDisabled={isDisabled}
-      >
-        {getButtonText()}
-      </Button>
-    );
-  }
-
   return (
     <Button
-      onClick={handleEnroll}
-      variant={isEnrolled ? ButtonVariant.Ghost : ButtonVariant.Shadow}
+      onClick={handleButton}
+      variant={isSubscribed ? ButtonVariant.Ghost : ButtonVariant.Shadow}
       className={styles.button}
       isLoading={isEnrollLoading}
       isDisabled={isDisabled}
-      aria-label={isEnrolled ? t('enrolled') : t('join-challenge')}
+      aria-label={isSubscribed ? t('enrolled') : t('join-challenge')}
       aria-live="polite"
     >
-      {getButtonText()}
+      {isDisabled ? 'Subscribed!' : ctaText}
     </Button>
   );
 };
