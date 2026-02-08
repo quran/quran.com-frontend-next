4bc27a9045592af4de6b49a14814331b9ec9f8de
Yousef Mohamed <egytech45189@gmail.com>
2026-02-08 22:41:38 +0200

    refactor: remove unused CourseFeedback styles and enhance ActionButtons layout
    - Deleted the CourseFeedback.module.scss file as it contained unused styles.
    - Updated CourseFeedback component to remove the container div and improve structure.
    - Refactored ActionButtons styles to introduce a secondary button container for better layout and responsiveness, including adjustments for smaller screens.

--- src/components/Course/CourseFeedback/index.tsx
+++ src/components/Course/CourseFeedback/index.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
+import { useRouter } from 'next/router';
 import useTranslation from 'next-translate/useTranslation';
 
-import styles from './CourseFeedback.module.scss';
 import CourseFeedbackModal from './CourseFeedbackModal';
 
 import Button, { ButtonSize, ButtonType } from '@/dls/Button/Button';
 import { Course } from '@/types/auth/Course';
+import { getUserType, isLoggedIn } from '@/utils/auth/login';
 import { logButtonClick } from '@/utils/eventLogger';
+import { getLoginNavigationUrl } from '@/utils/navigation';
 
 type Props = {
   course: Course;
@@ -23,6 +25,9 @@ export enum FeedbackSource {
 const CourseFeedback: React.FC<Props> = ({ source, course, shouldOpenModal = false }) => {
   const { t } = useTranslation('learn');
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
+  const userIsLoggedIn = isLoggedIn();
+  const userType = getUserType(userIsLoggedIn);
 
   /**
    * listen to changes from the parent component. This will happen when the user
@@ -37,12 +42,19 @@ const CourseFeedback: React.FC<Props> = ({ source, course, shouldOpenModal = fal
   const onAddFeedbackClicked = () => {
     logButtonClick('add_course_feedback', {
       source,
+      userType,
     });
+
+    if (!userIsLoggedIn) {
+      router.push(getLoginNavigationUrl(encodeURIComponent(router.asPath)));
+      return;
+    }
+
     setIsModalOpen(true);
   };
 
   return (
-    <div className={styles.container}>
+    <>
       <Button size={ButtonSize.Small} type={ButtonType.Primary} onClick={onAddFeedbackClicked}>
         {t('feedback.add-feedback')}
       </Button>
@@ -51,7 +63,7 @@ const CourseFeedback: React.FC<Props> = ({ source, course, shouldOpenModal = fal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
       />
-    </div>
+    </>
   );
 };
 
