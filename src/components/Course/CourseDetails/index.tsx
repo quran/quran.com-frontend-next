f8884f1cc56cf645dcd2f5e2e151e20eb74b8684
ahmedosamasawah <sawahahmedosama@gmail.com>
2026-02-10 20:32:20 +0400

    fix(LPs): improve authors section plural and spacing

--- src/components/Course/CourseDetails/index.tsx
+++ src/components/Course/CourseDetails/index.tsx
@@ -123,7 +123,7 @@ const CourseDetails: React.FC<Props> = ({ course }) => {
             </>
           )}
           <DetailSection
-            title={t('about-author')}
+            title={t('about-author', { count: course.authors.length })}
             description={<AuthorsDetails authors={course.authors} />}
           />
           {course?.editors?.length > 0 && (
