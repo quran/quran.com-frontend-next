a5c3ad0198503ef5ac4161820b032ccee9b98508
afifvdin <afifudin.fdn@gmail.com>
2026-02-12 17:46:00 +0700

    fix: usePreventBodyScrolling

--- src/hooks/usePreventBodyScrolling.ts
+++ src/hooks/usePreventBodyScrolling.ts
@@ -6,6 +6,18 @@ import useBrowserLayoutEffect from './useBrowserLayoutEffect';
  *
  * @param {boolean} shouldDisableScrolling
  */
+const preventScrollingToken = 'prevent-scrolling';
+/**
+ * access the count of how many components have requested to prevent scrolling
+ */
+let preventScrollingCount = 0;
+
+/**
+ * This hook disables body scrolling depending on the value passed
+ * to the hook. Inspired by {@link https://usehooks-typescript.com/react-hook/use-locked-body}
+ *
+ * @param {boolean} shouldDisableScrolling
+ */
 const usePreventBodyScrolling = (shouldDisableScrolling = false) => {
   /**
    * Do the side effect before render since we need to get the value of document.body.style.overflow
@@ -16,13 +28,21 @@ const usePreventBodyScrolling = (shouldDisableScrolling = false) => {
     if (!shouldDisableScrolling) {
       return undefined;
     }
-    // Save the initial body style
-    const originalOverflow = document.body.style.overflow;
-    // disable body scrolling bt setting overflow to hidden on the body
-    document.body.style.overflow = 'hidden';
+
+    preventScrollingCount += 1;
+
+    // if this is the first component to prevent scrolling, we need to hide the scrollbar
+    if (preventScrollingCount === 1) {
+      document.body.classList.add(preventScrollingToken);
+    }
+
     return () => {
-      // revert it back
-      document.body.style.overflow = originalOverflow;
+      preventScrollingCount -= 1;
+
+      // if this is the last component to prevent scrolling, we need to show the scrollbar
+      if (preventScrollingCount === 0) {
+        document.body.classList.remove(preventScrollingToken);
+      }
     };
   }, [shouldDisableScrolling]);
 };
