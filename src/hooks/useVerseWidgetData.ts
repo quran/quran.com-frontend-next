f27d61fb28941689a95089457f158a6e230353d7
ahmedosamasawah <sawahahmedosama@gmail.com>
2026-02-13 16:22:27 +0400

    fix: default LP widget translation to Clear Quran

--- src/hooks/useVerseWidgetData.ts
+++ src/hooks/useVerseWidgetData.ts
@@ -2,12 +2,13 @@ import { useSelector } from 'react-redux';
 import useSWR from 'swr';
 
 import type { AyahWidgetData } from '@/components/AyahWidget/getAyahWidgetData';
-import { DEFAULT_TRANSLATIONS } from '@/redux/defaultSettings/defaultSettings';
 import { selectSelectedTranslations } from '@/redux/slices/QuranReader/translations';
 import { selectTheme } from '@/redux/slices/theme';
 import ThemeType from '@/redux/types/ThemeType';
 import type { VerseReference } from '@/utils/lessonContentParser';
 import { fetcher } from 'src/api';
+
+const CLEAR_QURAN_TRANSLATION_ID = 131;
 
 const resolveTheme = (type: string): string => {
   if (type !== ThemeType.Auto) return type;
@@ -22,7 +23,7 @@ const useVerseWidgetData = (reference: VerseReference) => {
   const theme = useSelector(selectTheme);
 
   const translationIds =
-    selectedTranslations?.length > 0 ? selectedTranslations : DEFAULT_TRANSLATIONS;
+    selectedTranslations?.length > 0 ? selectedTranslations : [CLEAR_QURAN_TRANSLATION_ID];
 
   const params = new URLSearchParams({
     chapter: String(reference.chapter),
