bfbe1ef46b24233858f7d3bc40218f2ddf4818a7
Ahmed Osama Sawah <122928535+ahmedosamasawah@users.noreply.github.com>
2026-02-14 19:33:19 +0400

    fix: set learning plan lesson metadata and reuse LP OG image (#3117)
    fix: add LP lesson meta description and OG thumbnail

--- src/components/Course/LessonContent/index.tsx
+++ src/components/Course/LessonContent/index.tsx
@@ -15,11 +15,17 @@ interface Props {
 
 const LessonContent: React.FC<Props> = ({ lesson, lessonSlugOrId, courseSlug }) => {
   const { lang } = useTranslation('learn');
+  const description = `Lesson ${lesson.day} of "${lesson.course.title}"`;
+
   return (
     <>
       <NextSeoWrapper
         title={lesson.title}
+        description={description}
         url={getCanonicalUrl(lang, getLessonNavigationUrl(courseSlug, lessonSlugOrId))}
+        image={lesson.course.thumbnail}
+        imageWidth={1200}
+        imageHeight={1000}
       />
       <LessonView lesson={lesson} lessonSlugOrId={lessonSlugOrId} courseSlug={courseSlug} />
     </>
