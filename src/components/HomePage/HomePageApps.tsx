4b226420b72820d52eb1899ed22e8e8e774dbe33
Al Imam <alimam01828@gmail.com>
2026-02-17 12:26:31 +0600

    [QF-4781] Add Quran Apps Section to Homepage (#3147)
    * feat: add Quran apps section to homepage with styling and localization
    
    * Introduced a new section for Quran apps on the homepage, including descriptions and links for each app.
    * Created corresponding SCSS module for styling the apps section.
    * Updated the homepage layout to incorporate the new apps component.
    * Added localization support for app titles and descriptions in English.
    
    * style: update HomePageApps layout and spacing
    
    * Adjusted the layout of the apps section to use space-between for better distribution.
    * Changed the gap size to small for improved visual consistency.
    * Updated responsive behavior for mobileL breakpoint to use space-evenly.
    
    * feat: enhance app click tracking on HomePageApps
    
    * Added event logging for app clicks to track user interactions.
    * Implemented a handler for logging clicks on individual apps and the "see more" link.
    * Improved code readability by organizing event logging functions.
    
    * fix: update localization and styling for Quran Space app
    
    * Changed localization key from "quran-spaces" to "quran-space" for consistency.
    * Updated the app icon source for the Quran Space app.
    * Adjusted SCSS styles to enhance visual presentation with a new box shadow effect.
    
    * optimize image
    
    * fix: update Quran Space app link and adjust styling
    
    * Sync local
    
    * style: add overflow hidden to appsContainer
    
    * feat: add Quran Apps section and update navigation
    
    * Introduced a new localization key for "Quran Apps" in common.json.
    * Added an SVG icon for Quran Apps to the project.
    * Updated HomePageApps component to use a dynamic URL for the apps link.
    * Enhanced NavigationDrawerList to include a link to the Quran Apps section.
    * Adjusted styles in HomePageApps and index.module.scss for better layout and spacing.
    * Defined a new APPS_URL constant for consistent navigation across the application.
    
    * sync common locals
    
    ---------
    
    Co-authored-by: Osama Sayed <toto777_90@hotmail.com>
    Co-authored-by: Yousef Mohamed <egytech45189@gmail.com>

---
+++ src/components/HomePage/HomePageApps.tsx
@@ -0,0 +1,109 @@
+import classNames from 'classnames';
+import Link from 'next/link';
+import useTranslation from 'next-translate/useTranslation';
+
+import styles from './HomePageApps.module.scss';
+
+import { logButtonClick } from '@/utils/eventLogger';
+import { APPS_URL } from '@/utils/navigation';
+
+interface App {
+  id: string;
+  name: string;
+  description: string;
+  iconSrc: string;
+  iconAlt: string;
+  href: string;
+}
+
+const HomePageApps: React.FC = () => {
+  const { t } = useTranslation('home');
+
+  const handleAppClick = (appId: string) => {
+    logButtonClick(`home_apps_${appId.replaceAll('-', '_')}`);
+  };
+
+  const apps: App[] = [
+    {
+      id: 'quranreflect',
+      name: t('apps.quranreflect.name'),
+      description: t('apps.quranreflect.description'),
+      iconSrc: '/images/app-portal/icon_web_optimized.png',
+      iconAlt: t('apps.quranreflect.name'),
+      href: 'https://quranreflect.com/apps',
+    },
+    {
+      id: 'qariah',
+      name: t('apps.qariah.name'),
+      description: t('apps.qariah.description'),
+      iconSrc: '/images/app-portal/featured/qaariah-icon.webp',
+      iconAlt: t('apps.qariah.name'),
+      href: 'https://qariah.app',
+    },
+    {
+      id: 'quran-space',
+      name: t('apps.quran-space.name'),
+      description: t('apps.quran-space.description'),
+      iconSrc: '/images/app-portal/quran-space-100.jpg',
+      iconAlt: t('apps.quran-space.name'),
+      href: 'https://spaces.labs.quran.com',
+    },
+    {
+      id: 'quran-ios',
+      name: t('apps.quran-ios.name'),
+      description: t('apps.quran-ios.description'),
+      iconSrc: '/images/app-portal/qdc-ios-logo.webp',
+      iconAlt: t('apps.quran-ios.name'),
+      href: 'https://apps.apple.com/us/app/quran-by-quran-com-%D9%82%D8%B1%D8%A2%D9%86/id1118663303',
+    },
+    {
+      id: 'quran-android',
+      name: t('apps.quran-android.name'),
+      description: t('apps.quran-android.description'),
+      iconSrc: '/images/app-portal/qdc-android-logo.webp',
+      iconAlt: t('apps.quran-android.name'),
+      href: 'https://play.google.com/store/apps/details?id=com.quran.labs.androidquran',
+    },
+  ];
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h2 className={styles.title}>{t('apps.title')}</h2>
+        <p className={styles.subtitle}>{t('apps.description')}</p>
+      </div>
+      <div className={styles.appsContainer}>
+        <div className={styles.appsHeader}>
+          <div className={styles.featured}>{t('apps.featured-apps')}</div>
+          <Link
+            href={APPS_URL}
+            className={styles.seeMoreLink}
+            onClick={() => logButtonClick('home_apps_see_more')}
+          >
+            {t('apps.see-more')}
+          </Link>
+        </div>
+        <div className={styles.apps}>
+          {apps.map((app) => (
+            <Link
+              key={app.id}
+              href={APPS_URL}
+              className={classNames(styles.appCard, styles[app.id])}
+              onClick={() => handleAppClick(app.id)}
+            >
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src={app.iconSrc} alt={app.iconAlt} className={styles.appIconImage} />
+
+              <div className={styles.appInfo}>
+                <h3 className={styles.appName}>{app.name}</h3>
+                <p className={styles.appDescription}>{app.description}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePageApps;
