5c84f1b1374343ac5c49b867db7b82faea1bbcf0
Rayane STASZEWSKI <zonetecde@gmail.com>
2026-02-19 22:09:16 +0100

    added the two extra options in the 3-dots menu : settings & translations

--- src/components/Verse/OverflowVerseActionsMenuBody/index.tsx
+++ src/components/Verse/OverflowVerseActionsMenuBody/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
-import { useSelector } from 'react-redux';
+import useTranslation from 'next-translate/useTranslation';
+import { useDispatch, useSelector } from 'react-redux';
 
 import PinVerseAction from '../PinVerseAction';
 import TranslationFeedbackAction from '../TranslationFeedback/TranslationFeedbackAction';
@@ -12,6 +13,11 @@ import ShareVerseActionsMenu from './ShareVerseActionsMenu';
 
 import VerseActionsMenuType from '@/components/QuranReader/ReadingView/WordActionsMenu/types';
 import WordByWordVerseAction from '@/components/QuranReader/ReadingView/WordByWordVerseAction';
+import IconContainer, { IconColor, IconSize } from '@/dls/IconContainer/IconContainer';
+import PopoverMenu from '@/dls/PopoverMenu/PopoverMenu';
+import SettingsIcon from '@/icons/settings.svg';
+import TranslationsIcon from '@/icons/translation.svg';
+import { setIsSettingsDrawerOpen, setSettingsView, SettingsView } from '@/redux/slices/navbar';
 import { selectStudyModeIsOpen } from '@/redux/slices/QuranReader/studyMode';
 import Verse from 'types/Verse';
 
@@ -28,8 +34,25 @@ const OverflowVerseActionsMenuBody: React.FC<Props> = ({
   onActionTriggered,
   isInsideStudyMode = false,
 }) => {
+  const { t } = useTranslation('common');
+  const dispatch = useDispatch();
+
   const [selectedMenu, setSelectedMenu] = useState<VerseActionsMenuType>(VerseActionsMenuType.Main);
   const isStudyModeOpen = useSelector(selectStudyModeIsOpen);
+
+  // Open the translations settings
+  const onTranslationsSettingsClick = () => {
+    dispatch(setSettingsView(SettingsView.Translation));
+    dispatch(setIsSettingsDrawerOpen(true));
+    onActionTriggered?.();
+  };
+
+  // Open the general settings
+  const onSettingsClick = () => {
+    dispatch(setSettingsView(SettingsView.Body));
+    dispatch(setIsSettingsDrawerOpen(true));
+    onActionTriggered?.();
+  };
 
   return selectedMenu === VerseActionsMenuType.Main ? (
     <div>
@@ -50,6 +73,20 @@ const OverflowVerseActionsMenuBody: React.FC<Props> = ({
       {!isStudyModeOpen && (
         <VerseActionRepeatAudio isTranslationView={isTranslationView} verseKey={verse.verseKey} />
       )}
+      <PopoverMenu.Item
+        icon={
+          <IconContainer
+            icon={<TranslationsIcon />}
+            color={IconColor.tertiary}
+            size={IconSize.Custom}
+            shouldFlipOnRTL={false}
+          />
+        }
+        onClick={onTranslationsSettingsClick}
+        shouldCloseMenuAfterClick
+      >
+        {t('translations')}
+      </PopoverMenu.Item>
       <TranslationFeedbackAction
         verse={verse}
         isTranslationView={isTranslationView}
@@ -61,6 +98,20 @@ const OverflowVerseActionsMenuBody: React.FC<Props> = ({
         isTranslationView={isTranslationView}
         onActionTriggered={onActionTriggered}
       />
+      <PopoverMenu.Item
+        icon={
+          <IconContainer
+            icon={<SettingsIcon />}
+            color={IconColor.tertiary}
+            size={IconSize.Custom}
+            shouldFlipOnRTL={false}
+          />
+        }
+        onClick={onSettingsClick}
+        shouldCloseMenuAfterClick
+      >
+        {t('settings.title')}
+      </PopoverMenu.Item>
     </div>
   ) : (
     <ShareVerseActionsMenu
