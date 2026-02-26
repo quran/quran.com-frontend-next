0ed7fe002e5fcf22f89e60afe2b7cf72c87a43df
afifvdin <afifudin.fdn@gmail.com>
2026-02-27 00:14:09 +0700

    fix ssr header style for study mode

--- src/components/QuranReader/ReadingView/StudyModeModal/StudyModeSsrHeader.tsx
+++ src/components/QuranReader/ReadingView/StudyModeModal/StudyModeSsrHeader.tsx
@@ -7,6 +7,7 @@ import styles from './StudyModeModal.module.scss';
 
 import Button, { ButtonShape, ButtonSize, ButtonVariant } from '@/dls/Button/Button';
 import { FakeContentClose } from '@/dls/ContentModal/FakeContentModal';
+import Separator from '@/dls/Separator/Separator';
 import ArrowIcon from '@/icons/arrow.svg';
 import ChevronLeftIcon from '@/icons/chevron-left.svg';
 import CloseIcon from '@/icons/close.svg';
@@ -45,76 +46,81 @@ const StudyModeSsrHeader: React.FC<StudyModeSsrHeaderProps> = ({
   const showHeaderLeftControls = !!verseHistory && !!onGoBack;
 
   return (
-    <div
-      className={classNames(styles.header, {
-        [styles.hideHeaderLeftControls]: !showHeaderLeftControls,
-      })}
-      data-testid={TestId.STUDY_MODE_HEADER}
-    >
-      {showHeaderLeftControls && (
-        <div className={styles.headerLeftControls}>
+    <div className={styles.headerContainer}>
+      <div
+        className={classNames(styles.header, {
+          [styles.hideHeaderLeftControls]: !showHeaderLeftControls,
+        })}
+        data-testid={TestId.STUDY_MODE_HEADER}
+      >
+        {showHeaderLeftControls && (
+          <div className={styles.headerLeftControls}>
+            <Button
+              className={styles.previousRelatedVerseButton}
+              contentClassName={styles.previousRelatedVerseButtonContent}
+              size={ButtonSize.Small}
+              variant={ButtonVariant.Compact}
+              onClick={onGoBack}
+              ariaLabel={t('aria.previous-related-verse')}
+            >
+              <ChevronLeftIcon />
+              <p>
+                {verseHistory?.chapterName} {verseHistory?.localizedVerseKey}
+              </p>
+            </Button>
+          </div>
+        )}
+        <div className={styles.headerMiddleControls}>
+          <div className={styles.selectionWrapper}>
+            <SearchableVerseSelector
+              selectedChapterId={selectedChapterId}
+              selectedVerseNumber={selectedVerseNumber}
+              onChapterChange={onChapterChange}
+              onVerseChange={onVerseChange}
+            />
+          </div>
           <Button
-            className={styles.previousRelatedVerseButton}
-            contentClassName={styles.previousRelatedVerseButtonContent}
             size={ButtonSize.Small}
-            variant={ButtonVariant.Compact}
-            onClick={onGoBack}
-            ariaLabel={t('aria.previous-related-verse')}
-          >
-            <ChevronLeftIcon />
-            <p>
-              {verseHistory?.chapterName} {verseHistory?.localizedVerseKey}
-            </p>
-          </Button>
-        </div>
-      )}
-      <div className={styles.headerMiddleControls}>
-        <div className={styles.selectionWrapper}>
-          <SearchableVerseSelector
-            selectedChapterId={selectedChapterId}
-            selectedVerseNumber={selectedVerseNumber}
-            onChapterChange={onChapterChange}
-            onVerseChange={onVerseChange}
-          />
-        </div>
-        <Button
-          size={ButtonSize.Small}
-          variant={ButtonVariant.Ghost}
-          onClick={onPreviousVerse}
-          className={classNames(styles.navButton, styles.prevButton)}
-          ariaLabel={t('aria.previous-verse')}
-          isDisabled={!canNavigatePrev}
-          shouldFlipOnRTL={false}
-          data-testid={TestId.STUDY_MODE_PREV_VERSE_BUTTON}
-        >
-          <ArrowIcon />
-        </Button>
-        <Button
-          size={ButtonSize.Small}
-          variant={ButtonVariant.Ghost}
-          onClick={onNextVerse}
-          className={classNames(styles.navButton, styles.nextButton)}
-          ariaLabel={t('aria.next-verse')}
-          isDisabled={!canNavigateNext}
-          shouldFlipOnRTL={false}
-          data-testid={TestId.STUDY_MODE_NEXT_VERSE_BUTTON}
-        >
-          <ArrowIcon />
-        </Button>
-      </div>
-      <div className={styles.headerRightControls}>
-        <FakeContentClose
-          className={styles.closeButton}
-          data-testid={TestId.STUDY_MODE_CLOSE_BUTTON}
-        >
-          <Button
             variant={ButtonVariant.Ghost}
-            shape={ButtonShape.Circle}
-            ariaLabel={t('aria.close')}
+            onClick={onPreviousVerse}
+            className={classNames(styles.navButton, styles.prevButton)}
+            ariaLabel={t('aria.previous-verse')}
+            isDisabled={!canNavigatePrev}
+            shouldFlipOnRTL={false}
+            data-testid={TestId.STUDY_MODE_PREV_VERSE_BUTTON}
           >
-            <CloseIcon />
+            <ArrowIcon />
           </Button>
-        </FakeContentClose>
+          <Button
+            size={ButtonSize.Small}
+            variant={ButtonVariant.Ghost}
+            onClick={onNextVerse}
+            className={classNames(styles.navButton, styles.nextButton)}
+            ariaLabel={t('aria.next-verse')}
+            isDisabled={!canNavigateNext}
+            shouldFlipOnRTL={false}
+            data-testid={TestId.STUDY_MODE_NEXT_VERSE_BUTTON}
+          >
+            <ArrowIcon />
+          </Button>
+        </div>
+        <div className={styles.headerRightControls}>
+          <FakeContentClose
+            className={styles.closeButton}
+            data-testid={TestId.STUDY_MODE_CLOSE_BUTTON}
+          >
+            <Button
+              variant={ButtonVariant.Ghost}
+              shape={ButtonShape.Circle}
+              ariaLabel={t('aria.close')}
+            >
+              <CloseIcon />
+            </Button>
+          </FakeContentClose>
+        </div>
+      </div>
+      <div className={styles.separatorContainer}>
+        <Separator />
       </div>
     </div>
   );
