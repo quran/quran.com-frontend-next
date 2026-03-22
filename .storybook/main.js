2cdb90d79df1c1c9a3d62bd751372c9eb4d067ab
Mir-Zairan <zairanmir123@gmail.com>
2026-03-22 09:28:44 +0530

    fix: add missing @/styles alias to Storybook webpack config
    The @/styles path alias was not included in the manually defined aliases
    in .storybook/main.js. This caused a SassError when Storybook tried to
    compile components that use `@use '@/styles/constants'` or
    `@use '@/styles/breakpoints'` in their SCSS modules (e.g. Button, Sorter).

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,13 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  stories: ["../src/**/*.stories.tsx"],
+  stories: ['../src/**/*.stories.tsx'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-a11y",
-    "@storybook/addon-storysource",
-    "storybook-addon-rtl"
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-storysource',
+    'storybook-addon-rtl',
   ],
   framework: {
     name: '@storybook/nextjs',
@@ -19,7 +19,7 @@ module.exports = {
     checkOptions: {},
     // ideally we use `react-docgen-typescript`. But there's still some issue, related to webpack 5.
     // so we use `react-docgen` for now
-    reactDocgen: "react-docgen",
+    reactDocgen: 'react-docgen',
     // reactDocgenTypescriptOptions: {
     //   shouldExtractLiteralValuesFromEnum: true,
     //   propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
@@ -27,36 +27,32 @@ module.exports = {
   },
   webpackFinal: async (config) => {
     // Add support for absolute paths
-    config.resolve.modules = [
-      ...(config.resolve.modules || []),
-      path.resolve("./"),
-    ];
+    config.resolve.modules = [...(config.resolve.modules || []), path.resolve('./')];
 
     // @/ root alias doesn't work in storybook, so we have to write the aliases manually
-    const otherAliases = ["components", "utils", "redux", "hooks", "contexts"];
+    const otherAliases = ['components', 'utils', 'redux', 'hooks', 'contexts'];
 
     // Add support for module aliases (same aliases in tsconfig.json)
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      "@/icons": path.resolve(__dirname, "../public/icons"),
-      "@/public": path.resolve(__dirname, "../public"),
-      "@/dls": path.resolve(__dirname, "../src/components/dls"),
-      "@/api": path.resolve(__dirname, "../src/api"),
-      "@/data": path.resolve(__dirname, "../data"),
-      "@/types": path.resolve(__dirname, "../types"),
+      '@/icons': path.resolve(__dirname, '../public/icons'),
+      '@/public': path.resolve(__dirname, '../public'),
+      '@/styles': path.resolve(__dirname, '../src/styles'),
+      '@/dls': path.resolve(__dirname, '../src/components/dls'),
+      '@/api': path.resolve(__dirname, '../src/api'),
+      '@/data': path.resolve(__dirname, '../data'),
+      '@/types': path.resolve(__dirname, '../types'),
       ...otherAliases.reduce(
         (acc, folder) => ({
           ...acc,
-          [`@/${folder}`]: path.resolve(__dirname, "../src", folder),
+          [`@/${folder}`]: path.resolve(__dirname, '../src', folder),
         }),
-        {}
+        {},
       ),
     };
 
     const fileLoaderRule = config.module.rules.find((rule) =>
-      Array.isArray(rule.test)
-        ? rule.test[0]?.test(".svg")
-        : rule.test?.test(".svg")
+      Array.isArray(rule.test) ? rule.test[0]?.test('.svg') : rule.test?.test('.svg'),
     );
     fileLoaderRule.exclude = /\.svg$/;
     // This is needed for inline svg imports
@@ -65,14 +61,14 @@ module.exports = {
       exclude: /node_modules/,
       use: [
         {
-          loader: "@svgr/webpack",
+          loader: '@svgr/webpack',
           options: {
             prettier: false,
             svgo: true,
             svgoConfig: {
               plugins: [
                 {
-                  name: "preset-default",
+                  name: 'preset-default',
                   params: {
                     overrides: {
                       removeViewBox: false,
