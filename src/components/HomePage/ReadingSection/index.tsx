8d40aa5a9a57a222683fb34e958c0178594d4590
Spacey <85053722+Mubashir78@users.noreply.github.com>
2026-07-10 15:51:35 +0000

    fix: fall back to Redux lastReadVerse for Continue Reading surah (#3210)

--- src/components/HomePage/ReadingSection/index.tsx
+++ src/components/HomePage/ReadingSection/index.tsx
@@ -21,6 +21,7 @@ import useMappedBookmark from '@/hooks/useMappedBookmark';
 import BookmarkRemoveIcon from '@/icons/bookmark_remove.svg';
 import { selectGuestReadingBookmark } from '@/redux/slices/guestBookmark';
 import { selectQuranReaderStyles } from '@/redux/slices/QuranReader/styles';
+import { selectLastReadVerseKey } from '@/redux/slices/QuranReader/readingTracker';
 import { selectUserState } from '@/redux/slices/session';
 import BookmarkType from '@/types/BookmarkType';
 import { getMushafId } from '@/utils/api';
@@ -28,7 +29,7 @@ import { GuestReadingBookmark } from '@/utils/bookmark';
 import { logButtonClick } from '@/utils/eventLogger';
 import { MY_QURAN_URL } from '@/utils/navigation';
 import { isMobile } from '@/utils/responsive';
-import { getPageFirstVerseKey } from '@/utils/verse';
+import { getPageFirstVerseKey, getVerseAndChapterNumbersFromKey } from '@/utils/verse';
 
 interface Props {}
 
@@ -37,6 +38,7 @@ const ReadingSection: React.FC<Props> = () => {
   const { isFirstTimeGuest, isGuest } = useSelector(selectUserState);
   const quranReaderStyles = useSelector(selectQuranReaderStyles);
   const guestReadingBookmark = useSelector(selectGuestReadingBookmark);
+  const lastReadVerse = useSelector(selectLastReadVerseKey);
   const mushafId = getMushafId(quranReaderStyles.quranFont, quranReaderStyles.mushafLines).mushaf;
 
   // Use global reading bookmark hook (singleton pattern)
@@ -105,11 +107,15 @@ const ReadingSection: React.FC<Props> = () => {
     effectiveAyahVerseKey?.surahNumber ??
     firstVerseOfStoredPage?.surahNumber ??
     recentlyReadVerseKeys?.[0]?.surah ??
+    lastReadVerse?.chapterId ??
     1;
   const effectiveVerseNumber =
     effectiveAyahVerseKey?.verseNumber ??
     firstVerseOfStoredPage?.verseNumber ??
-    recentlyReadVerseKeys?.[0]?.ayah;
+    recentlyReadVerseKeys?.[0]?.ayah ??
+    (lastReadVerse?.verseKey
+      ? getVerseAndChapterNumbersFromKey(lastReadVerse.verseKey)[1]
+      : undefined);
 
   // Resolve page number for current mushaf (use effectivePageNumber from hook)
   const resolvedPageNumber = isPageBookmark ? effectivePageNumber ?? undefined : undefined;
